6fbccb7ae9c289ae43d9ad771aab2656380bff86
William Liu <shengwei.liu@okcoin.net>
2020-04-15 15:12:51 +0800

    init swap module.

---
+++ x/swap/client/cli/query.go
@@ -0,0 +1,38 @@
+package cli
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+
+	"github.com/okex/okchain/x/swap/types"
+)
+
+// GetQueryCmd returns the cli query commands for this module
+func GetQueryCmd(queryRoute string, cdc *codec.Codec) *cobra.Command {
+	// Group swap queries under a subcommand
+	swapQueryCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("Querying commands for the %s module", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	swapQueryCmd.AddCommand(
+		flags.GetCommands(
+		// TODO: Add query Cmds
+		)...,
+	)
+
+	return swapQueryCmd
+}
+
+// TODO: Add Query Commands
---
+++ x/swap/client/cli/tx.go
@@ -0,0 +1,59 @@
+package cli
+
+import (
+	"bufio"
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/context"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	"github.com/cosmos/cosmos-sdk/x/auth/client/utils"
+	"github.com/okex/okchain/x/swap/types"
+)
+
+// GetTxCmd returns the transaction commands for this module
+func GetTxCmd(cdc *codec.Codec) *cobra.Command {
+	swapTxCmd := &cobra.Command{
+		Use:                        types.ModuleName,
+		Short:                      fmt.Sprintf("%s transactions subcommands", types.ModuleName),
+		DisableFlagParsing:         true,
+		SuggestionsMinimumDistance: 2,
+		RunE:                       client.ValidateCmd,
+	}
+
+	swapTxCmd.AddCommand(flags.PostCommands(
+	// TODO: Add tx based commands
+	// GetCmd<Action>(cdc)
+	)...)
+
+	return swapTxCmd
+}
+
+// Example:
+//
+// GetCmd<Action> is the CLI command for doing <Action>
+// func GetCmd<Action>(cdc *codec.Codec) *cobra.Command {
+// 	return &cobra.Command{
+// 		Use:   "/* Describe your action cmd */",
+// 		Short: "/* Provide a short description on the cmd */",
+// 		Args:  cobra.ExactArgs(2), // Does your request require arguments
+// 		RunE: func(cmd *cobra.Command, args []string) error {
+// 			cliCtx := context.NewCLIContext().WithCodec(cdc)
+// 			inBuf := bufio.NewReader(cmd.InOrStdin())
+// 			txBldr := auth.NewTxBuilderFromCLI(inBuf).WithTxEncoder(utils.GetTxEncoder(cdc))
+
+// 			msg := types.NewMsg<Action>(/* Action params */)
+// 			err = msg.ValidateBasic()
+// 			if err != nil {
+// 				return err
+// 			}
+
+// 			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+// 		},
+// 	}
+// }
