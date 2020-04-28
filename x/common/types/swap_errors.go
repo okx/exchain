51f0068fee8829b07f279d9f2f911de0f148496f
lei.ji@okcoin.com <lei.ji@okcoin.net>
2020-04-28 21:42:04 +0800

    update code in swap_errors

--- x/common/types/swap_errors.go
+++ x/common/types/swap_errors.go
@@ -17,64 +17,64 @@ func ErrQuoteOnlySupportsNativeToken(codespace sdk.CodespaceType) sdk.Error {
 
 // nolint
 func ErrSwapTokenPairIsExist(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. swapTokenPair already exists")
+	return sdk.NewError(codespace, 63002, "failed. swapTokenPair already exists")
 }
 
 // nolint
 func ErrBlockTimeExceededDeadline(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. blockTime exceeded deadline")
+	return sdk.NewError(codespace, 63003, "failed. blockTime exceeded deadline")
 }
 
 // nolint
 func ErrGetPoolTokenFailed(codespace sdk.CodespaceType, poolToken, msg string) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. get poolToken %s failed: %s", poolToken, msg)
+	return sdk.NewError(codespace, 63004, "failed. get poolToken %s failed: %s", poolToken, msg)
 }
 
 // nolint
 func ErrUnexpectedTotalSupply(codespace sdk.CodespaceType, poolToken string) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. unexpected totalSupply in poolToken %s", poolToken)
+	return sdk.NewError(codespace, 63005, "failed. unexpected totalSupply in poolToken %s", poolToken)
 }
 
 // nolint
 func ErrInvalidSwapTokenPair(codespace sdk.CodespaceType, tokenPair string) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. invalid swapTokenPair %s", tokenPair)
+	return sdk.NewError(codespace, 63006, "failed. invalid swapTokenPair %s", tokenPair)
 }
 
 // nolint
 func ErrMaxBaseAmountIsTooLow(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. MaxBaseAmount is too low")
+	return sdk.NewError(codespace, 63007, "failed. MaxBaseAmount is too low")
 }
 
 // nolint
 func ErrMinLiquidityIsTooHigh(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. MinLiquidity is too high")
+	return sdk.NewError(codespace, 63008, "failed. MinLiquidity is too high")
 }
 
 // nolint
 func ErrFailToMintPoolCoins(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. fail to mint poolCoins")
+	return sdk.NewError(codespace, 63009, "failed. fail to mint poolCoins")
 }
 
 // nolint
 func ErrInsufficientPoolToken(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. insufficient poolToken")
+	return sdk.NewError(codespace, 63010, "failed. insufficient poolToken")
 }
 
 // nolint
 func ErrMinBaseAmountIsTooHigh(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. MinBaseAmount is too high")
+	return sdk.NewError(codespace, 63011, "failed. MinBaseAmount is too high")
 }
 // nolint
 func ErrMinQuoteAmountIsTooHigh(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. MinQuoteAmount is too high")
+	return sdk.NewError(codespace, 63012, "failed. MinQuoteAmount is too high")
 }
 
 // nolint
 func ErrFailToBurnPoolCoins(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. fail to burn poolCoins")
+	return sdk.NewError(codespace, 63013, "failed. fail to burn poolCoins")
 }
 
 // nolint
 func ErrMinBountTokenAmountIsTooHigh(codespace sdk.CodespaceType) sdk.Error {
-	return sdk.NewError(codespace, 63001, "failed. MinBoughtTokenAmount is too high")
+	return sdk.NewError(codespace, 63014, "failed. MinBoughtTokenAmount is too high")
 }
