a02f91a27f2846a40076a5e4f442464ad4cf5a57
MichaelWang <guanyi.wang@okcoin.net>
2020-04-28 11:58:07 +0800

    FINISH: error frame

--- x/common/types/common_errors.go
+++ x/common/types/common_errors.go
@@ -1,1 +1,24 @@
 package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+type CodeType = sdk.CodeType
+
+const (
+	commonCodespace sdk.CodespaceType = "common"
+
+	codeMissingRequiredParam CodeType = 60001
+	codeInvalidRequestParam  CodeType = 60002
+)
+
+// ErrMissingRequiredParam returns an error when the required param is missing
+func ErrMissingRequiredParam() sdk.Error {
+	return sdk.NewError(commonCodespace, codeMissingRequiredParam, "failed. the required param is missing")
+}
+
+// ErrInvalidRequestParam returns an error when the the request param is invalid
+func ErrInvalidRequestParam(msg string) sdk.Error {
+	return sdk.NewError(commonCodespace, codeInvalidRequestParam, "failed. invalid request param: %s", msg)
+}
--- x/common/types/spot_errors.go
+++ x/common/types/spot_errors.go
@@ -1,1 +1,9 @@
 package types
+
+import (
+	sdk "github.com/cosmos/cosmos-sdk/types"
+)
+
+const (
+	spotCodespace sdk.CodespaceType = "spot"
+)
