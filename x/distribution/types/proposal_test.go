1937e2d40adb7e54974a8b4ffc3bdb09d8d609b7
banishee <gl12138@live.com>
2020-04-29 06:39:26 +0800

    Increase the x/distribution unit-test coverage (#88)
    * promote the distribution/keeper ut coverage
    
    * promote the distribution/types ut coverage
    
    * promote the distribution ut coverage about some error
    
    * promote the distribution ut coverage about proposal
    
    * fix distribution ut about proposal
    
    Co-authored-by: GeYiLi <yili.ge@okcoin.net>
    Co-authored-by: Zhong Qiu <36867992+zhongqiuwood@users.noreply.github.com>

---
+++ x/distribution/types/proposal_test.go
@@ -0,0 +1,35 @@
+package types
+
+import (
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/stretchr/testify/require"
+	"github.com/tendermint/tendermint/crypto/ed25519"
+)
+
+func TestNewCommunityPoolSpendProposal(t *testing.T) {
+	title := "Withdraw coins"
+	description := "Want to get some coins as reward"
+	recipient := sdk.AccAddress(ed25519.GenPrivKey().PubKey().Address())
+	amount := sdk.NewDecCoinsFromDec(sdk.DefaultBondDenom, sdk.OneDec())
+	proposal := NewCommunityPoolSpendProposal(title, description, recipient, amount)
+
+	require.Equal(t, title, proposal.GetTitle())
+	require.Equal(t, description, proposal.GetDescription())
+	require.Equal(t, RouterKey, proposal.ProposalRoute())
+	require.Equal(t, ProposalTypeCommunityPoolSpend, proposal.ProposalType())
+	require.Nil(t, proposal.ValidateBasic())
+	require.NotPanics(t, func() {
+		_ = proposal.String()
+	})
+
+	proposal.Title = ""
+	require.Error(t, proposal.ValidateBasic())
+	proposal.Title = title
+	proposal.Amount = sdk.DecCoins{sdk.DecCoin{Denom: "UNKNOWN", Amount: sdk.OneDec()}}
+	require.Error(t, proposal.ValidateBasic())
+	proposal.Amount = sdk.NewDecCoinsFromDec(sdk.DefaultBondDenom, sdk.OneDec())
+	proposal.Recipient = nil
+	require.Error(t, proposal.ValidateBasic())
+}
