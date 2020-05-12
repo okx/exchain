6d04c5066936fd8235a1c4943e1a9a6fa465ad1c
MichaelWang <guanyi.wang@okcoin.net>
2020-05-12 19:26:08 +0800

    FIX: pass ut

--- x/token/types/params_test.go
+++ x/token/types/params_test.go
@@ -14,7 +14,6 @@ FeeIssue: 20000.00000000okt
 FeeMint: 2000.00000000okt
 FeeBurn: 10.00000000okt
 FeeModify: 0.00000000okt
-FeeSend: 0.00000000okt
 FeeMultiSend: 0.01000000okt
 FeeChown: 10.00000000okt
 `
@@ -26,7 +25,6 @@ FeeChown: 10.00000000okt
 		{Key: KeyFeeMint, Value: &param.FeeMint},
 		{Key: KeyFeeBurn, Value: &param.FeeBurn},
 		{Key: KeyFeeModify, Value: &param.FeeModify},
-		{Key: KeyFeeSend, Value: &param.FeeSend},
 		{Key: KeyFeeMultiSend, Value: &param.FeeMultiSend},
 		{Key: KeyFeeChown, Value: &param.FeeChown},
 	}
