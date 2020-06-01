27811034116c8277175b151496369df50db59ac4
yanwei.han <yanwei.han@okcoin.net>
2020-06-01 16:18:17 +0800

    delete saving module

--- x/saving/types/errors.go
+++
@@ -1,7 +0,0 @@
-package types
-
-// TODO: Fill out some custom errors for the module
-// You can see how they are constructed below:
-// var (
-//	ErrInvalid = sdkerrors.Register(ModuleName, 1, "custom error message")
-// )
