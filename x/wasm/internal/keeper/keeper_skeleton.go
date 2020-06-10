cc98b3b4891cddcbab22e2b745a4b9cea0e997f3
liuxiong <xiong.liu@okcoin.net>
2020-06-10 11:24:56 +0800

    make all struct empty in wasm module

--- x/wasm/internal/keeper/keeper_skeleton.go
+++ x/wasm/internal/keeper/keeper_skeleton.go
@@ -1,9 +1,6 @@
 package keeper
 
 import (
-	"encoding/json"
-
-	wasmTypes "github.com/CosmWasm/go-cosmwasm/types"
 	"github.com/cosmos/cosmos-sdk/codec"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
@@ -11,26 +8,9 @@ import (
 	"github.com/okex/okchain/x/wasm/internal/types"
 )
 
-type BankEncoder func(sender sdk.AccAddress, msg *wasmTypes.BankMsg) ([]sdk.Msg, error)
-type CustomEncoder func(sender sdk.AccAddress, msg json.RawMessage) ([]sdk.Msg, error)
-type StakingEncoder func(sender sdk.AccAddress, msg *wasmTypes.StakingMsg) ([]sdk.Msg, error)
-type WasmEncoder func(sender sdk.AccAddress, msg *wasmTypes.WasmMsg) ([]sdk.Msg, error)
+type MessageEncoders struct {}
 
-type MessageEncoders struct {
-	Bank    BankEncoder
-	Custom  CustomEncoder
-	Staking StakingEncoder
-	Wasm    WasmEncoder
-}
-
-type CustomQuerier func(ctx sdk.Context, request json.RawMessage) ([]byte, error)
-
-type QueryPlugins struct {
-	Bank    func(ctx sdk.Context, request *wasmTypes.BankQuery) ([]byte, error)
-	Custom  CustomQuerier
-	Staking func(ctx sdk.Context, request *wasmTypes.StakingQuery) ([]byte, error)
-	Wasm    func(ctx sdk.Context, request *wasmTypes.WasmQuery) ([]byte, error)
-}
+type QueryPlugins struct {}
 
 type Keeper struct{}
 
--- x/wasm/internal/types/types.go
+++ x/wasm/internal/types/types.go
@@ -1,18 +1,10 @@
 package types
 
-const defaultLRUCacheSize = uint64(0)
-const defaultQueryGasLimit = uint64(3000000)
 
 // WasmConfig is the extra config required for wasm
-type WasmConfig struct {
-	SmartQueryGasLimit uint64 `mapstructure:"query_gas_limit"`
-	CacheSize          uint64 `mapstructure:"lru_size"`
-}
+type WasmConfig struct {}
 
 // DefaultWasmConfig returns the default settings for WasmConfig
 func DefaultWasmConfig() WasmConfig {
-	return WasmConfig{
-		SmartQueryGasLimit: defaultQueryGasLimit,
-		CacheSize:          defaultLRUCacheSize,
-	}
+	return WasmConfig{}
 }
