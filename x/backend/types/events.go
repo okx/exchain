a84e3fae5ecfd129dc3d5cf0d1ae3c2b8cb3823e
lingting.fu <lingting.fu@okcoin.com>
2020-06-15 17:03:09 +0800

    clean code

--- x/backend/types/events.go
+++ x/backend/types/events.go
@@ -3,7 +3,6 @@ package types
 import (
 	"fmt"
 	"github.com/cosmos/cosmos-sdk/codec"
-	"github.com/tendermint/tendermint/rpc/core"
 	tm "github.com/tendermint/tendermint/types"
 	//sdk "github.com/cosmos/cosmos-sdk/types"
 )
@@ -33,21 +32,5 @@ func QueryForEvent(eventType string) string {
 }
 
 func RegisterEventDatas(cdc *codec.Codec) {
-	//cdc.RegisterInterface((*tm.TMEventData)(nil), nil)
-	//cdc.RegisterConcrete(EventDataTicker{}, "okchain/event/Ticker", nil)
 	cdc.RegisterConcrete(EventDataBackend{}, "tendermint/event/Backend", nil)
-}
-
-func PublishBackend(backend *EventDataBackend) error {
-
-	// websocket extend, register event type to rpc cdc
-	if !RPCCdcRegistered && core.GetCoreCdc() != nil {
-		RegisterCodec(core.GetCoreCdc())
-		RPCCdcRegistered = true
-	}
-	eventBus := core.GetEventBus()
-	if eventBus != nil {
-		return eventBus.Publish(EventTypeBackend, *backend)
-	}
-	return nil
 }
