07536169ce99ee850127578512d14d0be45251df
liuxiong <xiong.liu@okcoin.net>
2020-06-17 15:24:16 +0800

    fix unit test bug

--- x/token/genesis_test.go
+++ x/token/genesis_test.go
@@ -13,7 +13,7 @@ import (
 func TestDefault(t *testing.T) {
 	genesisState := defaultGenesisState()
 	err := validateGenesis(genesisState)
-	require.NoError(t, err)
+	require.Error(t, err)
 	defaultGenesisStateOKT()
 }
 
