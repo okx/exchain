3978e49475238e6d6f0d7e67a5373cfc20623aeb
yanwei.han <yanwei.han@okcoin.net>
2020-06-18 11:12:39 +0800

    fix bind same proxy bug

--- x/staking/handler_vote.go
+++ x/staking/handler_vote.go
@@ -35,6 +35,15 @@ func handleMsgBindProxy(ctx sdk.Context, msg types.MsgBindProxy, k keeper.Keeper
 		return types.ErrDoubleProxy(types.DefaultCodespace, delegator.DelegatorAddress.String()).Result()
 	}
 
+	// same proxy, only update shares
+	if delegator.HasProxy() && delegator.ProxyAddress.Equals(proxyDelegator.DelegatorAddress) {
+		updateTokens := proxyDelegator.TotalDelegatedTokens.Add(proxyDelegator.Tokens)
+		if err := k.UpdateVotes(ctx, proxyDelegator.DelegatorAddress, updateTokens); err != nil {
+			return types.ErrInvalidDelegation(types.DefaultCodespace, proxyDelegator.DelegatorAddress.String()).Result()
+		}
+		return sdk.Result{Events: ctx.EventManager().Events()}
+	}
+
 	// unbind from the original proxy
 	if len(delegator.ProxyAddress) != 0 {
 		if sdkErr := unbindProxy(ctx, delegator.DelegatorAddress, k); sdkErr != nil {
@@ -46,10 +55,6 @@ func handleMsgBindProxy(ctx sdk.Context, msg types.MsgBindProxy, k keeper.Keeper
 	delegator.BindProxy(msg.ProxyAddress)
 
 	// update proxy's vote weight
-	proxyDelegator, found = k.GetDelegator(ctx, msg.ProxyAddress)
-	if !found {
-		return types.ErrNotFoundProxy(types.DefaultCodespace, msg.ProxyAddress.String()).Result()
-	}
 	proxyDelegator.TotalDelegatedTokens = proxyDelegator.TotalDelegatedTokens.Add(delegator.Tokens)
 
 	k.SetDelegator(ctx, delegator)
