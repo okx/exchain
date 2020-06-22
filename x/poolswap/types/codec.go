1be1309412d2f62b961a6d2943d1d75b78ab66bb
william liu <geek.yoio.liu@gmail.com>
2020-06-22 14:10:09 +0800

    spell check

--- x/poolswap/types/codec.go
+++ x/poolswap/types/codec.go
@@ -6,10 +6,10 @@ import (
 
 // RegisterCodec registers concrete types on codec
 func RegisterCodec(cdc *codec.Codec) {
-	cdc.RegisterConcrete(MsgAddLiquidity{}, "okchain/swap/MsgAddLiquidity", nil)
-	cdc.RegisterConcrete(MsgRemoveLiquidity{}, "okchain/swap/MsgRemoveLiquidity", nil)
-	cdc.RegisterConcrete(MsgCreateExchange{}, "okchain/swap/MsgCreateExchange", nil)
-	cdc.RegisterConcrete(MsgTokenToNativeToken{}, "okchain/swap/MsgSwapToken", nil)
+	cdc.RegisterConcrete(MsgAddLiquidity{}, "okchain/poolswap/MsgAddLiquidity", nil)
+	cdc.RegisterConcrete(MsgRemoveLiquidity{}, "okchain/poolswap/MsgRemoveLiquidity", nil)
+	cdc.RegisterConcrete(MsgCreateExchange{}, "okchain/poolswap/MsgCreateExchange", nil)
+	cdc.RegisterConcrete(MsgTokenToNativeToken{}, "okchain/poolswap/MsgSwapToken", nil)
 }
 
 // ModuleCdc defines the module codec
