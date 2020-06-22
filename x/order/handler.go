a969b80b45dced980b3fa1c518925d64bd013b50
MichaelWang <michael_wangguanyi@163.com>
2020-06-22 17:53:44 +0800

    FIX: nil pointer CalculateGas

--- x/order/handler.go
+++ x/order/handler.go
@@ -18,6 +18,9 @@ import (
 )
 
 func CalculateGas(msg sdk.Msg, params *types.Params) (gas uint64) {
+	if params == nil {
+		panic("failed. a nil pointer appears")
+	}
 	switch msg := msg.(type) {
 	case types.MsgNewOrders:
 		gas = msg.CalculateGas(params.NewOrderMsgGasUnit)
