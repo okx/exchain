d52207b0f2d85b130ef29b895698f4d63492b84a
MichaelWang <michael_wangguanyi@163.com>
2020-09-25 18:36:06 +0800

    FINISH: querier for pool

--- x/farm/keeper/querier.go
+++ x/farm/keeper/querier.go
@@ -1,6 +1,7 @@
 package keeper
 
 import (
+	"fmt"
 	"github.com/cosmos/cosmos-sdk/codec"
 	abci "github.com/tendermint/tendermint/abci/types"
 
@@ -28,9 +29,41 @@ func NewQuerier(k Keeper) sdk.Querier {
 	}
 }
 
-// TODO: queriers
 func queryPool(ctx sdk.Context, req abci.RequestQuery, k Keeper) ([]byte, sdk.Error) {
-	return nil, nil
+	////////////////////////////////////////////////////////////
+	// TODO: demo for test. remove it later
+	tPool := types.FarmPool{
+		PoolName:          "pool-airtoken1-eth",
+		LockedTokenSymbol: "locked_token_symbol",
+		TotalLockedCoin:   sdk.NewDecCoinFromDec("btc", sdk.OneDec()),
+		YieldingCoins: types.YieldingCoins{{
+			sdk.NewDecCoinFromDec("btc", sdk.OneDec()),
+			1024,
+			sdk.OneDec(),
+		}},
+		YieldedCoins:           sdk.Coins{sdk.NewDecCoinFromDec("btc", sdk.OneDec())},
+		LastYieldedBlockHeight: 2048,
+		TotalLockedWeight:      sdk.OneDec(),
+	}
+	k.SetFarmPool(ctx, tPool)
+	////////////////////////////////////////////////////////////
+	var params types.QueryPoolParams
+
+	if err := types.ModuleCdc.UnmarshalJSON(req.Data, &params); err != nil {
+		return nil, defaultQueryErrParseParams(err)
+	}
+
+	pool, found := k.GetFarmPool(ctx, params.PoolName)
+	if !found {
+		return nil, types.ErrNoFarmPoolFound(types.DefaultCodespace, params.PoolName)
+	}
+
+	res, err := codec.MarshalJSONIndent(types.ModuleCdc, pool)
+	if err != nil {
+		return nil, defaultQueryErrJSONMarshal(err)
+	}
+
+	return res, nil
 }
 
 func queryPools(ctx sdk.Context, k Keeper) ([]byte, sdk.Error) {
@@ -72,4 +105,8 @@ func queryWhitelist(ctx sdk.Context, k Keeper) ([]byte, sdk.Error) {
 
 func defaultQueryErrJSONMarshal(err error) sdk.Error {
 	return sdk.ErrInternal(sdk.AppendMsgToErr("failed to marshal result to JSON", err.Error()))
+}
+
+func defaultQueryErrParseParams(err error) sdk.Error {
+	return sdk.ErrInternal(fmt.Sprintf("failed to parse params. %s", err))
 }
--- x/farm/types/querier.go
+++ x/farm/types/querier.go
@@ -7,3 +7,16 @@ const (
 	QueryParams    = "params"
 	QueryWhitelist = "whitelist"
 )
+
+// QueryPoolParams defines the params for the following queries:
+// - 'custom/farm/pool'
+type QueryPoolParams struct {
+	PoolName string
+}
+
+// NewQueryPoolParams creates a new instance of QueryPoolParams
+func NewQueryPoolParams(poolName string) QueryPoolParams {
+	return QueryPoolParams{
+		PoolName: poolName,
+	}
+}
