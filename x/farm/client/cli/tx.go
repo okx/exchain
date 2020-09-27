3f9bb0d77bce79a4ba27ec60a9b4d4817e4dd02a
evan.han <iloversevan@gmail.com>
2020-09-27 14:40:33 +0800

    create pool

--- x/farm/client/cli/tx.go
+++ x/farm/client/cli/tx.go
@@ -26,19 +26,56 @@ func GetTxCmd(cdc *codec.Codec) *cobra.Command {
 	}
 
 	farmTxCmd.AddCommand(
+		GetCmdCreatePool(cdc),
+		GetCmdDestroyPool(cdc),
 		GetCmdProvide(cdc),
 		GetCmdLock(cdc),
 		GetCmdUnlock(cdc),
 		GetCmdClaim(cdc),
-		)
+	)
 	return farmTxCmd
+}
+
+func GetCmdCreatePool(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "create-pool [pool-name] [lock-token] [yield-token]",
+		Short: "create a farm pool with the name of pool, token to be locked in the pool and token to be yielded",
+		Args:  cobra.ExactArgs(3),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+
+			poolName := args[0]
+			lockToken := args[1]
+			yieldToken := args[2]
+			msg := types.NewMsgCreatePool(cliCtx.GetFromAddress(), poolName, lockToken, yieldToken)
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
+	return cmd
+}
+
+func GetCmdDestroyPool(cdc *codec.Codec) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "destroy-pool [pool-name]",
+		Short: "destroy a farm pool",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
+			cliCtx := context.NewCLIContext().WithCodec(cdc)
+			poolName := args[0]
+			msg := types.NewMsgDestroyPool(cliCtx.GetFromAddress(), poolName)
+			return utils.GenerateOrBroadcastMsgs(cliCtx, txBldr, []sdk.Msg{msg})
+		},
+	}
+	return cmd
 }
 
 func GetCmdProvide(cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "provide [pool-name] [amount] [yield-per-block] [start-height-to-yield]",
 		Short: "provide yiled-token into a pool, and start mining the token after the specified height",
-		Args: cobra.ExactArgs(4),
+		Args:  cobra.ExactArgs(4),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
@@ -53,7 +90,7 @@ func GetCmdProvide(cdc *codec.Codec) *cobra.Command {
 				return err
 			}
 
-			startHeightToYiled, err := strconv.ParseInt(args[3], 10,64)
+			startHeightToYiled, err := strconv.ParseInt(args[3], 10, 64)
 			if err != nil {
 				return err
 			}
@@ -70,7 +107,7 @@ func GetCmdLock(cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "lock [pool-name] [amount]",
 		Short: "lock a number of coins for liquidity mining",
-		Args: cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
@@ -92,7 +129,7 @@ func GetCmdUnlock(cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "unlock [pool-name] [amount]",
 		Short: "unlock a number of coins for mining reward",
-		Args: cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
@@ -114,7 +151,7 @@ func GetCmdClaim(cdc *codec.Codec) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "claim [pool-name]",
 		Short: "claim all the mining rewards till this moment",
-		Args: cobra.ExactArgs(1),
+		Args:  cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			txBldr := auth.NewTxBuilderFromCLI().WithTxEncoder(utils.GetTxEncoder(cdc))
 			cliCtx := context.NewCLIContext().WithCodec(cdc)
--- x/farm/types/events.go
+++ x/farm/types/events.go
@@ -8,14 +8,12 @@ const (
 	EventTypeUnlock     = "unlock"
 	EventTypeClaim      = "claim"
 
-	// TODO: Create keys fo your events, the values will be derivided from the msg
 	AttributeKeyAddress            = "address"
 	AttributeKeyPool               = "pool"
 	AttributeKeyStartHeightToYield = "start_height_to_yield"
 	AttributeKeyYiledPerBlock      = "yield_per_block"
-
-	// TODO: Some events may not have values for that reason you want to emit that something happened.
-	// AttributeValueDoubleSign = "double_sign"
+	AttributeKeyLockToken          = "lock_token"
+	AttributeKeyYieldToken         = "yield_token"
 
 	AttributeValueCategory = ModuleName
 )
