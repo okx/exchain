a94866dd62c2602a591f20eed355cacbb76a609d
Green <gl12138@live.com>
2020-09-29 18:43:49 +0800

    commit the alpha version about begin test

--- x/farm/beginblocker_test.go
+++ x/farm/beginblocker_test.go
@@ -1,6 +1,9 @@
 package farm
 
 import (
+	"bytes"
+	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/cosmos/cosmos-sdk/codec"
@@ -8,7 +11,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/x/auth"
 	"github.com/cosmos/cosmos-sdk/x/bank"
-	"github.com/cosmos/cosmos-sdk/x/mock"
 	"github.com/cosmos/cosmos-sdk/x/supply"
 	swap "github.com/okex/okexchain/x/ammswap/keeper"
 	swaptypes "github.com/okex/okexchain/x/ammswap/types"
@@ -26,37 +28,39 @@ const (
 	TestChainID = "okexchain"
 )
 
+var (
+	Addrs = createTestAddrs(10)
+)
+
 type MockFarmKeeper struct {
 	Keeper
 	StoreKey     sdk.StoreKey
-	TkeyStoreKey sdk.StoreKey
 	SupplyKeeper supply.Keeper
 	MountedStore store.MultiStore
 	AccKeeper    auth.AccountKeeper
 }
 
-type MockApp struct {
-	*mock.App
-
-	//keyOrder     *sdk.KVStoreKey
-	//keyToken     *sdk.KVStoreKey
-	//keyLock      *sdk.KVStoreKey
-	//keyDex       *sdk.KVStoreKey
-	//keyTokenPair *sdk.KVStoreKey
-	//keySupply *sdk.KVStoreKey
-
-	bankKeeper   bank.Keeper
-	tokenKeeper  token.Keeper
-	supplyKeeper supply.Keeper
-	swapKeeper   swap.Keeper
+func NewMockFarmKeeper(k Keeper, keyStoreKey sdk.StoreKey, sKeeper supply.Keeper,
+	ms store.MultiStore, accKeeper auth.AccountKeeper) MockFarmKeeper {
+	return MockFarmKeeper{
+		k,
+		keyStoreKey,
+		sKeeper,
+		ms,
+		accKeeper,
+	}
 }
 
-func TestEndBlocker(t *testing.T) {
-
+func TestBeginBlocker(t *testing.T) {
+	ctx, mk := getKeeper(t)
+	k := mk.Keeper
+	for i := int64(1); i < 10; i++ {
+		ctx = ctx.WithBlockHeight(i)
+		BeginBlocker(ctx, abci.RequestBeginBlock{Header: abci.Header{Height: i}}, k)
+	}
 }
 
-func getMockApp(t *testing.T) {
-	mApp := mock.NewApp()
+func getKeeper(t *testing.T) (sdk.Context, MockFarmKeeper) {
 
 	// 0.1 init store key
 	keyFarm := sdk.NewKVStoreKey(types.StoreKey)
@@ -66,6 +70,8 @@ func getMockApp(t *testing.T) {
 	tkeyParams := sdk.NewTransientStoreKey(params.TStoreKey)
 	keySupply := sdk.NewKVStoreKey(supply.StoreKey)
 	keyToken := sdk.NewKVStoreKey(token.StoreKey)
+	keyLock := sdk.NewKVStoreKey(token.KeyLock)
+	keySwap := sdk.NewKVStoreKey(swaptypes.StoreKey)
 
 	// 0.2 init db
 	db := dbm.NewMemDB()
@@ -77,6 +83,7 @@ func getMockApp(t *testing.T) {
 	ms.MountStoreWithDB(tkeyParams, sdk.StoreTypeTransient, db)
 	ms.MountStoreWithDB(keySupply, sdk.StoreTypeIAVL, db)
 	ms.MountStoreWithDB(keyToken, sdk.StoreTypeIAVL, db)
+	ms.MountStoreWithDB(keySwap, sdk.StoreTypeIAVL, db)
 	err := ms.LoadLatestVersion()
 	require.Nil(t, err)
 
@@ -96,6 +103,7 @@ func getMockApp(t *testing.T) {
 	supply.RegisterCodec(cdc)
 	bank.RegisterCodec(cdc)
 	token.RegisterCodec(cdc)
+	auth.RegisterCodec(cdc)
 	params.RegisterCodec(cdc)
 	swaptypes.RegisterCodec(cdc)
 	codec.RegisterCrypto(cdc)
@@ -115,6 +123,14 @@ func getMockApp(t *testing.T) {
 	blacklistedAddrs[farmAcc.String()] = true
 
 	bk := bank.NewBaseKeeper(ak, pk.Subspace(bank.DefaultParamspace), bank.DefaultCodespace, blacklistedAddrs)
+	// fill all the addresses with some coins
+	initCoins := sdk.NewCoins(sdk.NewCoin(sdk.DefaultBondDenom, sdk.NewInt(1000)))
+	for _, addr := range Addrs {
+		_, err := bk.AddCoins(ctx, addr, initCoins)
+		if err != nil {
+			panic(err)
+		}
+	}
 
 	// 1.4 init supply keeper
 	maccPerms := map[string][]string{
@@ -123,10 +139,69 @@ func getMockApp(t *testing.T) {
 	}
 	sk := supply.NewKeeper(cdc, keySupply, ak, bk, maccPerms)
 	sk.SetSupply(ctx, supply.NewSupply(sdk.NewDecCoinsFromDec(sdk.DefaultBondDenom, sdk.NewDec(1000000000))))
+	sk.SetModuleAccount(ctx, feeCollectorAcc)
+	sk.SetModuleAccount(ctx, farmAcc)
 
 	// 1.5 init token keeper
-	tk := token.NewKeeper(bk, pk.Subspace(token.DefaultParamspace), auth.FeeCollectorName, sk, keyToken, keyToken, cdc, false)
+	tk := token.NewKeeper(bk, pk.Subspace(token.DefaultParamspace), auth.FeeCollectorName, sk, keyToken, keyLock, cdc, false)
 
-	// 1.6 init farm keeper
-	fk := NewKeeper(auth.FeeCollectorName, sk, tk, )
+	// 1.6 init swap keeper
+	swapKeeper := swap.NewKeeper(sk, tk, cdc, keySwap, pk.Subspace(swaptypes.DefaultParamspace))
+
+	// 1.7 init farm keeper
+	fk := NewKeeper(auth.FeeCollectorName, sk, tk, swapKeeper, pk.Subspace(types.DefaultParamspace), keyFarm, cdc)
+	fk.SetParams(ctx, types.DefaultParams())
+	// 2. init mock keeper
+	mk := NewMockFarmKeeper(fk, keyFarm, sk, ms, ak)
+
+	//// 3. init mockApp
+	//mApp := mock.NewApp()
+	//mApp.Router().AddRoute(types.RouterKey, NewHandler(fk))
+	//
+	//require.NoError(t, mApp.CompleteSetup(mk.StoreKey))
+	return ctx, mk
+}
+
+func createTestAddrs(numAddrs int) []sdk.AccAddress {
+	var addresses []sdk.AccAddress
+	var buffer bytes.Buffer
+
+	// start at 100 so we can make up to 999 test addresses with valid test addresses
+	for i := 100; i < (numAddrs + 100); i++ {
+		numString := strconv.Itoa(i)
+		buffer.WriteString("A58856F0FD53BF058B4909A21AEC019107BA6") //base address string
+
+		buffer.WriteString(numString) //adding on final two digits to make addresses unique
+		res, err := sdk.AccAddressFromHex(buffer.String())
+		if err != nil {
+			fmt.Print("error")
+		}
+		bech := res.String()
+		addresses = append(addresses, testAddr(buffer.String(), bech))
+		buffer.Reset()
+	}
+	return addresses
+}
+
+// TestAddr is designed for incode address generation
+func testAddr(addr string, bech string) sdk.AccAddress {
+
+	res, err := sdk.AccAddressFromHex(addr)
+	if err != nil {
+		panic(err)
+	}
+	bechexpected := res.String()
+	if bech != bechexpected {
+		panic("Bech encoding doesn't match reference")
+	}
+
+	bechres, err := sdk.AccAddressFromBech32(bech)
+	if err != nil {
+		panic(err)
+	}
+	if !bytes.Equal(bechres, res) {
+		panic("Bech decode and hex decode don't match")
+	}
+
+	return res
 }
