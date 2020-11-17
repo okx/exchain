17ad2cd43edeef5f59aeb1206dc71fc6e4a2de4a
MengXiangJian <805442788@qq.com>
2020-11-17 10:35:21 +0800

    optimize checking upper bound for minting token (#394)

--- x/token/handler.go
+++ x/token/handler.go
@@ -216,18 +216,18 @@ func handleMsgTokenMint(ctx sdk.Context, keeper Keeper, msg types.MsgTokenMint, 
 		return sdk.ErrUnauthorized(fmt.Sprintf("token(%s) is not mintable", token.Symbol)).Result()
 	}
 
+	// check upper bound
+	totalSupplyAfterMint := keeper.supplyKeeper.GetSupplyByDenom(ctx, msg.Amount.Denom).Add(msg.Amount.Amount)
+	if totalSupplyAfterMint.GT(sdk.NewDec(types.TotalSupplyUpperbound)) {
+		return sdk.ErrInternal(fmt.Sprintf("total-supply(%s) exceeds the upper limit(%d)",
+			totalSupplyAfterMint, types.TotalSupplyUpperbound)).Result()
+	}
+
 	mintCoins := msg.Amount.ToCoins()
 	// set supply
 	err := keeper.supplyKeeper.MintCoins(ctx, types.ModuleName, mintCoins)
 	if err != nil {
 		return sdk.ErrInternal(fmt.Sprintf("supply mint coins error:%s", err.Error())).Result()
-	}
-
-	// check upper bound
-	totalSupply := keeper.supplyKeeper.GetSupplyByDenom(ctx, msg.Amount.Denom)
-	if totalSupply.GT(sdk.NewDec(types.TotalSupplyUpperbound)) {
-		return sdk.ErrInternal(fmt.Sprintf("total-supply(%s) exceeds the upper limit(%d)",
-			totalSupply, types.TotalSupplyUpperbound)).Result()
 	}
 
 	// send coins to acc
