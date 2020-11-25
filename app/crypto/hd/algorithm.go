ce7e99f0401f688e6e5345b86b0a843ffbb33cf2
ylsGit <>
2020-11-25 18:39:37 +0800

    reset import

--- app/crypto/hd/algorithm.go
+++ app/crypto/hd/algorithm.go
@@ -3,9 +3,9 @@ package hd
 import (
 	"github.com/pkg/errors"
 
-	"github.com/bartekn/go-bip39"
 	"github.com/btcsuite/btcd/chaincfg"
 	"github.com/btcsuite/btcutil/hdkeychain"
+	"github.com/tyler-smith/go-bip39"
 
 	ethaccounts "github.com/ethereum/go-ethereum/accounts"
 	ethcrypto "github.com/ethereum/go-ethereum/crypto"
