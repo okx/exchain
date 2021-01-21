2b9fc6ea9d04db0cb4998bc19b02f16ee898d26a
MichaelWang <michael_wangguanyi@163.com>
2021-01-21 11:22:15 +0800

    FIX: conflicts

--- x/evm/types/utils_test.go
+++ x/evm/types/utils_test.go
@@ -1,13 +1,12 @@
 package types
 
 import (
-	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-
 	ethcmn "github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/require"
+	"strings"
+	"testing"
 )
 
 func TestEvmDataEncoding(t *testing.T) {
@@ -34,6 +33,32 @@ func TestEvmDataEncoding(t *testing.T) {
 	require.Equal(t, bloom, res.Bloom)
 	require.Equal(t, data.Logs, res.Logs)
 	require.Equal(t, ret, res.Ret)
+}
+
+func TestValidateSigner(t *testing.T) {
+	const digest = "default digest"
+	digestHash := crypto.Keccak256([]byte(digest))
+	priv, err := crypto.GenerateKey()
+	require.NotNil(t, priv)
+	require.NoError(t, err)
+
+	ethAddr := crypto.PubkeyToAddress(priv.PublicKey)
+	require.NoError(t, err)
+
+	sig, err := crypto.Sign(digestHash, priv)
+	require.NoError(t, err)
+
+	err = ValidateSigner(digestHash, sig, ethAddr)
+	require.NoError(t, err)
+
+	// different eth address
+	otherEthAddr := ethcmn.BytesToAddress([]byte{1})
+	err = ValidateSigner(digestHash, sig, otherEthAddr)
+	require.Error(t, err)
+
+	// invalid digestHash
+	err = ValidateSigner(digestHash[1:], sig, otherEthAddr)
+	require.Error(t, err)
 }
 
 func TestResultData_String(t *testing.T) {
