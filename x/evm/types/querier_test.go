308a8ba921619ab44976342b202394cedecd6dce
MichaelWang <michael_wangguanyi@163.com>
2021-01-27 13:34:17 +0800

    Merge PR: improve evm types UT coverage (#602)
    * FIX: conflicts
    
    * FIX: conflicts
    
    * FIX: conflicts
    
    * FIX: conflicts
    
    * FINISH: TestTxDecoder
    
    * FINISH: TestTxData_String
    
    * FINISH: TestParams_String
    
    * FINISH: TestQueryString
    
    Co-authored-by: KamiD <44460798+KamiD@users.noreply.github.com>

--- x/evm/types/querier_test.go
+++ x/evm/types/querier_test.go
@@ -7,6 +7,38 @@ import (
 	"testing"
 )
 
+func TestQueryString(t *testing.T) {
+	const (
+		balance                        = "1024.1024okt"
+		number                         = int64(1024)
+		nonce                          = uint64(1024)
+		expectedQueryResBalanceStr     = "1024.1024okt"
+		expectedQueryResBlockNumberStr = "1024"
+		expectedBytesStr               = "test bytes"
+		expectedQueryResNonceStr       = "1024"
+	)
+
+	bytes := []byte("test bytes")
+	queryResBalance := QueryResBalance{balance}
+	require.True(t, strings.EqualFold(expectedQueryResBalanceStr, queryResBalance.String()))
+
+	queryResBlockNumber := QueryResBlockNumber{number}
+	require.True(t, strings.EqualFold(expectedQueryResBlockNumberStr, queryResBlockNumber.String()))
+
+	queryResCode := QueryResCode{bytes}
+	require.True(t, strings.EqualFold(expectedBytesStr, queryResCode.String()))
+
+	queryResStorage := QueryResStorage{bytes}
+	require.True(t, strings.EqualFold(expectedBytesStr, queryResStorage.String()))
+
+	queryResNonce := QueryResNonce{nonce}
+	require.True(t, strings.EqualFold(expectedQueryResNonceStr, queryResNonce.String()))
+
+	bloom := ethtypes.BytesToBloom(bytes)
+	queryBloomFilter := QueryBloomFilter{bloom}
+	require.True(t, strings.EqualFold(string(bloom[:]), queryBloomFilter.String()))
+}
+
 func TestQueryETHLogs_String(t *testing.T) {
 	const expectedQueryETHLogsStr = `{0x0000000000000000000000000000000000000000 [] [1 2 3 4] 9 0x0000000000000000000000000000000000000000000000000000000000000000 0 0x0000000000000000000000000000000000000000000000000000000000000000 0 false}
 {0x0000000000000000000000000000000000000000 [] [5 6 7 8] 10 0x0000000000000000000000000000000000000000000000000000000000000000 0 0x0000000000000000000000000000000000000000000000000000000000000000 0 false}
--- x/evm/types/utils_test.go
+++ x/evm/types/utils_test.go
@@ -1,13 +1,15 @@
 package types
 
 import (
-	"strings"
-	"testing"
-
-	"github.com/stretchr/testify/require"
-
+	"github.com/cosmos/cosmos-sdk/codec"
+	sdk "github.com/cosmos/cosmos-sdk/types"
 	ethcmn "github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/stretchr/testify/require"
+	"math/big"
+	"strings"
+	"testing"
 )
 
 func TestEvmDataEncoding(t *testing.T) {
@@ -34,6 +36,36 @@ func TestEvmDataEncoding(t *testing.T) {
 	require.Equal(t, bloom, res.Bloom)
 	require.Equal(t, data.Logs, res.Logs)
 	require.Equal(t, ret, res.Ret)
+
+	// error check
+	_, err = DecodeResultData(enc[1:])
+	require.Error(t, err)
+}
+
+func TestValidateSigner(t *testing.T) {
+	const digest = "default digest"
+	digestHash := crypto.Keccak256([]byte(digest))
+	priv, err := crypto.GenerateKey()
+	require.NotNil(t, priv)
+	require.NoError(t, err)
+
+	ethAddr := crypto.PubkeyToAddress(priv.PublicKey)
+	require.NoError(t, err)
+
+	sig, err := crypto.Sign(digestHash, priv)
+	require.NoError(t, err)
+
+	err = ValidateSigner(digestHash, sig, ethAddr)
+	require.NoError(t, err)
+
+	// different eth address
+	otherEthAddr := ethcmn.BytesToAddress([]byte{1})
+	err = ValidateSigner(digestHash, sig, otherEthAddr)
+	require.Error(t, err)
+
+	// invalid digestHash
+	err = ValidateSigner(digestHash[1:], sig, otherEthAddr)
+	require.Error(t, err)
 }
 
 func TestResultData_String(t *testing.T) {
@@ -65,4 +97,35 @@ func TestResultData_String(t *testing.T) {
 	}
 
 	require.True(t, strings.EqualFold(expectedResultDataStr, data.String()))
+}
+
+func TestTxDecoder(t *testing.T) {
+	expectUint64, expectedBigInt, expectedBytes := uint64(1024), big.NewInt(1024), []byte("default payload")
+	expectedEthAddr := ethcmn.BytesToAddress([]byte("test_address"))
+	expectedEthMsg := NewMsgEthereumTx(expectUint64, &expectedEthAddr, expectedBigInt, expectUint64, expectedBigInt, expectedBytes)
+
+	// register codec
+	cdc := codec.New()
+	cdc.RegisterInterface((*sdk.Tx)(nil), nil)
+	RegisterCodec(cdc)
+
+	txbytes := cdc.MustMarshalBinaryLengthPrefixed(expectedEthMsg)
+	txDecoder := TxDecoder(cdc)
+	tx, err := txDecoder(txbytes)
+	require.NoError(t, err)
+
+	msgs := tx.GetMsgs()
+	require.Equal(t, 1, len(msgs))
+	require.NoError(t, msgs[0].ValidateBasic())
+	require.True(t, strings.EqualFold(expectedEthMsg.Route(), msgs[0].Route()))
+	require.True(t, strings.EqualFold(expectedEthMsg.Type(), msgs[0].Type()))
+
+	require.NoError(t, tx.ValidateBasic())
+
+	// error check
+	_, err = txDecoder([]byte{})
+	require.Error(t, err)
+
+	_, err = txDecoder(txbytes[1:])
+	require.Error(t, err)
 }
