fedb52712f27dbd6784ee800ad2aa9512797d5af
shaoyun.zhan@okcoin.com <shaoyun.zhan@okcoin.com>
2021-02-03 19:06:22 +0800

    add ut : compate 0x address to send and query

--- app/types/address_test.go
+++ app/types/address_test.go
@@ -3,6 +3,7 @@ package types
 import (
 	"github.com/cosmos/cosmos-sdk/types"
 	"github.com/stretchr/testify/require"
+	"strings"
 	"testing"
 )
 
@@ -19,6 +20,8 @@ func TestAccAddressFromBech32(t *testing.T) {
 		{"okexchain19n6w5l0htdgn2zwet9rtgvrzuf4a3qp49c9fml", true},
 		{"0x0073F2E28ef8F117e53d858094086Defaf1837D5", true},
 		{"2CF4ea7dF75b513509d95946B43062E26bD88035", true},
+		{strings.ToLower("2CF4ea7dF75b513509d95946B43062E26bD88035"), true},
+		{strings.ToUpper("2CF4ea7dF75b513509d95946B43062E26bD88035"), true},
 		{"okexchain19n6w5l0htdgn2zwet9rtgvrzuf4a3qp49c9fml_", false},
 		{"0x0073F2E28ef8F117e53d858094086Defaf1837D5_", false},
 		{"0073F2E28ef8F117e53d858094086Defaf1837D5_", false},
