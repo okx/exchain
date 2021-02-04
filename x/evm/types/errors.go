73d75787e10620663154773404d46ecb68f669fb
KamiD <44460798+KamiD@users.noreply.github.com>
2021-02-04 18:39:42 +0800

    Merge PR #626 : fix undefined error message returned when calling a contract interface
    * fix no error returned from solidity
    
    * delete unused code
    
    * optimize code
    
    * add ut to test revert tx message will returned

--- x/evm/types/errors.go
+++ x/evm/types/errors.go
@@ -21,4 +21,6 @@ var (
 
 	// ErrCallDisabled returns an error if the EnableCall parameter is false.
 	ErrCallDisabled = sdkerrors.Register(ModuleName, 6, "EVM Call operation is disabled")
+
+	CodeSpaceEvmCallFailed = uint32(7)
 )
