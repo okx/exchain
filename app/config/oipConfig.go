bdf42e6a38dd0ed12961004131c1e5709c7e4fcc
KamiD <yzglwt@sina.com>
2021-02-10 16:36:10 +0800

    set disable block number to 99999999 for temp

--- app/config/oipConfig.go
+++ app/config/oipConfig.go
@@ -5,7 +5,7 @@ import sdk "github.com/cosmos/cosmos-sdk/types"
 //All of the OIP Configure in this package
 
 const (
-	DisableTransferToContractBlock = int64(10)
+	DisableTransferToContractBlock = int64(999999999)
 )
 
 func IsDisableTransferToContract(ctx sdk.Context) bool {
