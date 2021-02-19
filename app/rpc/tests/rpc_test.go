486c156abd75574f66bf6234fdd9506d602ca51a
Green <gl12138@live.com>
2021-02-19 16:50:27 +0800

    ENHANCE: eth_subscribe & eth_unsubscribe ut

--- app/rpc/tests/rpc_test.go
+++ app/rpc/tests/rpc_test.go
@@ -1240,16 +1240,24 @@ func TestEth_Subscribe_And_UnSubscribe(t *testing.T) {
 	ws, err := websocket.Dial(url, "", origin)
 	require.NoError(t, err)
 
-	// send message
-	message := []byte(`{"id": 2, "method": "eth_subscribe", "params": ["newHeads"]}`)
-	excuteOneRound(t, ws, message)
+	// send valid message
+	validMessage := []byte(`{"id": 2, "method": "eth_subscribe", "params": ["newHeads"]}`)
+	excuteValidMessage(t, ws, validMessage)
+
+	// send invalid message
+	invalidMessage := []byte(`{"id": 2, "method": "eth_subscribe", "params": ["non-existent method"]}`)
+	excuteInvalidMessage(t, ws, invalidMessage)
+
+	invalidMessage = []byte(`{"id": 2, "method": "eth_subscribe", "params": [""]}`)
+	excuteInvalidMessage(t, ws, invalidMessage)
 
 	// close websocket
 	err = ws.Close()
 	require.NoError(t, err)
 }
 
-func excuteOneRound(t *testing.T, ws  *websocket.Conn, message []byte) {
+func excuteValidMessage(t *testing.T, ws  *websocket.Conn, message []byte) {
+	fmt.Println("Send:", string(message))
 	_, err := ws.Write(message)
 	require.NoError(t, err)
 
@@ -1270,7 +1278,7 @@ func excuteOneRound(t *testing.T, ws  *websocket.Conn, message []byte) {
 
 	// cancel the subscription
 	cancelMsg := fmt.Sprintf(`{"id": 2, "method": "eth_unsubscribe", "params": [%s]}`, subscriptionId)
-	fmt.Println(cancelMsg)
+	fmt.Println("Send:", cancelMsg)
 	_, err = ws.Write([]byte(cancelMsg))
 	require.NoError(t, err)
 
@@ -1279,4 +1287,20 @@ func excuteOneRound(t *testing.T, ws  *websocket.Conn, message []byte) {
 	require.NoError(t, err)
 	require.NoError(t, json.Unmarshal(msg[:n], &res))
 	require.Equal(t, "true", string(res.Result))
+}
+
+func excuteInvalidMessage(t *testing.T, ws  *websocket.Conn, message []byte) {
+	fmt.Println("Send:", string(message))
+	_, err := ws.Write(message)
+	require.NoError(t, err)
+
+	msg := make([]byte, 10240)
+	// receive error msg
+	n, err := ws.Read(msg)
+	require.NoError(t, err)
+
+	var res Response
+	require.NoError(t, json.Unmarshal(msg[:n], &res))
+	require.Equal(t, -32600, res.Error.Code)
+	require.Equal(t, 0, res.ID)
 }
