b2af0d247e683acf5ee93f5292464ea2eb342618
MichaelWang <michael_wangguanyi@163.com>
2021-03-10 15:43:25 +0800

    FINISH: right block hash in header subscription of websocket

--- app/rpc/types/types.go
+++ app/rpc/types/types.go
@@ -2,6 +2,7 @@ package types
 
 import (
 	"fmt"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"strings"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -137,4 +138,24 @@ type Account struct {
 	Balance   **hexutil.Big                `json:"balance"`
 	State     *map[common.Hash]common.Hash `json:"state"`
 	StateDiff *map[common.Hash]common.Hash `json:"stateDiff"`
+}
+
+// HeaderWithBlockHash represents a block header in the Ethereum blockchain with block hash generated from Tendermint Block
+type HeaderWithBlockHash struct {
+	ParentHash  common.Hash         `json:"parentHash"`
+	UncleHash   common.Hash         `json:"sha3Uncles"`
+	Coinbase    common.Address      `json:"miner"`
+	Root        common.Hash         `json:"stateRoot"`
+	TxHash      common.Hash         `json:"transactionsRoot"`
+	ReceiptHash common.Hash         `json:"receiptsRoot"`
+	Bloom       ethtypes.Bloom      `json:"logsBloom"`
+	Difficulty  *hexutil.Big        `json:"difficulty"`
+	Number      *hexutil.Big        `json:"number"`
+	GasLimit    hexutil.Uint64      `json:"gasLimit"`
+	GasUsed     hexutil.Uint64      `json:"gasUsed"`
+	Time        hexutil.Uint64      `json:"timestamp"`
+	Extra       hexutil.Bytes       `json:"extraData"`
+	MixDigest   common.Hash         `json:"mixHash"`
+	Nonce       ethtypes.BlockNonce `json:"nonce"`
+	Hash        common.Hash         `json:"hash"`
 }
