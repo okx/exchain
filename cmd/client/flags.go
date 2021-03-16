81d110b6b9bc4a1d454265521807971f0a4161a3
ylsGit <bzwdyls@163.com>
2021-03-16 16:01:23 +0800

    add flag 'personal-api'

---
+++ cmd/client/flags.go
@@ -0,0 +1,11 @@
+package client
+
+import "github.com/spf13/cobra"
+
+const (
+	FlagPersonalAPI = "personal-api"
+)
+
+func RegisterAppFlag(cmd *cobra.Command) {
+	cmd.Flags().Bool(FlagPersonalAPI, false, "Enable the the personal_ prefixed set of APIs in the Web3 JSON-RPC spec")
+}
