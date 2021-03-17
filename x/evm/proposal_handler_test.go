c60ea7ff5e177e1c1a42a25a6e5bd4be1c65f4c0
MichaelWang <michael_wangguanyi@163.com>
2021-03-17 20:56:01 +0800

    FINISH: proposal handler ut — ManageContractBlockedListProposal

--- x/evm/proposal_handler_test.go
+++ x/evm/proposal_handler_test.go
@@ -7,7 +7,7 @@ import (
 	govtypes "github.com/okex/okexchain/x/gov/types"
 )
 
-func (suite *EvmTestSuite) TestProposalHandler() {
+func (suite *EvmTestSuite) TestProposalHandler_ManageContractDeploymentWhitelistProposal() {
 	proposal := types.NewManageContractDeploymentWhitelistProposal(
 		"default title",
 		"default description",
@@ -62,7 +62,78 @@ func (suite *EvmTestSuite) TestProposalHandler() {
 	}
 
 	for _, tc := range testCases {
-		suite.Run("", func() {
+		suite.Run(tc.msg, func() {
+			tc.malleate()
+
+			err := suite.govHandler(suite.ctx, &govProposal)
+
+			tc.statusCheck()
+
+			if tc.expectedError {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+			}
+		})
+	}
+}
+
+func (suite *EvmTestSuite) TestProposalHandler_ManageContractBlockedListProposal() {
+	proposal := types.NewManageContractBlockedListProposal(
+		"default title",
+		"default description",
+		ethcmn.BytesToAddress([]byte{0x0}).Bytes(),
+		true,
+	)
+
+	suite.govHandler = evm.NewManageContractDeploymentWhitelistProposalHandler(suite.app.EvmKeeper)
+	govProposal := govtypes.Proposal{
+		Content: proposal,
+	}
+
+	testCases := []struct {
+		msg           string
+		malleate      func()
+		statusCheck   func()
+		expectedError bool
+	}{
+		{
+			"add address into blocked list",
+			func() {},
+			func() {
+				blockedList := suite.stateDB.GetContractBlockedList()
+				suite.Require().Equal(1, len(blockedList))
+			},
+			false,
+		},
+		{
+			"add address repeatedly",
+			func() {},
+			func() {},
+			true,
+		},
+		{
+			"delete address from blocked list",
+			func() {
+				proposal.IsAdded = false
+				govProposal.Content = proposal
+			},
+			func() {
+				blockedList := suite.stateDB.GetContractBlockedList()
+				suite.Require().Zero(len(blockedList))
+			},
+			false,
+		},
+		{
+			"delete an address not in the blocked list",
+			func() {},
+			func() {},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.msg, func() {
 			tc.malleate()
 
 			err := suite.govHandler(suite.ctx, &govProposal)
