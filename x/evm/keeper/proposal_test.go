55941b49ae684378bad5a6739d14878d16bd1bbc
MichaelWang <michael_wangguanyi@163.com>
2021-03-18 11:52:35 +0800

    FINISH: table test for blocked list checking proposal in evmKeeper

--- x/evm/keeper/proposal_test.go
+++ x/evm/keeper/proposal_test.go
@@ -10,7 +10,10 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func (suite *KeeperTestSuite) TestProposal() {
+func (suite *KeeperTestSuite) TestProposal_ManageContractDeploymentWhitelistProposal() {
+	// reset state
+	suite.SetupTest()
+
 	addr := ethcmn.BytesToAddress([]byte{0x0}).Bytes()
 	addrUnqualified := ethcmn.BytesToAddress([]byte{0x1}).Bytes()
 	proposal := types.NewManageContractDeploymentWhitelistProposal(
@@ -33,14 +36,110 @@ func (suite *KeeperTestSuite) TestProposal() {
 	msg := govtypes.NewMsgSubmitProposal(proposal, minDeposit, addr)
 	require.NoError(suite.T(), suite.app.EvmKeeper.CheckMsgSubmitProposal(suite.ctx, msg))
 
-	// check ManageContractDeploymentWhitelistProposal in details
-	require.NoError(suite.T(), suite.app.EvmKeeper.CheckMsgManageContractDeploymentWhitelistProposal(suite.ctx, proposal))
-	// error check
-	// to add a address already exists in whitelist
-	suite.stateDB.SetContractDeploymentWhitelistMember(addr)
-	require.Error(suite.T(), suite.app.EvmKeeper.CheckMsgManageContractDeploymentWhitelistProposal(suite.ctx, proposal))
-	// to delete a address not in the whitelist
-	proposal.DistributorAddr = addrUnqualified
-	proposal.IsAdded = false
-	require.Error(suite.T(), suite.app.EvmKeeper.CheckMsgManageContractDeploymentWhitelistProposal(suite.ctx, proposal))
+	testCases := []struct {
+		msg           string
+		malleate      func()
+		expectedError bool
+	}{
+		{
+			"pass check",
+			func() {},
+			false,
+		},
+		{
+			"try to add an address already exists in whitelist",
+			func() {
+				suite.stateDB.SetContractDeploymentWhitelistMember(addr)
+			},
+			true,
+		},
+		{
+			"try to delete an address not in the whitelist",
+			func() {
+				proposal.IsAdded = false
+				proposal.DistributorAddr = addrUnqualified
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.msg, func() {
+			tc.malleate()
+
+			err := suite.app.EvmKeeper.CheckMsgManageContractDeploymentWhitelistProposal(suite.ctx, proposal)
+			if tc.expectedError {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+			}
+		})
+	}
+}
+
+func (suite *KeeperTestSuite) TestProposal_ManageContractBlockedListProposal() {
+	// reset state
+	suite.SetupTest()
+
+	addr := ethcmn.BytesToAddress([]byte{0x0}).Bytes()
+	addrUnqualified := ethcmn.BytesToAddress([]byte{0x1}).Bytes()
+	proposal := types.NewManageContractBlockedListProposal(
+		"default title",
+		"default description",
+		addr,
+		true,
+	)
+
+	minDeposit := suite.app.EvmKeeper.GetMinDeposit(suite.ctx, proposal)
+	require.Equal(suite.T(), sdk.SysCoins{sdk.NewDecCoin(sdk.DefaultBondDenom, sdk.NewInt(100))}, minDeposit)
+
+	maxDepositPeriod := suite.app.EvmKeeper.GetMaxDepositPeriod(suite.ctx, proposal)
+	require.Equal(suite.T(), time.Hour*24, maxDepositPeriod)
+
+	votingPeriod := suite.app.EvmKeeper.GetVotingPeriod(suite.ctx, proposal)
+	require.Equal(suite.T(), time.Hour*72, votingPeriod)
+
+	// check submit proposal
+	msg := govtypes.NewMsgSubmitProposal(proposal, minDeposit, addr)
+	require.NoError(suite.T(), suite.app.EvmKeeper.CheckMsgSubmitProposal(suite.ctx, msg))
+
+	testCases := []struct {
+		msg           string
+		malleate      func()
+		expectedError bool
+	}{
+		{
+			"pass check",
+			func() {},
+			false,
+		},
+		{
+			"try to add an address already exists in blocked list",
+			func() {
+				suite.stateDB.SetContractBlockedListMember(addr)
+			},
+			true,
+		},
+		{
+			"try to delete an address not in the blocked list",
+			func() {
+				proposal.IsAdded = false
+				proposal.ContractAddr = addrUnqualified
+			},
+			true,
+		},
+	}
+
+	for _, tc := range testCases {
+		suite.Run(tc.msg, func() {
+			tc.malleate()
+
+			err := suite.app.EvmKeeper.CheckMsgManageContractBlockedListProposal(suite.ctx, proposal)
+			if tc.expectedError {
+				suite.Require().Error(err)
+			} else {
+				suite.Require().NoError(err)
+			}
+		})
+	}
 }
