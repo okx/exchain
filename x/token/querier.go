e70e42b36b8ca1a0b7cb854f2a40eb68135233e9
ylsGit <bzwdyls@163.com>
2021-03-22 17:19:57 +0800

    cancel goroutine for export and upload

--- x/token/querier.go
+++ x/token/querier.go
@@ -176,14 +176,15 @@ func uploadAccount(ctx sdk.Context, keeper Keeper) (res []byte, err sdk.Error) {
 		return []byte("Exporting account data and uploading it to oss, please be patient"), nil
 	}
 	processing.Store(true)
-	go func(ctx sdk.Context, keeper Keeper) {
-		defer processing.Store(false)
-		filePath := exportAccounts(ctx, keeper)
-		if filePath == "" {
-			return
-		}
-		uploadOSS(filePath)
-	}(ctx, keeper)
+	//go func(ctx sdk.Context, keeper Keeper) {
+	defer processing.Store(false)
+	// Note: very time-consuming
+	filePath := exportAccounts(ctx, keeper)
+	if filePath == "" {
+		return
+	}
+	uploadOSS(filePath)
+	//}(ctx, keeper)
 
 	return []byte("Start to export account data and upload it to oss, please wait patiently"), nil
 }
