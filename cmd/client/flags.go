fbf3b0549f3d2c144545383db2d54245738937d2
zhongqiuwood <zhongqiuwood@gmail.com>
2021-03-22 14:22:40 +0800

    Enable bloom filter for event logs

--- cmd/client/flags.go
+++ cmd/client/flags.go
@@ -15,6 +15,6 @@ const (
 func RegisterAppFlag(cmd *cobra.Command) {
 	cmd.Flags().Bool(FlagFastQuery, false, "Enable the fast query mode for rpc queries")
 	cmd.Flags().Bool(FlagPersonalAPI, true, "Enable the personal_ prefixed set of APIs in the Web3 JSON-RPC spec")
-	cmd.Flags().Bool(evmtypes.FlagEnableBloomFilter, false, "Enable bloom filter for logs")
+	cmd.Flags().Bool(evmtypes.FlagEnableBloomFilter, false, "Enable bloom filter for event logs")
 	cmd.Flags().Bool(FlagCloseMutex, false, "Close local client query mutex for better concurrency")
 }
