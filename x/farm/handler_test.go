4676ec204088a1ff407ac856a510218f82214f7b
ylsGit <bzwdyls@163.com>
2021-03-31 17:42:25 +0800

    fix UT

--- x/farm/handler_test.go
+++ x/farm/handler_test.go
@@ -360,7 +360,7 @@ func TestHandlerMsgDestroyPool(t *testing.T) {
 				return destroyPoolMsg
 			},
 			verification: verification,
-			expectedErr:  types.ErrInvalidPoolOwner("ex1rf9wr069pt64e58f2w3mjs9w72g8vemzw26658", "abc"),
+			expectedErr:  types.ErrInvalidPoolOwner("ex15ky9du8a2wlstz6fpx3p4mqpjyrm5cgp83ahy9", "abc"),
 		},
 		{
 			caseName: "failed. insufficient fee coins",
@@ -718,7 +718,7 @@ func TestHandlerMsgUnlock(t *testing.T) {
 			},
 			getMsg:       normalGetUnlockMsg,
 			verification: verification,
-			expectedErr:  types.ErrNoLockInfoFound("ex1rf9wr069pt64e58f2w3mjs9w72g8vemzw26658", "abc"),
+			expectedErr:  types.ErrNoLockInfoFound("ex15ky9du8a2wlstz6fpx3p4mqpjyrm5cgq68fzeh", "abc"),
 		},
 		{
 			caseName: "failed. The coin name should be %s, not %s",
