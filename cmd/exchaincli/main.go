4e78321d888c48c16fa5257cb9148b249ff00bdb
meng <805442788@qq.com>
2021-04-19 14:45:26 +0800

    clean okexchain

---
+++ cmd/exchaincli/main.go
@@ -0,0 +1,144 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	tmamino "github.com/tendermint/tendermint/crypto/encoding/amino"
+	"github.com/tendermint/tendermint/crypto/multisig"
+	"github.com/tendermint/tendermint/libs/cli"
+
+	sdkclient "github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	clientkeys "github.com/cosmos/cosmos-sdk/client/keys"
+	clientrpc "github.com/cosmos/cosmos-sdk/client/rpc"
+	sdkcodec "github.com/cosmos/cosmos-sdk/codec"
+	"github.com/cosmos/cosmos-sdk/crypto/keys"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/version"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	authcmd "github.com/cosmos/cosmos-sdk/x/auth/client/cli"
+	"github.com/cosmos/cosmos-sdk/x/bank"
+	tokencmd "github.com/okex/exchain/x/token/client/cli"
+
+	"github.com/okex/exchain/app"
+	"github.com/okex/exchain/app/codec"
+	"github.com/okex/exchain/app/crypto/ethsecp256k1"
+	"github.com/okex/exchain/app/rpc"
+	exchain "github.com/okex/exchain/app/types"
+	"github.com/okex/exchain/cmd/client"
+)
+
+var (
+	cdc = codec.MakeCodec(app.ModuleBasics)
+)
+
+func main() {
+	// Configure cobra to sort commands
+	cobra.EnableCommandSorting = false
+
+	tmamino.RegisterKeyType(ethsecp256k1.PubKey{}, ethsecp256k1.PubKeyName)
+	tmamino.RegisterKeyType(ethsecp256k1.PrivKey{}, ethsecp256k1.PrivKeyName)
+	multisig.RegisterKeyType(ethsecp256k1.PubKey{}, ethsecp256k1.PubKeyName)
+
+	keys.CryptoCdc = cdc
+	clientkeys.KeysCdc = cdc
+
+	// Read in the configuration file for the sdk
+	config := sdk.GetConfig()
+	exchain.SetBech32Prefixes(config)
+	exchain.SetBip44CoinType(config)
+	config.Seal()
+
+	rootCmd := &cobra.Command{
+		Use:   "exchaincli",
+		Short: "Command line interface for interacting with exchaind",
+	}
+
+	// Add --chain-id to persistent flags and mark it required
+	rootCmd.PersistentFlags().String(flags.FlagChainID, "", "Chain ID of tendermint node")
+	rootCmd.PersistentPreRunE = func(_ *cobra.Command, _ []string) error {
+		return client.InitConfig(rootCmd)
+	}
+
+	// Construct Root Command
+	rootCmd.AddCommand(
+		clientrpc.StatusCommand(),
+		sdkclient.ConfigCmd(app.DefaultCLIHome),
+		queryCmd(cdc),
+		txCmd(cdc),
+		client.ValidateChainID(
+			rpc.ServeCmd(cdc),
+		),
+		flags.LineBreak,
+		client.KeyCommands(),
+		flags.LineBreak,
+		version.Cmd,
+		flags.NewCompletionCmd(rootCmd, true),
+	)
+
+	// Add flags and prefix all env exposed with EXCHAIN
+	executor := cli.PrepareMainCmd(rootCmd, "EXCHAIN", app.DefaultCLIHome)
+
+	err := executor.Execute()
+	if err != nil {
+		panic(fmt.Errorf("failed executing CLI command: %w", err))
+	}
+}
+
+func queryCmd(cdc *sdkcodec.Codec) *cobra.Command {
+	queryCmd := &cobra.Command{
+		Use:     "query",
+		Aliases: []string{"q"},
+		Short:   "Querying subcommands",
+	}
+
+	queryCmd.AddCommand(
+		authcmd.GetAccountCmd(cdc),
+		flags.LineBreak,
+		authcmd.QueryTxsByEventsCmd(cdc),
+		authcmd.QueryTxCmd(cdc),
+		flags.LineBreak,
+	)
+
+	// add modules' query commands
+	app.ModuleBasics.AddQueryCommands(queryCmd, cdc)
+
+	return queryCmd
+}
+
+func txCmd(cdc *sdkcodec.Codec) *cobra.Command {
+	txCmd := &cobra.Command{
+		Use:   "tx",
+		Short: "Transactions subcommands",
+	}
+
+	txCmd.AddCommand(
+		tokencmd.SendTxCmd(cdc),
+		flags.LineBreak,
+		authcmd.GetSignCommand(cdc),
+		authcmd.GetMultiSignCommand(cdc),
+		flags.LineBreak,
+		authcmd.GetBroadcastCommand(cdc),
+		authcmd.GetEncodeCommand(cdc),
+		authcmd.GetDecodeCommand(cdc),
+		flags.LineBreak,
+	)
+
+	// add modules' tx commands
+	app.ModuleBasics.AddTxCommands(txCmd, cdc)
+
+	// remove auth and bank commands as they're mounted under the root tx command
+	var cmdsToRemove []*cobra.Command
+
+	for _, cmd := range txCmd.Commands() {
+		if cmd.Use == auth.ModuleName || cmd.Use == bank.ModuleName {
+			cmdsToRemove = append(cmdsToRemove, cmd)
+		}
+	}
+
+	txCmd.RemoveCommand(cmdsToRemove...)
+
+	return txCmd
+}
---
+++ cmd/exchaind/rest.go
@@ -0,0 +1,86 @@
+package main
+
+import (
+	"fmt"
+	evmclient "github.com/okex/exchain/x/evm/client"
+
+	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/lcd"
+	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/x/auth"
+	authrest "github.com/cosmos/cosmos-sdk/x/auth/client/rest"
+	bankrest "github.com/cosmos/cosmos-sdk/x/bank/client/rest"
+	supplyrest "github.com/cosmos/cosmos-sdk/x/supply/client/rest"
+	"github.com/okex/exchain/app/rpc"
+	"github.com/okex/exchain/app/types"
+	ammswaprest "github.com/okex/exchain/x/ammswap/client/rest"
+	backendrest "github.com/okex/exchain/x/backend/client/rest"
+	dexclient "github.com/okex/exchain/x/dex/client"
+	dexrest "github.com/okex/exchain/x/dex/client/rest"
+	dist "github.com/okex/exchain/x/distribution"
+	distr "github.com/okex/exchain/x/distribution"
+	distrest "github.com/okex/exchain/x/distribution/client/rest"
+	evmrest "github.com/okex/exchain/x/evm/client/rest"
+	farmclient "github.com/okex/exchain/x/farm/client"
+	farmrest "github.com/okex/exchain/x/farm/client/rest"
+	govrest "github.com/okex/exchain/x/gov/client/rest"
+	orderrest "github.com/okex/exchain/x/order/client/rest"
+	paramsclient "github.com/okex/exchain/x/params/client"
+	stakingrest "github.com/okex/exchain/x/staking/client/rest"
+	"github.com/okex/exchain/x/token"
+	tokensrest "github.com/okex/exchain/x/token/client/rest"
+	"github.com/spf13/viper"
+)
+
+// registerRoutes registers the routes from the different modules for the LCD.
+// NOTE: details on the routes added for each module are in the module documentation
+// NOTE: If making updates here you also need to update the test helper in client/lcd/test_helper.go
+func registerRoutes(rs *lcd.RestServer) {
+	rpc.RegisterRoutes(rs)
+	pathPrefix := viper.GetString(server.FlagRestPathPrefix)
+	if pathPrefix == "" {
+		pathPrefix = types.EthBech32Prefix
+	}
+	registerRoutesV1(rs, pathPrefix)
+	registerRoutesV2(rs, pathPrefix)
+}
+
+func registerRoutesV1(rs *lcd.RestServer, pathPrefix string) {
+	v1Router := rs.Mux.PathPrefix(fmt.Sprintf("/%s/v1", pathPrefix)).Name("v1").Subrouter()
+	client.RegisterRoutes(rs.CliCtx, v1Router)
+	authrest.RegisterRoutes(rs.CliCtx, v1Router, auth.StoreKey)
+	bankrest.RegisterRoutes(rs.CliCtx, v1Router)
+	stakingrest.RegisterRoutes(rs.CliCtx, v1Router)
+	distrest.RegisterRoutes(rs.CliCtx, v1Router, dist.StoreKey)
+
+	orderrest.RegisterRoutes(rs.CliCtx, v1Router)
+	tokensrest.RegisterRoutes(rs.CliCtx, v1Router, token.StoreKey)
+	backendrest.RegisterRoutes(rs.CliCtx, v1Router)
+	dexrest.RegisterRoutes(rs.CliCtx, v1Router)
+	ammswaprest.RegisterRoutes(rs.CliCtx, v1Router)
+	supplyrest.RegisterRoutes(rs.CliCtx, v1Router)
+	farmrest.RegisterRoutes(rs.CliCtx, v1Router)
+	evmrest.RegisterRoutes(rs.CliCtx, v1Router)
+	govrest.RegisterRoutes(rs.CliCtx, v1Router,
+		[]govrest.ProposalRESTHandler{
+			paramsclient.ProposalHandler.RESTHandler(rs.CliCtx),
+			distr.ProposalHandler.RESTHandler(rs.CliCtx),
+			dexclient.DelistProposalHandler.RESTHandler(rs.CliCtx),
+			farmclient.ManageWhiteListProposalHandler.RESTHandler(rs.CliCtx),
+			evmclient.ManageContractDeploymentWhitelistProposalHandler.RESTHandler(rs.CliCtx),
+		},
+	)
+}
+
+func registerRoutesV2(rs *lcd.RestServer, pathPrefix string) {
+	v2Router := rs.Mux.PathPrefix(fmt.Sprintf("/%s/v2", pathPrefix)).Name("v1").Subrouter()
+	client.RegisterRoutes(rs.CliCtx, v2Router)
+	authrest.RegisterRoutes(rs.CliCtx, v2Router, auth.StoreKey)
+	bankrest.RegisterRoutes(rs.CliCtx, v2Router)
+	stakingrest.RegisterRoutes(rs.CliCtx, v2Router)
+	distrest.RegisterRoutes(rs.CliCtx, v2Router, dist.StoreKey)
+
+	orderrest.RegisterRoutesV2(rs.CliCtx, v2Router)
+	tokensrest.RegisterRoutesV2(rs.CliCtx, v2Router, token.StoreKey)
+	backendrest.RegisterRoutesV2(rs.CliCtx, v2Router)
+}
