544a3efe6c0dea05b1bc209e6bedab202d36a031
KamiD <44460798+KamiD@users.noreply.github.com>
2021-04-20 17:24:08 +0800

    Merge PR: set the default gas price of eth_call to 1 Gwei (#825)

--- app/types/params.go
+++ app/types/params.go
@@ -2,7 +2,7 @@ package types
 
 const (
 	// DefaultGasPrice is default gas price for evm transactions
-	DefaultGasPrice = 20
+	DefaultGasPrice = 1
 	// DefaultRPCGasLimit is default gas limit for RPC call operations
 	DefaultRPCGasLimit = 30000000
 )
