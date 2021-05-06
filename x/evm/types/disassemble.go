3a9bd66799a6f6e451e4bfa467f3a88b8d35f5ce
KamiD <yzglwt@sina.com>
2021-05-06 14:23:59 +0800

    add opcode delegatecall\callcode\log0~4

--- x/evm/types/disassemble.go
+++ x/evm/types/disassemble.go
@@ -9,7 +9,7 @@ func IsContractReadonly(code []byte) bool {
 	it := asm.NewInstructionIterator(code)
 	for it.Next() {
 		switch it.Op() {
-		case vm.SSTORE, vm.CREATE, vm.CREATE2:
+		case vm.SSTORE, vm.CREATE, vm.CREATE2, vm.DELEGATECALL, vm.CALLCODE, vm.LOG0, vm.LOG1, vm.LOG2, vm.LOG3, vm.LOG4:
 			return false
 		}
 	}
