4a7df7cb77b51073fef4f3cdd47786ee778895f1
meng <805442788@qq.com>
2021-05-27 17:50:26 +0800

    add config hbase watcher for exchaincli rest-server

--- app/rpc/cmd.go
+++ app/rpc/cmd.go
@@ -4,6 +4,7 @@ import (
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/lcd"
 	"github.com/cosmos/cosmos-sdk/codec"
+	"github.com/okex/exchain/x/evm/watcher"
 	"github.com/spf13/cobra"
 )
 
@@ -14,5 +15,11 @@ func ServeCmd(cdc *codec.Codec) *cobra.Command {
 	cmd.Flags().String(flagUnlockKey, "", "Select a key to unlock on the RPC server")
 	cmd.Flags().String(flagWebsocket, "8546", "websocket port to listen to")
 	cmd.Flags().StringP(flags.FlagBroadcastMode, "b", flags.BroadcastSync, "Transaction broadcasting mode (sync|async|block)")
+
+	cmd.Flags().Bool(watcher.FlagFastQuery, false, "Enable the fast query mode for rpc queries")
+	cmd.Flags().String(watcher.FlagWatcherDisLockUrl, "redis://127.0.0.1:6379", "config watcher dis lock url")
+	cmd.Flags().String(watcher.FlagWatcherDisLockUrlPassword, "", "config watcher dis lock password")
+	cmd.Flags().String(watcher.FlagHbaseDBUrl, "", "config watcher hbase db url")
+
 	return cmd
 }
