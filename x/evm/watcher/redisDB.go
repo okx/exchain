5b2c90241f72f08a879d96288fcffa15ac0ecf83
shaoyun.zhan@okcoin.com <shaoyun.zhan@okcoin.com>
2021-05-31 15:38:19 +0800

    add redis pool

--- x/evm/watcher/redisDB.go
+++ x/evm/watcher/redisDB.go
@@ -2,40 +2,65 @@ package watcher
 
 import (
 	"encoding/hex"
-	"github.com/go-redis/redis"
+	"fmt"
+	"github.com/garyburd/redigo/redis"
+	"github.com/okex/exchain/x/stream/common"
+	"github.com/tendermint/tendermint/libs/log"
+	"os"
 )
 
 type RedisDB struct {
-	db *redis.Client
+	db     *redis.Pool
+	logger log.Logger
 }
 
 func initRedisDB(dbUrl string, dbPassword string) *RedisDB {
-	client := redis.NewClient(&redis.Options{
-		Addr:     dbUrl,
-		Password: dbPassword,
-	})
-
-	return &RedisDB{db: client}
+	logger := log.NewTMLogger(log.NewSyncWriter(os.Stdout))
+	pool, err := common.NewPool(dbUrl, dbPassword, logger)
+	if err != nil {
+		panic(err)
+	}
+	return &RedisDB{db: pool, logger: logger}
 }
 
 func (db *RedisDB) Set(key []byte, value []byte) {
-	db.db.Set(hex.EncodeToString(key), hex.EncodeToString(value), 0).Result()
+	conn := db.db.Get()
+	defer conn.Close()
+
+	_, err := redis.String(conn.Do("SET", hex.EncodeToString(key), hex.EncodeToString(value)))
+	if nil != err {
+		db.logger.Error(fmt.Sprintf("redis: trying to set key(%s) with value(%s), err(%+v)", hex.EncodeToString(key), hex.EncodeToString(value), err))
+	}
 }
 
 func (db *RedisDB) Get(key []byte) ([]byte, error) {
-	result, err := db.db.Get(hex.EncodeToString(key)).Result()
-	if err != nil {
-		return nil, err
+	conn := db.db.Get()
+	defer conn.Close()
+
+	result, err := redis.String(conn.Do("GET", hex.EncodeToString(key)))
+	if nil != err {
+		db.logger.Error(fmt.Sprintf("redis: trying to get key(%s) , err(%+v)", hex.EncodeToString(key), err))
 	}
-	bz, err := hex.DecodeString(result)
-	return bz, err
+	return hex.DecodeString(result)
 }
 
 func (db *RedisDB) Delete(key []byte) {
-	db.db.Del(hex.EncodeToString(key)).Result()
+	conn := db.db.Get()
+	defer conn.Close()
+
+	_, err := redis.Bool(conn.Do("DEL", hex.EncodeToString(key)))
+	if nil != err {
+		db.logger.Error(fmt.Sprintf("redis: trying to del key(%s) , err(%+v)", hex.EncodeToString(key), err))
+	}
 }
 
 func (db *RedisDB) Has(key []byte) bool {
-	result, _ := db.db.Exists(hex.EncodeToString(key)).Result()
-	return result > 0
+	conn := db.db.Get()
+	defer conn.Close()
+
+	result, err := redis.Bool(conn.Do("EXISTS", hex.EncodeToString(key)))
+	if nil != err {
+		db.logger.Error(fmt.Sprintf("redis: trying to exits key(%s) , err(%+v)", hex.EncodeToString(key), err))
+	}
+	return result
 }
