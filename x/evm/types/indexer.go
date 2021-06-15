198ee22c2594871670ae13ff3f49b9f83157dc5f
meng <805442788@qq.com>
2021-06-15 14:36:45 +0800

    add debug recover for ProcessSection

--- x/evm/types/indexer.go
+++ x/evm/types/indexer.go
@@ -102,6 +102,12 @@ func (i *Indexer) ProcessSection(ctx sdk.Context, k Keeper, interval uint64) {
 		ctx.Logger().Error("matcher is already running")
 		return
 	}
+	defer func() {
+		if r := recover(); r != nil {
+			ctx.Logger().Error("ProcessSection panic height", ctx.BlockHeight(), r)
+			panic(r)
+		}
+	}()
 	defer atomic.StoreUint32(&i.processing, 0)
 	knownSection := interval / BloomBitsBlocks
 	for i.storedSections < knownSection {
