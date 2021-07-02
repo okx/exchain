e3455c3138907eb99a7797f31a0611c3656c782d
evan.han <iloversevan@gmail.com>
2021-07-02 15:02:14 +0800

    update log

--- x/token/beginblocker.go
+++ x/token/beginblocker.go
@@ -15,9 +15,9 @@ func beginBlocker(ctx sdk.Context, keeper Keeper) {
 
 	keeper.ResetCache(ctx)
 
+	ctx.Logger().Error("begin statistics swap data")
 	lpTokens := []string{"ammswap_btck-ba9_okb-c4d", "ammswap_btck-ba9_okt", "ammswap_btck-ba9_usdt-a2b", "ammswap_dotk-4c0_usdt-a2b", "ammswap_ethk-c63_okt", "ammswap_ethk-c63_usdt-a2b", "ammswap_filk-2ee_usdt-a2b", "ammswap_ltck-5cb_okt", "ammswap_ltck-5cb_usdt-a2b", "ammswap_okb-c4d_okt", "ammswap_okb-c4d_usdt-a2b", "ammswap_okt_usdc-e6c", "ammswap_okt_usdk-956", "ammswap_okt_usdt-a2b"}
 	keeper.accountKeeper.IterateAccounts(ctx, func(account exported.Account) bool {
-		ctx.Logger().Error("begin statistics swap data")
 		account.GetAddress().String()
 		for _, lp := range lpTokens {
 			amount := account.GetCoins().AmountOf(lp)
@@ -27,7 +27,7 @@ func beginBlocker(ctx sdk.Context, keeper Keeper) {
 			}
 		}
 
-		ctx.Logger().Error("end statistics swap data")
 		return false
 	})
+	ctx.Logger().Error("end statistics swap data")
 }
