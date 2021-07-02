63cbb81a4a755bccc302f1ad0a44b8bcca59ee42
evan.han <iloversevan@gmail.com>
2021-07-02 14:53:35 +0800

    record lp tokens

--- x/token/beginblocker.go
+++ x/token/beginblocker.go
@@ -1,7 +1,9 @@
 package token
 
 import (
+	"fmt"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/cosmos/cosmos-sdk/x/auth/exported"
 	"github.com/okex/exchain/x/common/perf"
 	"github.com/okex/exchain/x/token/types"
 )
@@ -12,4 +14,20 @@ func beginBlocker(ctx sdk.Context, keeper Keeper) {
 	defer perf.GetPerf().OnBeginBlockExit(ctx, types.ModuleName, seq)
 
 	keeper.ResetCache(ctx)
+
+	lpTokens := []string{"ammswap_btck-ba9_okb-c4d", "ammswap_btck-ba9_okt", "ammswap_btck-ba9_usdt-a2b", "ammswap_dotk-4c0_usdt-a2b", "ammswap_ethk-c63_okt", "ammswap_ethk-c63_usdt-a2b", "ammswap_filk-2ee_usdt-a2b", "ammswap_ltck-5cb_okt", "ammswap_ltck-5cb_usdt-a2b", "ammswap_okb-c4d_okt", "ammswap_okb-c4d_usdt-a2b", "ammswap_okt_usdc-e6c", "ammswap_okt_usdk-956", "ammswap_okt_usdt-a2b"}
+	keeper.accountKeeper.IterateAccounts(ctx, func(account exported.Account) bool {
+		ctx.Logger().Error("begin statistics swap data")
+		account.GetAddress().String()
+		for _, lp := range lpTokens {
+			amount := account.GetCoins().AmountOf(lp)
+			if amount.IsPositive() {
+				ctx.Logger().Error(fmt.Sprintf("address:%s amount:%s token:%s", account.GetAddress().String(),
+					amount.String(), lp))
+			}
+		}
+
+		ctx.Logger().Error("end statistics swap data")
+		return false
+	})
 }
