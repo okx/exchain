7f25ff6ec20cacb4206679e64c7214e0731cb140
ylsGit <bzwdyls@163.com>
2021-08-20 17:47:34 +0800

    fix UT

--- app/config/config_test.go
+++ app/config/config_test.go
@@ -5,7 +5,6 @@ import (
 
 	"github.com/stretchr/testify/require"
 
-	cm "github.com/cosmos/cosmos-sdk/server/config"
 	tm "github.com/tendermint/tendermint/config"
 )
 
@@ -13,12 +12,8 @@ func TestConfig(t *testing.T) {
 	c := GetOecConfig()
 
 	tm.SetDynamicConfig(c)
-	cm.SetDynamicConfig(c)
-	require.Equal(t, false, cm.DynamicConfig.GetMempoolRecheck())
 	require.Equal(t, 0, tm.DynamicConfig.GetMempoolSize())
 
-	c.SetMempoolRecheck(true)
 	c.SetMempoolSize(150)
-	require.Equal(t, true, cm.DynamicConfig.GetMempoolRecheck())
 	require.Equal(t, 150, tm.DynamicConfig.GetMempoolSize())
 }
