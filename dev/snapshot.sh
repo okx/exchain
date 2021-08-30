d7028cacb4bb1a4a4158109fa672a41e74285d2a
liang xiong <liang.xiong@okg.com>
2021-08-31 02:43:29 +0800

    fix bug of s1, execute one more cmd

--- dev/snapshot.sh
+++ dev/snapshot.sh
@@ -3,6 +3,7 @@
 HOME=$1
 VERSION=$2
 CMD=exchaind
+NUM_EXECUTIONS=2
 
 set -e
 set -o errexit
@@ -10,14 +11,14 @@ set -a
 set -m
 
 killbyname() {
-  NAME=$1
-  ps -ef|grep "$NAME"|grep -v grep |awk '{print "kill -9 "$2", "$8}'
-  ps -ef|grep "$NAME"|grep -v grep |awk '{print "kill -9 "$2}' | sh
-  echo "All <$NAME> killed!"
+    NAME=$1
+    ps -ef|grep "$NAME"|grep -v grep |awk '{print "kill -9 "$2", "$8}'
+    ps -ef|grep "$NAME"|grep -v grep |awk '{print "kill -9 "$2}' | sh
+    echo "All <$NAME> killed!"
 }
 
 
-# killbyname exchaind
+killbyname exchaind
 killbyname exchaincli
 
 set -x # activate debugging
@@ -46,12 +47,20 @@ echo using $VERSION mode
 
 if [ "$VERSION" = "s0" ];
 then
-    $CMD data prune-compact all --home $HOME
+    for (( i=0; i<NUM_EXECUTIONS; i++ ))
+    do
+        echo runing $i-th times...
+        $CMD data prune-compact all --home $HOME
+    done
     rm -rf $HOME/data/cs.wal
     rm -rf $HOME/data/tx_index.db
     rm -rf $HOME/data/evidence.db
     rm -rf $HOME/data/watch.db
 else
-    $CMD data prune-compact state -p=false --home $HOME
+    for (( i=0; i<NUM_EXECUTIONS; i++ ))
+    do
+        echo runing $i-th times...
+        $CMD data prune-compact state --home $HOME
+    done
     rm -rf $HOME/data/watch.db
 fi
