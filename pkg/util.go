e3d2f1908229cac3061e1ff0ba19c17910e46e38
stevenyangek9 <165165164@qq.com>
2021-09-27 11:49:46 +0800

    feature : evm commitstatedb 统计细化功能

--- pkg/util.go
+++ pkg/util.go
@@ -3,25 +3,15 @@ package pkg
 import (
 	"runtime"
 	"time"
-
-	//"github.com/satori/go.uuid"
 )
 
-
-func RunFuncName()string{
-	pc := make([]uintptr,1)
-	runtime.Callers(2,pc)
+func RunFuncName() string {
+	pc := make([]uintptr, 1)
+	runtime.Callers(2, pc)
 	f := runtime.FuncForPC(pc[0])
 	return f.Name()
 }
 
-/*
-func UniqIndex() string{
-	id := uuid.NewV4()
-	ids := id.String()
-	return ids
-}*/
-
-func GetNowTimeMs() int64{
+func GetNowTimeMs() int64 {
 	return time.Now().UnixNano() / 1e6
 }
