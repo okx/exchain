a168b260447c19e749140d7a1195b2716229941f
stevenyangek9 <165165164@qq.com>
2021-10-09 13:18:58 +0800

    Timestamp log (#1062)
    * test
    
    * feature : 增加log 并埋点
    
    * fix log
    
    * add logger
    
    * merger dev
    
    * merger dev
    
    * fix build
    
    * modify analyser,  add function for tendermint
    
    * add log format
    
    * add log fprmat
    
    * modify format print
    
    * go mode
    
    * e.infoMap["Evm"]
    
    * upd
    
    * last modify
    
    * format tm const
    
    * format tm const
    
    * format tm const
    
    * format tm const
    
    * use tm  commit replace tag
    
    Co-authored-by: oker <oker@okerdeMacBook-Pro.local>
    Co-authored-by: KamiD <44460798+KamiD@users.noreply.github.com>
    Co-authored-by: zhongqiuwood <zhongqiuwood@gmail.com>
    Co-authored-by: Zhong Qiu <36867992+zhongqiuwood@users.noreply.github.com>

--- app/elapse_info.go
+++ app/elapse_info.go
@@ -42,7 +42,7 @@ func (e *ElapsedTimeInfos) Dump(logger log.Logger) {
 		trace.Tx, e.infoMap[trace.Tx],
 		trace.GasUsed, e.infoMap[trace.GasUsed],
 		trace.RunTx, e.infoMap[trace.RunTx],
-		"Evm", e.infoMap["Evm"],
+		trace.Evm, e.infoMap[trace.Evm],
 		"DB", e.infoMap["DB"],
 		trace.Round, e.infoMap[trace.Round],
 		trace.CommitRound, e.infoMap[trace.CommitRound],
