3626ebc0f8f82ab5ec2381d9262697a6b4ad5fc4
zhongqiuwood <zhongqiuwood@gmail.com>
2021-10-15 07:29:01 +0800

    rm exchainDeliverTx

--- x/analyzer/analyzer.go
+++ x/analyzer/analyzer.go
@@ -225,6 +225,6 @@ func (s *analyer) format() {
 
 	trace.GetElapsedInfo().AddInfo(trace.Evm, fmt.Sprintf(EVM_FORMAT, s.dbRead, s.dbWrite, evmcore-s.dbRead-s.dbWrite))
 
-	format += fmt.Sprintf("%s<%dms>", "exchainDeliverTx", s.delliverTxCost)
+	//format += fmt.Sprintf("%s<%dms>", "exchainDeliverTx", s.delliverTxCost)
 	trace.GetElapsedInfo().AddInfo("DeliverTxs", format)
 }
