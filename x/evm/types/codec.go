929b21f9d8989b051542326cee1d0a000085db74
cwbhhjl <cwbhhjl@gmail.com>
2021-11-02 13:34:14 +0800

    register ChainConfig unmarshaller & test

--- x/evm/types/codec.go
+++ x/evm/types/codec.go
@@ -1,7 +1,11 @@
 package types
 
 import (
+	"fmt"
+
 	"github.com/okex/exchain/libs/cosmos-sdk/codec"
+	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
+	"github.com/tendermint/go-amino"
 )
 
 // ModuleCdc defines the evm module's codec
@@ -16,10 +20,130 @@ func RegisterCodec(cdc *codec.Codec) {
 	cdc.RegisterConcrete(ChainConfig{}, "ethermint/ChainConfig", nil)
 	cdc.RegisterConcrete(ManageContractDeploymentWhitelistProposal{}, "okexchain/evm/ManageContractDeploymentWhitelistProposal", nil)
 	cdc.RegisterConcrete(ManageContractBlockedListProposal{}, "okexchain/evm/ManageContractBlockedListProposal", nil)
+
+	cdc.RegisterConcreteUnmarshaller("ethermint/ChainConfig", func(c *amino.Codec, bytes []byte) (interface{}, int, error) {
+		return UnmarshalChainConfigFromAmino(c, bytes)
+	})
 }
 
 func init() {
 	RegisterCodec(ModuleCdc)
 	codec.RegisterCrypto(ModuleCdc)
 	ModuleCdc.Seal()
+}
+
+// UnmarshalChainConfigFromAmino unmarshal a ChainConfig from an amino encoded byte slice
+func UnmarshalChainConfigFromAmino(_ *amino.Codec, data []byte) (*ChainConfig, int, error) {
+	var dataLen uint64 = 0
+	var read int
+	var err error
+	config := &ChainConfig{}
+
+	for {
+		data = data[dataLen:]
+		read += int(dataLen)
+
+		if len(data) <= 0 {
+			break
+		}
+
+		pos, _ := amino.ParseProtoPosAndTypeMustOneByte(data[0])
+		data = data[1:]
+		read += 1
+
+		var n int
+		dataLen, n, err = amino.DecodeUvarint(data)
+		if err != nil {
+			return nil, read, err
+		}
+
+		data = data[n:]
+		read += n
+		subData := data[:dataLen]
+
+		switch pos {
+		case 1:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.HomesteadBlock = integer
+		case 2:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.DAOForkBlock = integer
+		case 3:
+			if len(subData) != 1 {
+				return nil, read, fmt.Errorf("invalid DAO fork support flag")
+			}
+			config.DAOForkSupport = subData[0] == 1
+		case 4:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.EIP150Block = integer
+		case 5:
+			config.EIP150Hash = string(subData)
+		case 6:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.EIP155Block = integer
+		case 7:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.EIP158Block = integer
+		case 8:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.ByzantiumBlock = integer
+		case 9:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.ConstantinopleBlock = integer
+		case 10:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.PetersburgBlock = integer
+		case 11:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.IstanbulBlock = integer
+		case 12:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.MuirGlacierBlock = integer
+		case 13:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.YoloV2Block = integer
+		case 14:
+			integer, err := sdk.NewIntFromAmino(subData)
+			if err != nil {
+				return nil, read, err
+			}
+			config.EWASMBlock = integer
+		default:
+			return nil, read, fmt.Errorf("unexpect feild num %d", pos)
+		}
+	}
+	return config, read, nil
 }
