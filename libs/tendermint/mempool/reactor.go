5e7ba3c17a1f20dc7ef449f93affe1e8a4798fc9
stevenyangek9 <165165164@qq.com>
2021-11-02 16:06:52 +0800

    modify log print

--- libs/tendermint/mempool/reactor.go
+++ libs/tendermint/mempool/reactor.go
@@ -240,7 +240,7 @@ func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
 		// 如果peer 落后太多区块就不发送该tx
 		if peerState.GetHeight() < memTx.Height()-1 { // Allow for a lag of 1 block
 			GetGlobalRecord(memR.Logger).AddPeer(peer, false, memTx.Height()-1, peerState.GetHeight())
-			memR.Logger.Error("Error height: %s", GetGlobalRecord(memR.Logger).Detail())
+			memR.Logger.Error(fmt.Sprintf("Error height: %s", GetGlobalRecord(memR.Logger).Detail()))
 			time.Sleep(peerCatchupSleepIntervalMS * time.Millisecond)
 			continue
 		}
@@ -252,7 +252,7 @@ func (memR *Reactor) broadcastTxRoutine(peer p2p.Peer) {
 			success := peer.Send(MempoolChannel, cdc.MustMarshalBinaryBare(msg))
 			if !success {
 				GetGlobalRecord(memR.Logger).AddPeer(peer, false, memTx.Height()-1, peerState.GetHeight())
-				memR.Logger.Error("Error peer.Send: %s", GetGlobalRecord(memR.Logger).Detail())
+				memR.Logger.Error(fmt.Sprintf("Error peer.Send: %s", GetGlobalRecord(memR.Logger).Detail()))
 				time.Sleep(peerCatchupSleepIntervalMS * time.Millisecond)
 				continue
 			}
