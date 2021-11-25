669a1d41a331ccbb53f189e148230c6b669c5e23
ylsGit <bzwdyls@163.com>
2021-11-25 19:44:43 +0800

    Update dynamic_config_okchain.go

--- libs/iavl/config/dynamic_config_okchain.go
+++ libs/iavl/config/dynamic_config_okchain.go
@@ -4,8 +4,15 @@ type IDynamicConfig interface {
 	GetIavlCacheSize() int
 }
 
-var DynamicConfig IDynamicConfig
+var DynamicConfig IDynamicConfig = MockDynamicConfig{}
 
 func SetDynamicConfig(c IDynamicConfig) {
 	DynamicConfig = c
+}
+
+type MockDynamicConfig struct {
+}
+
+func (d MockDynamicConfig) GetIavlCacheSize() int {
+	return 10000
 }
