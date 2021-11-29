921169bdf1ade8b2d0e6c871b6fc75560919196b
KamiD <44460798+KamiD@users.noreply.github.com>
2021-11-29 16:08:14 +0800

    add flag of max gas used per block to replay

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -92,6 +92,7 @@ func replayCmd(ctx *server.Context) *cobra.Command {
 	cmd.Flags().Bool(runWithPprofFlag, false, "Dump the pprof of the entire replay process")
 	cmd.Flags().Bool(sm.FlagParalleledTx, false, "pall Tx")
 	cmd.Flags().Bool(saveBlock, false, "save block when replay")
+	cmd.Flags().Int64(config.FlagMaxGasUsedPerBlock, -1, "Maximum gas used of transactions in a block")
 	return cmd
 }
 
