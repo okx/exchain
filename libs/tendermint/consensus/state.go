047b3b394c029008f37f18507812fc13d362831d
stevenyangek9 <165165164@qq.com>
2021-12-01 16:38:41 +0800

    merge dev

--- libs/tendermint/consensus/state.go
+++ libs/tendermint/consensus/state.go
@@ -1820,6 +1820,7 @@ func (cs *State) addProposalBlockPart(msg *BlockPartMessage, peerID p2p.ID) (add
 				cs.ValidRound = cs.Round
 				cs.ValidBlock = cs.ProposalBlock
 				cs.ValidBlockParts = cs.ProposalBlockParts
+				// 1. start pre block execute
 				cs.blockExec.StartPreExecBlock(cs.ProposalBlock)
 			}
 			// TODO: In case there is +2/3 majority in Prevotes set for some
@@ -1985,6 +1986,7 @@ func (cs *State) addVote(
 					cs.ValidRound = vote.Round
 					cs.ValidBlock = cs.ProposalBlock
 					cs.ValidBlockParts = cs.ProposalBlockParts
+					// 2. start pre block execute
 					cs.blockExec.StartPreExecBlock(cs.ProposalBlock)
 				} else {
 					cs.Logger.Info(
