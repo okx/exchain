0260a2b13b83e64fba9b089137aff5131b4c8549
Leo <335209779@qq.com>
2021-12-08 16:39:29 +0800

    Update nodedb_oec.go
    parallel update branch, but fatal error of “concurrent map writes” has not been fixed

--- libs/iavl/nodedb_oec.go
+++ libs/iavl/nodedb_oec.go
@@ -3,7 +3,6 @@ package iavl
 import (
 	"bytes"
 	"container/list"
-	"encoding/hex"
 	"fmt"
 	"sync/atomic"
 	"time"
@@ -297,27 +296,49 @@ func (ndb *nodeDB) NewBatch() dbm.Batch {
 }
 
 func (ndb *nodeDB) updateBranch(node *Node, savedNodes map[string]*Node) []byte {
-	if node.persisted || node.prePersisted {
-		return node.hash
+	// TODO: find the optimal concurrent depth
+	defaultConcurDepth := 4
+	var updateBranchConcur func(node *Node, savedNodes map[string]*Node, concurDepth int, res chan []byte)
+	updateBranchConcur = func(node *Node, savedNodes map[string]*Node, concurDepth int, res chan []byte) {
+		if node == nil {
+			res <- []byte{}
+			return
+		}
+
+		if node.persisted || node.prePersisted {
+			res <- node.hash
+			return
+		}
+
+		// We can use channels to communicate between non-concurrent code,
+		// but we have to make sure the channel doesn't block.
+		leftChan := make(chan []byte, 1)
+		rightChan := make(chan []byte, 1)
+		// We need to get the results on a channel,
+		// and can re-use the same logic without goroutines for the sequential calls.
+		if concurDepth > 0 {
+			go updateBranchConcur(node.leftNode, savedNodes, concurDepth-1, leftChan)
+			go updateBranchConcur(node.rightNode, savedNodes, concurDepth-1, rightChan)
+		} else {
+			updateBranchConcur(node.leftNode, savedNodes, concurDepth-1, leftChan)
+			updateBranchConcur(node.rightNode, savedNodes, concurDepth-1, rightChan)
+		}
+		node.leftHash = <-leftChan
+		node.rightHash = <-rightChan
+		node._hash()
+		ndb.saveNodeToPrePersistCache(node)
+
+		node.leftNode = nil
+		node.rightNode = nil
+
+		// TODO: handle magic number
+		// TODO：fix the fatal error: concurrent map writes
+		//savedNodes[hex.EncodeToString(node.hash)] = node
+		res <- node.hash
 	}
-
-	if node.leftNode != nil {
-		node.leftHash = ndb.updateBranch(node.leftNode, savedNodes)
-	}
-	if node.rightNode != nil {
-		node.rightHash = ndb.updateBranch(node.rightNode, savedNodes)
-	}
-
-	node._hash()
-	ndb.saveNodeToPrePersistCache(node)
-
-	node.leftNode = nil
-	node.rightNode = nil
-
-	// TODO: handle magic number
-	savedNodes[hex.EncodeToString(node.hash)] = node
-
-	return node.hash
+	rootHash := make(chan []byte, 1) // must not block
+	updateBranchConcur(node, savedNodes, defaultConcurDepth, rootHash)
+	return <-rootHash
 }
 
 func (ndb *nodeDB) getRootWithCache(version int64) ([]byte, error) {
