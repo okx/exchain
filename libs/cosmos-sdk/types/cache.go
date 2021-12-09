9c6db8f472cb265ac05b20d0abb53ba0b14ccf10
chunfengSun <516108736@qq.com>
2021-12-09 19:12:46 +0800

    Update cache.go

--- libs/cosmos-sdk/types/cache.go
+++ libs/cosmos-sdk/types/cache.go
@@ -12,8 +12,8 @@ import (
 var (
 	maxAccInMap        = 100000
 	deleteAccCount     = 10000
-	maxStorageInMap    = 50000
-	deleteStorageCount = 5000
+	maxStorageInMap    = 110000
+	deleteStorageCount = 10000
 )
 var (
 	FlagMultiCache = "multi-cache"
