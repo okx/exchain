73d11fd65d4ac1aba56d9d975a91269c3876597d
zhongqiuwood <zhongqiuwood@gmail.com>
2021-12-20 09:32:44 +0800

    fix simulate bug

--- libs/cosmos-sdk/baseapp/baseapp_mode_handler.go
+++ libs/cosmos-sdk/baseapp/baseapp_mode_handler.go
@@ -1,8 +1,8 @@
 package baseapp
 
 import (
-	"fmt"
 	"encoding/json"
+	"fmt"
 
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	sdkerrors "github.com/okex/exchain/libs/cosmos-sdk/types/errors"
@@ -75,6 +75,11 @@ func (m *modeHandlerSimulate) handleStartHeight(info *runTxInfo, height int64) e
 	var err error
 	if height > startHeight && height < app.LastBlockHeight() {
 		info.ctx, err = app.getContextForSimTx(info.txBytes, height)
+	} else if height < startHeight && height != 0 {
+		err = sdkerrors.Wrap(sdkerrors.ErrInvalidRequest,
+			fmt.Sprintf("height(%d) should be greater than start block height(%d)", height, startHeight))
+	} else {
+		info.ctx = app.getContextForTx(m.mode, info.txBytes)
 	}
 
 	return err
