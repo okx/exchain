d66a659a3d850ba2db6c0957e800344a312c7e27
chengzhinei <chengxue5yuan@163.com>
2021-12-27 00:36:43 +0800

    add ut

--- libs/tendermint/delta/redis-cgi/cgi_test.go
+++ libs/tendermint/delta/redis-cgi/cgi_test.go
@@ -1,6 +1,7 @@
 package redis_cgi
 
 import (
+	"github.com/garyburd/redigo/redis"
 	"github.com/google/uuid"
 	"github.com/okex/exchain/libs/tendermint/libs/log"
 	"github.com/stretchr/testify/require"
@@ -28,24 +29,31 @@ func TestRedisClient_ResetLatestHeightAfterUpload(t *testing.T) {
 	require.True(t, err == nil, err)
 
 	height := int64(ConstTestHeight)
-	random, err := uuid.NewRandom()
-	require.Nil(t, err)
-	bytes := []byte(random.String())
+	getBytes := func() ([]byte, error) {
+		random, err := uuid.NewRandom()
+		require.Nil(t, err)
+		bytes := []byte(random.String())
+		return bytes, nil
+	}
+	conn := ss.pool.Get()
 
 	// 1. first time upload and set LatestHeight => success
-	succeed := ss.ResetLatestHeightAfterUpload(height, bytes)
-	latestHeight, err := ss.GetDistState(latestHeightKey)
+	succeed := ss.ResetLatestHeightAfterUpload(height, getBytes)
+	latestHeight, err := redis.Int64(conn.Do("GET", latestHeightKey))
 	require.Nil(t, err)
 	require.True(t, succeed, succeed)
 	require.True(t, latestHeight == height, height)
 
 	// 2. height < latestHeight => set failed
-	height =
-	succeed = ss.ResetLatestHeightAfterUpload(height, bytes)
-	require.True(t, !succeed, succeed)
+
+	// 3. height == latestHeight => set failed
+
+	// 4. height > latestHeight => success
+
+	// 5. getBytes err != nil => set failed
 }
 
-func TestRedisClient_SetGetDeltas(t *testing.T) {
+func TestRedisClient_GetDeltas(t *testing.T) {
 	ss, err := getRedisClient()
 	require.True(t, ss != nil, ss)
 	require.True(t, err == nil, err)
@@ -57,9 +65,12 @@ func TestRedisClient_SetGetDeltas(t *testing.T) {
 	require.True(t, s == nil, s)
 	require.True(t, err != nil, err)
 
-	deltaBytes := []byte(random.String())
-	err = ss.SetDeltas(height, deltaBytes)
-	require.True(t, err == nil, err)
+	getBytes := func() ([]byte, error) {
+		bytes := []byte(random.String())
+		return bytes, nil
+	}
+	succeed := ss.ResetLatestHeightAfterUpload(height, getBytes)
+	require.True(t, succeed, succeed)
 
 	result, err := ss.GetDeltas(height)
 	require.True(t, result != nil, result)
@@ -67,32 +78,6 @@ func TestRedisClient_SetGetDeltas(t *testing.T) {
 
 	fakeKey := int64(random.ID())
 	noResult, err := ss.GetDeltas(fakeKey)
-	require.True(t, noResult == nil, noResult)
-	require.True(t, err == nil, err)
-}
-
-func TestRedisClient_SetGetBlock(t *testing.T) {
-	ss, err := getRedisClient()
-	require.True(t, ss != nil, ss)
-	require.True(t, err == nil, err)
-
-	random, err := uuid.NewRandom()
-	require.Nil(t, err)
-	height := int64(ConstTestHeight)
-	s, err := ss.GetBlock(height)
-	require.True(t, s == nil, s)
-	require.True(t, err != nil, err)
-
-	deltaBytes := []byte(random.String())
-	err = ss.SetBlock(height, deltaBytes)
-	require.True(t, err == nil, err)
-
-	result, err := ss.GetBlock(height)
-	require.True(t, result != nil, result)
-	require.True(t, err == nil, err)
-
-	fakeKey := int64(random.ID())
-	noResult, err := ss.GetBlock(fakeKey)
 	require.True(t, noResult == nil, noResult)
 	require.True(t, err == nil, err)
 }
