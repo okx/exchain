b065481ca8246b6ecd94c0128f2a0e7037a41485
Molo Zhang <38446322+MoloZhang@users.noreply.github.com>
2022-01-04 15:16:54 +0800

    Merge PR: personal newaccount compatible with eth (#1333)
    * persnoal-newaccount-api
    
    * 947-personal-api-gotest
    
    * eth keystore name and update go test file
    
    * pull request
    
    * update keybase path
    
    * fix character ~ isn't parse
    
    * code review second
    
    * export keystore to the path must be absolute
    
    * fix call backend-file newaccount can't work
    
    * fix channel can't recieve passwd
    
    * remote key must be same as the last
    
    * debug
    
    * backendtest receive passwd
    
    * flag
    
    * close channel for remote passwd
    
    * roll back code
    
    Co-authored-by: chengzhinei <chengxue5yuan@163.com>
    Co-authored-by: xiangjianmeng <805442788@qq.com>
    Co-authored-by: Zhong Qiu <36867992+zhongqiuwood@users.noreply.github.com>

---
+++ app/crypto/ethkeystore/keystore_test.go
@@ -0,0 +1,56 @@
+package ethkeystore
+
+import (
+	"testing"
+
+	"github.com/okex/exchain/app/crypto/ethsecp256k1"
+	"github.com/okex/exchain/app/crypto/hd"
+	"github.com/okex/exchain/libs/cosmos-sdk/crypto/keys"
+	"github.com/okex/exchain/libs/cosmos-sdk/tests"
+	tmamino "github.com/okex/exchain/libs/tendermint/crypto/encoding/amino"
+	"github.com/okex/exchain/libs/tendermint/crypto/multisig"
+	"github.com/stretchr/testify/require"
+)
+
+func TestGetEthKey(t *testing.T) { testGetEthKey(t) }
+func testGetEthKey(t *testing.T) {
+	tmamino.RegisterKeyType(ethsecp256k1.PubKey{}, ethsecp256k1.PubKeyName)
+	tmamino.RegisterKeyType(ethsecp256k1.PrivKey{}, ethsecp256k1.PrivKeyName)
+	multisig.RegisterKeyType(ethsecp256k1.PubKey{}, ethsecp256k1.PubKeyName)
+
+	dir, cleanup := tests.NewTestCaseDir(t)
+	defer cleanup()
+	kb, err := keys.NewKeyring("keybasename", "test", dir, nil, hd.EthSecp256k1Options()...)
+	require.NoError(t, err)
+
+	tests := []struct {
+		name    string
+		passwd  string
+		keyType keys.SigningAlgo
+	}{
+		{
+			name:    "test-numbers-passwd",
+			passwd:  "12345678",
+			keyType: hd.EthSecp256k1,
+		},
+		{
+			name:    "test-characters-passwd",
+			passwd:  "abcdefgh",
+			keyType: hd.EthSecp256k1,
+		},
+	}
+	//generate test key
+	for _, tt := range tests {
+		_, _, err := kb.CreateMnemonic(tt.name, keys.English, tt.passwd, tt.keyType, "")
+		require.NoError(t, err)
+
+		// Exports private key from keybase using password
+		privKey, err := kb.ExportPrivateKeyObject(tt.name, tt.passwd)
+		require.NoError(t, err)
+
+		// Converts tendermint  key to ethereum key
+		_, err = EncodeTmKeyToEthKey(privKey)
+		require.NoError(t, err)
+
+	}
+}
