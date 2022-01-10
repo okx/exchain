47920a49e92c82be5a1605bdbd858eebd545c02c
YuanXingqiang <707312973@qq.com>
2022-01-10 18:04:25 +0800

    Merge PR ：RLP encode is not allowed until venus height (#1407)
    * RLP encode is not allowed until venus height
    
    * add optional heights paramter in sdk.TxDecoder
    
    * update rpc
    
    * update HigherThanVenus
    
    * txpool is not allowed until venus height
    
    * update global height when restarting the node
    
    Co-authored-by: KamiD <44460798+KamiD@users.noreply.github.com>

--- x/evm/types/utils.go
+++ x/evm/types/utils.go
@@ -5,6 +5,8 @@ import (
 	"encoding/binary"
 	"fmt"
 	authtypes "github.com/okex/exchain/libs/cosmos-sdk/x/auth/types"
+	"github.com/okex/exchain/libs/tendermint/global"
+	"github.com/okex/exchain/libs/tendermint/types"
 	"math/big"
 	"strings"
 
@@ -533,18 +535,29 @@ func DecodeResultData(in []byte) (ResultData, error) {
 // TxDecoder returns an sdk.TxDecoder that can decode both auth.StdTx and
 // MsgEthereumTx transactions.
 func TxDecoder(cdc *codec.Codec) sdk.TxDecoder {
-	return func(txBytes []byte) (sdk.Tx, error) {
+	return func(txBytes []byte, heights ...int64) (sdk.Tx, error) {
+		if len(heights) > 1 {
+			return nil, fmt.Errorf("to many height parameters")
+		}
 		var tx sdk.Tx
 		var err error
 		if len(txBytes) == 0 {
 			return nil, sdkerrors.Wrap(sdkerrors.ErrTxDecode, "tx bytes are empty")
 		}
 
-		// TODO: Is there a better way to Decode RLP-encoded MsgEthereumTx?
-		// Try to decode as MsgEthereumTx through RLP
-		var ethTx MsgEthereumTx
-		if err = authtypes.EthereumTxDecode(txBytes, &ethTx); err == nil {
-			return ethTx, nil
+		var height int64
+		if len(heights) == 1 {
+			height = heights[0]
+		} else {
+			height = global.GetGlobalHeight()
+		}
+
+		if types.HigherThanVenus(height) {
+			// Try to decode as MsgEthereumTx through RLP
+			var ethTx MsgEthereumTx
+			if err = authtypes.EthereumTxDecode(txBytes, &ethTx); err == nil {
+				return ethTx, nil
+			}
 		}
 
 		// sdk.Tx is an interface. The concrete message types
