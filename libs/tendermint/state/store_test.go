952c468f0c9f1168dbe2977a0057d70eea0beb01
cwbhhjl <cwbhhjl@gmail.com>
2022-01-11 16:10:10 +0800

    tmp

--- libs/tendermint/state/store_test.go
+++ libs/tendermint/state/store_test.go
@@ -198,6 +198,7 @@ func TestABCIResponsesAmino(t *testing.T) {
 		{
 			[]*abci.ResponseDeliverTx{
 				{}, nil, {12, tmp[:], "log", "info", 123, 456, []abci.Event{}, "sss", struct{}{}, []byte{}, 1},
+				{}, nil, {12, tmp[:], "log", "info", 123, 456, []abci.Event{}, "sss", struct{}{}, []byte{}, 1},
 			},
 			&abci.ResponseEndBlock{
 				ValidatorUpdates: []abci.ValidatorUpdate{
