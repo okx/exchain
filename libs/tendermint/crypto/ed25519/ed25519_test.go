1494ac1ccd85781be2cd258a891cec855d338cfc
zhongqiuwood <zhongqiuwood@gmail.com>
2022-01-18 07:12:30 +0800

    ut

--- libs/tendermint/crypto/ed25519/ed25519_test.go
+++ libs/tendermint/crypto/ed25519/ed25519_test.go
@@ -1,7 +1,6 @@
 package ed25519_test
 
 import (
-	"fmt"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"testing"
 
@@ -36,17 +35,6 @@ func TestSignAndValidateEd25519(t *testing.T) {
 	assert.Equal(t, bytes, recoverPubKey.Bytes())
 }
 
-func TestWTx2(t *testing.T) {
-
-}
-
-func genPrivkey(hex string) ed25519.PrivKeyEd25519 {
-	secert, err := hexutil.Decode(hex)
-	if err != nil {
-		panic(err)
-	}
-	return ed25519.GenPrivKeyFromSecret(secert)
-}
 
 func genPubkey(hex string) ed25519.PubKeyEd25519 {
 	bytes := hexutil.MustDecode(hex)
@@ -55,42 +43,3 @@ func genPubkey(hex string) ed25519.PubKeyEd25519 {
 	return recoverPubKey
 }
 
-func TestWTx(t *testing.T) {
-	payload := []byte("payload")
-
-	PrivKey := "0xa3288910406c97c4b1998e00171295f09681a385e898e8bf172c2befe181fdffa549b6b68568de211f86405eabee64b893e2938e753309ba294d8914887525c598c279bfd9"
-	PubKey := "0x1624de642068de211f86405eabee64b893e2938e753309ba294d8914887525c598c279bfd9"
-	//
-	PrivKey= "0xa3288910402de16907e788ccb9f3ed48ad6cca3198dd92334dd710b89ec19988b8d48d5f0fd134f5e36c5fdcf28ebe3b7ae039ace09d0198513f7d03500a2b4dc0465aff31"
-	PubKey= "0x1624de6420d134f5e36c5fdcf28ebe3b7ae039ace09d0198513f7d03500a2b4dc0465aff31"
-
-	priv := genPrivkey(t, PrivKey)
-	fmt.Printf("%s\n", 	hexutil.Encode(priv.PubKey().Bytes()))
-	fmt.Printf("%s\n", 	PubKey)
-
-
-
-
-	signature, err := priv.Sign(payload)
-	require.NoError(t, err)
-
-
-
-
-
-	res := priv.PubKey().VerifyBytes(payload, signature)
-	require.Equal(t, res, true)
-
-	bytes := priv.PubKey().Bytes()
-	var recoverPubKey ed25519.PubKeyEd25519
-	recoverPubKey.UnmarshalFromAmino(bytes)
-	assert.Equal(t, bytes, recoverPubKey.Bytes())
-
-
-	fmt.Printf("%s\n", 	hexutil.Encode(bytes))
-	fmt.Printf("%s\n", 	hexutil.Encode(priv.PubKey().Bytes()))
-	pubKeyBytes, err := hexutil.Decode(hexutil.Encode(bytes))
-	require.NoError(t, err)
-
-	assert.Equal(t, pubKeyBytes, recoverPubKey.Bytes())
-}
