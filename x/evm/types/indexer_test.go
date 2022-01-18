30353b759fd2e3474c92e51eef16333bd54e405d
chengzhinei <chengxue5yuan@163.com>
2022-01-18 16:22:48 +0800

    fix ut

--- x/evm/types/indexer_test.go
+++ x/evm/types/indexer_test.go
@@ -5,6 +5,7 @@ import (
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	"github.com/okex/exchain/libs/tendermint/libs/log"
+	tmtypes "github.com/okex/exchain/libs/tendermint/types"
 	"github.com/stretchr/testify/require"
 	"testing"
 
@@ -26,7 +27,7 @@ func TestIndexer_ProcessSection(t *testing.T) {
 		mock.SetBlockBloom(sdk.Context{}, int64(i), ethtypes.Bloom{})
 	}
 
-	bf := []*KV{}
+	bf := []*tmtypes.KV{}
 	indexer.ProcessSection(sdk.Context{}.WithLogger(log.NewNopLogger()), mock, uint64(blocks), &bf)
 
 	require.Equal(t, uint64(2), indexer.StoredSection())
