0d46265d94c2bc1b60694dd5128d9ee49e6f130b
Ben <codingben@163.com>
2022-01-20 15:16:35 +0800

    key value counter

---
+++ cmd/exchaind/data_statistics.go
@@ -0,0 +1,70 @@
+//go:build rocksdb
+// +build rocksdb
+
+package main
+
+import (
+	"log"
+
+	dbm "github.com/tendermint/tm-db"
+)
+
+func Statistic(name, fromDir string) {
+
+	log.Printf("statistics started")
+
+	pairCounter := 0
+
+	smallCounter := 0
+
+	midCounter := 0
+
+	largeCounter := 0
+
+	small := 128
+
+	mid := 1024
+
+	large := 16384
+
+	rdb, err := dbm.NewRocksDB(name, fromDir)
+	if err != nil {
+		panic(err)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	iter, err := rdb.Iterator(nil, nil)
+	if err != nil {
+		panic(err)
+	}
+
+	for ; iter.Valid(); iter.Next() {
+		keySize := iter.Key().Size()
+
+		valueSize := iter.Value().Size()
+
+		pairCounter++
+
+		if valueSize < small {
+			smallCounter++
+		}
+
+		if valueSize > mid && valueSize < large {
+			midCounter++
+		}
+
+		if valueSize > large {
+			largeCounter++
+		}
+	}
+
+	iter.Close()
+
+	log.Printf("pairs count: %s", pairCounter)
+	log.Printf("small count: %s", smaleCounter)
+	log.Printf("mid count: %s", midCounter)
+	log.Printf("large count: %s", largeCounter)
+}
