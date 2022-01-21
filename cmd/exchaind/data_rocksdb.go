62f580137bf26b78c7370d3715dd18987e5a698b
Ben <codingben@163.com>
2022-01-21 10:34:44 +0800

    rocks opt

--- cmd/exchaind/data_rocksdb.go
+++ cmd/exchaind/data_rocksdb.go
@@ -4,8 +4,6 @@
 package main
 
 import (
-	"log"
-
 	"github.com/tecbot/gorocksdb"
 	dbm "github.com/tendermint/tm-db"
 )
@@ -21,28 +19,28 @@ func init() {
 }
 
 func LtoR(name, fromDir, toDir string) {
-	log.Printf("convert %s(rocksdb => badgerdb) start...\n", name)
-
-	rdb, err := dbm.NewRocksDB(name, fromDir)
-	if err != nil {
-		panic(err)
-	}
-
-	bdb, err := dbm.NewBadgerDB(name, toDir)
-	if err != nil {
-		panic(err)
-	}
-
-	iter, err := rdb.Iterator(nil, nil)
-	if err != nil {
-		panic(err)
-	}
-
-	for ; iter.Valid(); iter.Next() {
-		bdb.Set(iter.Key(), iter.Value())
-	}
-	iter.Close()
-	log.Printf("convert %s(rocksdb => badgerdb) end.\n", name)
+	//log.Printf("convert %s(rocksdb => badgerdb) start...\n", name)
+	//
+	//rdb, err := dbm.NewRocksDB(name, fromDir)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//bdb, err := dbm.NewBadgerDB(name, toDir)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//iter, err := rdb.Iterator(nil, nil)
+	//if err != nil {
+	//	panic(err)
+	//}
+	//
+	//for ; iter.Valid(); iter.Next() {
+	//	bdb.Set(iter.Key(), iter.Value())
+	//}
+	//iter.Close()
+	//log.Printf("convert %s(rocksdb => badgerdb) end.\n", name)
 
 	//log.Printf("compact %s start...\n", name)
 	////bdb.DB()(gorocksdb.Range{})
