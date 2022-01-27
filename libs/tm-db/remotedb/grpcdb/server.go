f1bf1adabfb793646e8fe0debf7cd179427a8ca7
cwbhhjl <cwbhhjl@gmail.com>
2022-01-27 20:57:59 +0800

    Merge PR: fix tm-db import path (#1500)
    * update tm-db import path
    
    * update go mod

--- libs/tm-db/remotedb/grpcdb/server.go
+++ libs/tm-db/remotedb/grpcdb/server.go
@@ -9,8 +9,8 @@ import (
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials"
 
-	db "github.com/tendermint/tm-db"
-	protodb "github.com/tendermint/tm-db/remotedb/proto"
+	db "github.com/okex/exchain/libs/tm-db"
+	protodb "github.com/okex/exchain/libs/tm-db/remotedb/proto"
 )
 
 // ListenAndServe is a blocking function that sets up a gRPC based
