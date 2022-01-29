9398d5841bcb07ec4488139b867a93115ad1ea03
Molo Zhang <38446322+MoloZhang@users.noreply.github.com>
2022-01-29 18:04:53 +0800

    Merge PR: dds change json to amino   (#1510)
    * evm/watchdata amino
    
    * use capital letter when use encoder
    
    * TreeDeltaMap amino
    
    * abci response amino
    
    * amino format
    
    * update dds version
    
    * code format
    
    * debug
    
    * debug
    
    * debug
    
    * marshal struct* must check
    
    * add pl nil judge
    
    * fix dataInfo2Bytes
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    * debug
    
    Co-authored-by: chengzhinei <chengxue5yuan@163.com>
    Co-authored-by: xiangjianmeng <805442788@qq.com>
    Co-authored-by: cwbhhjl <cwbhhjl@gmail.com>

--- libs/tendermint/state/execution_dds_payload.go
+++ libs/tendermint/state/execution_dds_payload.go
@@ -2,6 +2,7 @@ package state
 
 import (
 	"fmt"
+
 	"github.com/okex/exchain/libs/iavl"
 	"github.com/okex/exchain/libs/tendermint/types"
 )
@@ -10,8 +11,8 @@ func unmarshalTreeDeltaMap(input []byte) (interface{}, error) {
 	if len(input) == 0 {
 		return nil, fmt.Errorf("failed unmarshal TreeDeltaMap: empty data")
 	}
-	var treeDeltaMap iavl.TreeDeltaMap
-	err := types.Json.Unmarshal(input, &treeDeltaMap)
+	treeDeltaMap := iavl.TreeDeltaMap{}
+	err := treeDeltaMap.UnmarshalFromAmino(nil, input)
 	return treeDeltaMap, err
 }
 
@@ -20,7 +21,7 @@ func marshalTreeDeltaMap(deltaMap interface{}) ([]byte, error) {
 	if !ok {
 		return nil, fmt.Errorf("failed marshal TreeDeltaMap")
 	}
-	return types.Json.Marshal(dm)
+	return dm.MarshalToAmino(nil)
 }
 
 type DeltaInfo struct {
@@ -28,14 +29,14 @@ type DeltaInfo struct {
 	treeDeltaMap  interface{}
 	watchData     interface{}
 
-	marshalWatchData    func() ([]byte, error)
+	marshalWatchData func() ([]byte, error)
 }
 
 // for upload
 func (info *DeltaInfo) dataInfo2Bytes() (types.DeltaPayload, error) {
 	var err error
 	pl := types.DeltaPayload{}
-	pl.ABCIRsp, err = types.Json.Marshal(info.abciResponses)
+	pl.ABCIRsp, err = info.abciResponses.MarshalToAmino(cdc)
 	if err != nil {
 		return pl, err
 	}
@@ -54,11 +55,16 @@ func (info *DeltaInfo) dataInfo2Bytes() (types.DeltaPayload, error) {
 }
 
 func (info *DeltaInfo) bytes2DeltaInfo(pl *types.DeltaPayload) error {
+	if pl == nil {
+		return fmt.Errorf("Failed bytes to delta info: empty delta payload. ")
+	}
 	var err error
-	err = types.Json.Unmarshal(pl.ABCIRsp, &info.abciResponses)
+	ar := &ABCIResponses{}
+	err = ar.UnmarshalFromAmino(nil, pl.ABCIRsp)
 	if err != nil {
 		return err
 	}
+	info.abciResponses = ar
 
 	info.treeDeltaMap, err = unmarshalTreeDeltaMap(pl.DeltasBytes)
 	if err != nil {
