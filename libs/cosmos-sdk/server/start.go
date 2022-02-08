daa25aa3f7e0380380fd992b10cd1eeeda797ada
evan.han <iloversevan@gmail.com>
2022-02-08 14:15:33 +0800

    update default iavl-output-modules value

--- libs/cosmos-sdk/server/start.go
+++ libs/cosmos-sdk/server/start.go
@@ -168,7 +168,7 @@ which accepts a path for the resulting pprof file.
 	cmd.Flags().String(FlagLogServerUrl, "", "log server url")
 
 	cmd.Flags().Int(iavl.FlagIavlCacheSize, 1000000, "Max size of iavl cache")
-	cmd.Flags().StringToInt(tmiavl.FlagOutputModules, map[string]int{"evm": 1, "acc": 1}, "decide which module in iavl to be printed")
+	cmd.Flags().StringToInt(tmiavl.FlagOutputModules, map[string]int{"evm": 1, "acc": 1, "ndb": 1}, "decide which module in iavl to be printed")
 	cmd.Flags().Int64(tmiavl.FlagIavlCommitIntervalHeight, 100, "Max interval to commit node cache into leveldb")
 	cmd.Flags().Int64(tmiavl.FlagIavlMinCommitItemCount, 500000, "Min nodes num to triggle node cache commit")
 	cmd.Flags().Int(tmiavl.FlagIavlHeightOrphansCacheSize, 8, "Max orphan version to cache in memory")
