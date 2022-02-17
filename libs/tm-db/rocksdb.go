0818742d70a0ec27afd5ed19083511bdf8b8c590
Ben <codingben@163.com>
2022-02-17 16:44:56 +0800

    pointLookup

--- libs/tm-db/rocksdb.go
+++ libs/tm-db/rocksdb.go
@@ -61,7 +61,7 @@ func NewRocksDB(name string, dir string) (*RocksDB, error) {
 		bbto.SetBlockCache(gorocksdb.NewLRUCache(cache))
 	}
 
-	blockSize := 64 * 1024
+	blockSize := 32 * 1024
 	bbto.SetBlockSize(blockSize)
 
 	bbto.SetCacheIndexAndFilterBlocks(true)
