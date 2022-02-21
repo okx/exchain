a96fb3eb3dccc102ecfa5232f5cee53a5d32f5cb
Zhong Qiu <36867992+zhongqiuwood@users.noreply.github.com>
2022-02-21 10:53:53 +0800

    Merge PR: Ante tracer (#1572)
    * add ante statistics log
    
    * add log for repair and replay
    
    * change log format
    
    * change code for coin init
    
    * test
    
    * ante tracer
    
    * ante tracer
    
    * ante tracer
    
    * ante tracer
    
    * ante tracer
    
    * ante tracer
    
    * ante tracer
    
    * code formatting
    
    Co-authored-by: BananaLF <864685021@qq.com>
    Co-authored-by: xiangjianmeng <805442788@qq.com>

--- app/ante/EthSetupContextDecorator.go
+++ app/ante/EthSetupContextDecorator.go
@@ -23,8 +23,7 @@ func NewEthSetupContextDecorator() EthSetupContextDecorator {
 // This is undone at the EthGasConsumeDecorator, where the context is set with the
 // ethereum tx GasLimit.
 func (escd EthSetupContextDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (newCtx sdk.Context, err error) {
-	// all transactions must implement GasTx
-	gasTx := tx
+	pinAnte(ctx.AnteTracer(), "EthSetupContextDecorator")
 
 	// Decorator will catch an OutOfGasPanic caused in the next antehandler
 	// AnteHandlers must have their own defer/recover in order for the BaseApp
@@ -37,7 +36,7 @@ func (escd EthSetupContextDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simu
 			case sdk.ErrorOutOfGas:
 				log := fmt.Sprintf(
 					"out of gas in location: %v; gasLimit: %d, gasUsed: %d",
-					rType.Descriptor, gasTx.GetGas(), ctx.GasMeter().GasConsumed(),
+					rType.Descriptor, tx.GetGas(), ctx.GasMeter().GasConsumed(),
 				)
 				err = sdkerrors.Wrap(sdkerrors.ErrOutOfGas, log)
 			default:
@@ -45,7 +44,5 @@ func (escd EthSetupContextDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simu
 			}
 		}
 	}()
-
 	return next(ctx, tx, simulate)
 }
-
