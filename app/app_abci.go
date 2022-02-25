34b502d1f9ea26268d232b72f9d74abe588027d9
worm <lcmmhcc@126.com>
2022-02-25 16:23:13 +0800

    add chainid check for begin block

--- app/app_abci.go
+++ app/app_abci.go
@@ -1,7 +1,11 @@
 package app
 
 import (
+	"fmt"
+
 	appconfig "github.com/okex/exchain/app/config"
+	okextypes "github.com/okex/exchain/app/types"
+	sdkerrors "github.com/okex/exchain/libs/cosmos-sdk/types/errors"
 	abci "github.com/okex/exchain/libs/tendermint/abci/types"
 	"github.com/okex/exchain/libs/tendermint/trace"
 	"github.com/okex/exchain/x/common/analyzer"
@@ -15,6 +19,10 @@ func (app *OKExChainApp) BeginBlock(req abci.RequestBeginBlock) (res abci.Respon
 
 	// dump app.LastBlockHeight()-1 info for reactor sync mode
 	trace.GetElapsedInfo().Dump(app.Logger())
+	// check req.header.ChainID
+	if req.Header.ChainID != okextypes.GetChainId() {
+		panic(sdkerrors.Wrap(okextypes.ErrInvalidChainID, fmt.Sprintf("Invalid Chain ID in RequestBeginBlock : %s, expected : %s", req.Header.ChainID, okextypes.GetChainId())))
+	}
 	return app.BaseApp.BeginBlock(req)
 }
 
