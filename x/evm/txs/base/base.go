776c0880d2cbc8ac9fa73fbee085ac133aaa5626
zhangkai <zhangkai.gis@163.com>
2022-02-28 19:30:42 +0800

    refactor handleMsgEthereum with template design pattern

--- x/evm/txs/base/base.go
+++ x/evm/txs/base/base.go
@@ -1,10 +1,18 @@
 package base
 
 import (
+	"fmt"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/okex/exchain/app/refund"
+	bam "github.com/okex/exchain/libs/cosmos-sdk/baseapp"
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
+	authexported "github.com/okex/exchain/libs/cosmos-sdk/x/auth/exported"
+	"github.com/okex/exchain/x/common/analyzer"
 	"github.com/okex/exchain/x/evm"
 	"github.com/okex/exchain/x/evm/keeper"
 	"github.com/okex/exchain/x/evm/types"
+	"github.com/okex/exchain/x/evm/watcher"
+	"math/big"
 )
 
 // Config tx's needed ctx and keeper
@@ -27,45 +35,67 @@ type Tx struct {
 	Keeper *evm.Keeper
 
 	StateTransition types.StateTransition
-	// state transition result
-	Result Result
 }
 
-func NewTx(config Config) *Tx {
-	return &Tx{
-		Ctx:    config.Ctx,
-		Keeper: config.Keeper,
+// DecorateResult TraceTxLog situation Decorate the result
+// it was replaced to trace logs when trace tx even if err != nil
+func (tx *Tx) DecorateResult(inResult *Result, inErr error) (result *sdk.Result, err error) {
+	if inResult == nil || inResult.ExecResult == nil {
+		return nil, fmt.Errorf("result is nil")
+	}
+
+	return inResult.ExecResult.Result, inErr
+}
+
+func (tx *Tx) RestoreWatcherTransactionReceipt(msg *types.MsgEthereumTx) {
+	tx.Keeper.Watcher.SaveTransactionReceipt(
+		watcher.TransactionFailed,
+		msg,
+		*tx.StateTransition.TxHash,
+		uint64(tx.Keeper.TxCount-1),
+		&types.ResultData{}, tx.Ctx.GasMeter().GasConsumed())
+}
+
+func (tx *Tx) GetChainConfig() (types.ChainConfig, bool) {
+	return tx.Keeper.GetChainConfig(tx.Ctx)
+}
+
+func (tx *Tx) GetSenderAccount() authexported.Account {
+	pm := tx.Keeper.GenerateCSDBParams()
+	infCtx := tx.Ctx.WithGasMeter(sdk.NewInfiniteGasMeter())
+
+	return pm.AccountKeeper.GetAccount(infCtx, tx.StateTransition.Sender.Bytes())
+}
+
+func (tx *Tx) ResetWatcher(account authexported.Account) {
+	tx.Keeper.Watcher.Reset()
+	// delete account which is already in Watcher.batch
+	if account != nil {
+		tx.Keeper.Watcher.AddDelAccMsg(account, true)
 	}
 }
 
-// Prepare convert msg to state transition
-func (tx *Tx) Prepare(msg *types.MsgEthereumTx) (err error) {
-	tx.StateTransition, err = msg2st(&tx.Ctx, tx.Keeper, msg)
-	return
-}
+func (tx *Tx) RefundFeesWatcher(account authexported.Account, coin sdk.Coins, price *big.Int) {
+	// fix account balance in watcher with refund fees
+	if account == nil || !tx.Keeper.Watcher.Enabled() {
+		return
+	}
+	gasConsumed := tx.Ctx.GasMeter().GasConsumed()
+	fixedFees := refund.CaculateRefundFees(gasConsumed, coin, price)
+	coins := account.GetCoins().Add2(fixedFees)
+	account.SetCoins(coins) //ignore err, no err will be returned in SetCoins
 
-// SaveTx since the txCount is used by the stateDB, and a simulated tx is run only on the node it's submitted to,
-// then this will cause the txCount/stateDB of the node that ran the simulated tx to be different with the
-// other nodes, causing a consensus error
-func (tx *Tx) SaveTx(msg *types.MsgEthereumTx) {
-	tx.Keeper.Watcher.SaveEthereumTx(msg, *tx.StateTransition.TxHash, uint64(tx.Keeper.TxCount))
-	// Prepare db for logs
-	tx.StateTransition.Csdb.Prepare(*tx.StateTransition.TxHash, tx.Keeper.Bhash, tx.Keeper.TxCount)
-	tx.StateTransition.Csdb.SetLogSize(tx.Keeper.LogSize)
-	tx.Keeper.TxCount++
+	pm := tx.Keeper.GenerateCSDBParams()
+	pm.Watcher.SaveAccount(account, false)
 }
 
 // Transition execute evm tx
-func (tx *Tx) Transition() (result *Result, err error) {
-	config, found := tx.Keeper.GetChainConfig(tx.Ctx)
-	if !found {
-		return nil, types.ErrChainConfigNotFound
-	}
+func (tx *Tx) Transition(config types.ChainConfig) (result *Result, err error) {
 	result.ExecResult, result.ResultData, err, result.InnerTxs, result.Erc20Contracts = tx.StateTransition.TransitionDb(tx.Ctx, config)
 	// async mod goes immediately
 	if tx.Ctx.IsAsync() {
 		tx.Keeper.LogsManages.Set(string(tx.Ctx.TxBytes()), keeper.TxResult{
-			ResultData: tx.Result.ResultData,
+			ResultData: result.ResultData,
 			Err:        err,
 		})
 	}
@@ -73,19 +103,32 @@ func (tx *Tx) Transition() (result *Result, err error) {
 	return
 }
 
-// DecorateResult TraceTxLog situation Decorate the result
-// it was replaced to trace logs when trace tx even if err != nil
-func (tx *Tx) DecorateResult(result *Result, err error) (*Result, error) {
-	//	if tx.Ctx.IsTraceTxLog() {
-	// the result was replaced to trace logs when trace tx even if err != nil
-	//		result.ExecResult.Result.Data = tx.Result.ExecResult.TraceLogs
-	//		return result.ExecResult.Result, nil
-	//	}
+func (tx *Tx) Commit(msg *types.MsgEthereumTx, result *Result) {
+	if result.InnerTxs != nil {
+		tx.Keeper.AddInnerTx(tx.StateTransition.TxHash.Hex(), result.InnerTxs)
+	}
+	if result.Erc20Contracts != nil {
+		tx.Keeper.AddContract(result.Erc20Contracts)
+	}
 
-	return result, err
+	// update block bloom filter
+	if !tx.Ctx.IsAsync() {
+		tx.Keeper.Bloom.Or(tx.Keeper.Bloom, result.ExecResult.Bloom)
+	}
+	tx.Keeper.LogSize = tx.StateTransition.Csdb.GetLogSize()
+	tx.Keeper.Watcher.SaveTransactionReceipt(watcher.TransactionSuccess,
+		msg, *tx.StateTransition.TxHash,
+		uint64(tx.Keeper.TxCount-1), result.ResultData, tx.Ctx.GasMeter().GasConsumed())
+	if msg.Data.Recipient == nil {
+		tx.StateTransition.Csdb.IteratorCode(func(addr common.Address, c types.CacheCode) bool {
+			tx.Keeper.Watcher.SaveContractCode(addr, c.Code)
+			tx.Keeper.Watcher.SaveContractCodeByHash(c.CodeHash, c.Code)
+			return true
+		})
+	}
 }
 
-func (tx *Tx) Emit(msg *types.MsgEthereumTx) *sdk.Result {
+func (tx *Tx) EmitEvent(msg *types.MsgEthereumTx, result *Result) {
 	tx.Ctx.EventManager().EmitEvents(sdk.Events{
 		sdk.NewEvent(
 			types.EventTypeEthereumTx,
@@ -108,16 +151,51 @@ func (tx *Tx) Emit(msg *types.MsgEthereumTx) *sdk.Result {
 	}
 
 	// set the events to the result
-	tx.Result.ExecResult.Result.Events = tx.Ctx.EventManager().Events()
-	if tx.Ctx.IsTraceTxLog() {
-		// the result was replaced to trace logs when trace tx
-		tx.Result.ExecResult.Result.Data = tx.Result.ExecResult.TraceLogs
-	}
-
-	return tx.Result.ExecResult.Result
+	result.ExecResult.Result.Events = tx.Ctx.EventManager().Events()
 }
 
-func (tx *Tx) Finalize() error {
-	//TODO implement me
-	panic("implement me")
+func (tx *Tx) FinalizeWatcher(account authexported.Account, err error) {
+	if err != nil {
+		tx.ResetWatcher(account)
+		return
+	}
+	tx.Keeper.Watcher.Finalize()
+}
+
+func (tx *Tx) AnalyzeStart(tag string) {
+	analyzer.StartTxLog(tag)
+}
+
+func (tx *Tx) AnalyzeStop(tag string) {
+	analyzer.StopTxLog(tag)
+}
+
+func NewTx(config Config) *Tx {
+	return &Tx{
+		Ctx:    config.Ctx,
+		Keeper: config.Keeper,
+	}
+}
+
+// Prepare convert msg to state transition
+func (tx *Tx) Prepare(msg *types.MsgEthereumTx) (err error) {
+	tx.AnalyzeStart(bam.Txhash)
+	defer tx.AnalyzeStop(bam.Txhash)
+
+	tx.StateTransition, err = msg2st(&tx.Ctx, tx.Keeper, msg)
+	return
+}
+
+// SaveTx since the txCount is used by the stateDB, and a simulated tx is run only on the node it's submitted to,
+// then this will cause the txCount/stateDB of the node that ran the simulated tx to be different with the
+// other nodes, causing a consensus error
+func (tx *Tx) SaveTx(msg *types.MsgEthereumTx) {
+	tx.AnalyzeStart(bam.SaveTx)
+	defer tx.AnalyzeStop(bam.SaveTx)
+
+	tx.Keeper.Watcher.SaveEthereumTx(msg, *tx.StateTransition.TxHash, uint64(tx.Keeper.TxCount))
+	// Prepare db for logs
+	tx.StateTransition.Csdb.Prepare(*tx.StateTransition.TxHash, tx.Keeper.Bhash, tx.Keeper.TxCount)
+	tx.StateTransition.Csdb.SetLogSize(tx.Keeper.LogSize)
+	tx.Keeper.TxCount++
 }
--- x/evm/txs/check/check.go
+++ x/evm/txs/check/check.go
@@ -2,41 +2,72 @@ package check
 
 import (
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
+	authexported "github.com/okex/exchain/libs/cosmos-sdk/x/auth/exported"
 	"github.com/okex/exchain/x/evm/txs/base"
 	"github.com/okex/exchain/x/evm/types"
+	"math/big"
 )
 
-type tx struct {
+type Tx struct {
 	baseTx *base.Tx
 }
 
-func (t tx) Prepare(msg *types.MsgEthereumTx) (err error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t tx) SaveTx(msg *types.MsgEthereumTx) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t tx) Transition() (result *base.Result, err error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t tx) DecorateResultError(result *base.Result, err error) (*base.Result, error) {
-	//TODO implement me
-	panic("implement me")
-}
-
-func (t tx) EmitEvent() *sdk.Result {
-	//TODO implement me
-	panic("implement me")
-}
-
-func NewTx(config base.Config) *tx {
-	return &tx{
+func NewTx(config base.Config) *Tx {
+	return &Tx{
 		baseTx: base.NewTx(config),
 	}
 }
+
+func (t *Tx) Prepare(msg *types.MsgEthereumTx) (err error) {
+	return t.baseTx.Prepare(msg)
+}
+
+// SaveTx check Tx do not transition state db
+func (t *Tx) SaveTx(msg *types.MsgEthereumTx) {
+	return
+}
+
+func (t *Tx) GetChainConfig() (types.ChainConfig, bool) {
+	return t.baseTx.GetChainConfig()
+}
+
+func (t *Tx) GetSenderAccount() authexported.Account {
+	return t.baseTx.GetSenderAccount()
+}
+
+func (t *Tx) ResetWatcher(account authexported.Account) {
+	t.baseTx.ResetWatcher(account)
+}
+
+// RefundFeesWatcher refund the watcher, check Tx do not save state so. skip
+func (t *Tx) RefundFeesWatcher(account authexported.Account, coins sdk.Coins, price *big.Int) {}
+
+func (t *Tx) Transition(config types.ChainConfig) (result *base.Result, err error) {
+	return t.baseTx.Transition(config)
+}
+
+func (t *Tx) DecorateResult(inResult *base.Result, inErr error) (result *sdk.Result, err error) {
+	return t.baseTx.DecorateResult(inResult, inErr)
+}
+
+// RestoreWatcherTransactionReceipt check Tx do not need restore
+func (t *Tx) RestoreWatcherTransactionReceipt(msg *types.MsgEthereumTx) {
+	return
+}
+
+func (t *Tx) Commit(msg *types.MsgEthereumTx, result *base.Result) {
+	t.baseTx.Commit(msg, result)
+}
+
+func (t *Tx) EmitEvent(msg *types.MsgEthereumTx, result *base.Result) {
+	t.baseTx.EmitEvent(msg, result)
+}
+
+// FinalizeWatcher check Tx do not need this
+func (t *Tx) FinalizeWatcher(account authexported.Account, err error) {}
+
+// AnalyzeStart check Tx do not analyze start
+func (t *Tx) AnalyzeStart(tag string) {}
+
+// AnalyzeStop check Tx do not analyze stop
+func (t *Tx) AnalyzeStop(tag string) {}
