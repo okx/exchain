4d94b999fe512977a66f70f80b3ccea3821f86d0
zhangkai <zhangkai.gis@163.com>
2022-02-28 09:04:39 +0800

    fix compile error

--- x/evm/txs/check/check.go
+++ x/evm/txs/check/check.go
@@ -1,11 +1,38 @@
 package check
 
 import (
+	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	"github.com/okex/exchain/x/evm/txs/base"
+	"github.com/okex/exchain/x/evm/types"
 )
 
 type tx struct {
 	baseTx *base.Tx
+}
+
+func (t tx) Prepare(msg *types.MsgEthereumTx) (err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t tx) SaveTx(msg *types.MsgEthereumTx) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t tx) Transition() (result *base.Result, err error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t tx) DecorateResultError(result *base.Result, err error) (*base.Result, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (t tx) EmitEvent() *sdk.Result {
+	//TODO implement me
+	panic("implement me")
 }
 
 func NewTx(config base.Config) *tx {
