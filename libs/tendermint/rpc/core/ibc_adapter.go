03eced0b58a0c03bc0a4d3a6a6b03b41767632c1
itsfunny <18757883747@163.com>
2022-03-01 15:26:45 +0800

    gomod remove tendermint

--- libs/tendermint/rpc/core/ibc_adapter.go
+++ libs/tendermint/rpc/core/ibc_adapter.go
@@ -3,34 +3,10 @@ package core
 import (
 	ctypes "github.com/okex/exchain/libs/tendermint/rpc/core/types"
 	rpctypes "github.com/okex/exchain/libs/tendermint/rpc/jsonrpc/types"
-	coretypes "github.com/tendermint/tendermint/rpc/core/types"
+	//coretypes "github.com/tendermint/tendermint/rpc/core/types"
 )
 
-//func CommitIBC(ctx *rpctypes.Context, heightPtr *int64) (*ctypes.IBCResultCommit, error) {
-//	coretypes.ResultCommit{}
-//	height, err := getHeight(env.BlockStore.Height(), heightPtr)
-//	if err != nil {
-//		return nil, err
-//	}
-//
-//	blockMeta := env.BlockStore.LoadBlockMeta(height)
-//	if blockMeta == nil {
-//		return nil, nil
-//	}
-//	header := blockMeta.Header
-//
-//	// If the next block has not been committed yet,
-//	// use a non-canonical commit
-//	if height == env.BlockStore.Height() {
-//		commit := env.BlockStore.LoadSeenCommit(height)
-//		return ConvResultCommitTOIBC(ctypes.NewResultCommit(&header, commit, false)), nil
-//	}
-//	// Return the canonical commit (comes from the block at height+1)
-//	commit := env.BlockStore.LoadBlockCommit(height)
-//	return ConvResultCommitTOIBC(ctypes.NewResultCommit(&header, commit, true)), nil
-//}
-
-func CommitIBC(ctx *rpctypes.Context, heightPtr *int64) (*coretypes.ResultCommit, error) {
+func CommitIBC(ctx *rpctypes.Context, heightPtr *int64) (*ctypes.IBCResultCommit, error) {
 	height, err := getHeight(env.BlockStore.Height(), heightPtr)
 	if err != nil {
 		return nil, err
@@ -46,9 +22,32 @@ func CommitIBC(ctx *rpctypes.Context, heightPtr *int64) (*coretypes.ResultCommit
 	// use a non-canonical commit
 	if height == env.BlockStore.Height() {
 		commit := env.BlockStore.LoadSeenCommit(height)
-		return ConvResultCommitToTendermint(ctypes.NewResultCommit(&header, commit, false)), nil
+		return ConvResultCommitTOIBC(ctypes.NewResultCommit(&header, commit, false)), nil
 	}
 	// Return the canonical commit (comes from the block at height+1)
 	commit := env.BlockStore.LoadBlockCommit(height)
-	return ConvResultCommitToTendermint(ctypes.NewResultCommit(&header, commit, true)), nil
+	return ConvResultCommitTOIBC(ctypes.NewResultCommit(&header, commit, true)), nil
 }
+
+//func CommitIBC(ctx *rpctypes.Context, heightPtr *int64) (*coretypes.ResultCommit, error) {
+//	height, err := getHeight(env.BlockStore.Height(), heightPtr)
+//	if err != nil {
+//		return nil, err
+//	}
+//
+//	blockMeta := env.BlockStore.LoadBlockMeta(height)
+//	if blockMeta == nil {
+//		return nil, nil
+//	}
+//	header := blockMeta.Header
+//
+//	// If the next block has not been committed yet,
+//	// use a non-canonical commit
+//	if height == env.BlockStore.Height() {
+//		commit := env.BlockStore.LoadSeenCommit(height)
+//		return ConvResultCommitToTendermint(ctypes.NewResultCommit(&header, commit, false)), nil
+//	}
+//	// Return the canonical commit (comes from the block at height+1)
+//	commit := env.BlockStore.LoadBlockCommit(height)
+//	return ConvResultCommitToTendermint(ctypes.NewResultCommit(&header, commit, true)), nil
+//}
--- libs/tendermint/rpc/core/ibc_utils.go
+++ libs/tendermint/rpc/core/ibc_utils.go
@@ -2,14 +2,9 @@ package core
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/okex/exchain/libs/tendermint/proto/version"
 	coretypes "github.com/okex/exchain/libs/tendermint/rpc/core/types"
 	"github.com/okex/exchain/libs/tendermint/types"
-	tmbytes "github.com/tendermint/tendermint/libs/bytes"
-	tmversion "github.com/tendermint/tendermint/proto/tendermint/version"
-	coretypes2 "github.com/tendermint/tendermint/rpc/core/types"
-	types2 "github.com/tendermint/tendermint/types"
 )
 
 func ConvResultCommitTOIBC(r *coretypes.ResultCommit) *coretypes.IBCResultCommit {
@@ -20,69 +15,73 @@ func ConvResultCommitTOIBC(r *coretypes.ResultCommit) *coretypes.IBCResultCommit
 	}
 
 	if hhash, chash := r.Header.IBCHash(), ret.Commit.BlockID.Hash; !bytes.Equal(hhash, chash) {
-		fmt.Println("asdd")
+		panic("asd")
 	}
 
 	return ret
 }
-func ConvResultCommitToTendermint(r *coretypes.ResultCommit) *coretypes2.ResultCommit {
-	ss := make([]types2.CommitSig, 0)
-	for _, v := range r.Commit.Signatures {
-		ss = append(ss, types2.CommitSig{
-			BlockIDFlag:      types2.BlockIDFlag(v.BlockIDFlag),
-			ValidatorAddress: types2.Address(v.ValidatorAddress),
-			Timestamp:        v.Timestamp,
-			Signature:        v.Signature,
-		})
-	}
-	rett := coretypes2.ResultCommit{
-		SignedHeader: types2.SignedHeader{
-			Header: &types2.Header{
-				Version: tmversion.Consensus{
-					Block: uint64(r.Header.Version.Block),
-					App:   uint64(r.Header.Version.App),
-				},
-				ChainID: r.SignedHeader.ChainID,
-				Height:  r.SignedHeader.Height,
-				Time:    r.SignedHeader.Time,
-				LastBlockID: types2.BlockID{
-					Hash: tmbytes.HexBytes(r.SignedHeader.LastBlockID.Hash),
-					PartSetHeader: types2.PartSetHeader{
-						Total: uint32(r.LastBlockID.PartsHeader.Total),
-						Hash:  tmbytes.HexBytes(r.LastBlockID.PartsHeader.Hash),
-					},
-				},
-				LastCommitHash:     tmbytes.HexBytes(r.Header.LastCommitHash),
-				DataHash:           tmbytes.HexBytes(r.Header.DataHash),
-				ValidatorsHash:     tmbytes.HexBytes(r.Header.ValidatorsHash),
-				NextValidatorsHash: tmbytes.HexBytes(r.Header.NextValidatorsHash),
-				ConsensusHash:      tmbytes.HexBytes(r.Header.ConsensusHash),
-				AppHash:            tmbytes.HexBytes(r.Header.AppHash),
-				LastResultsHash:    tmbytes.HexBytes(r.Header.LastResultsHash),
-				EvidenceHash:       tmbytes.HexBytes(r.Header.EvidenceHash),
-				ProposerAddress:    types2.Address(r.Header.ProposerAddress),
-			},
-			Commit: &types2.Commit{
-				Height: r.Commit.Height,
-				Round:  int32(r.Commit.Round),
-				BlockID: types2.BlockID{
-					Hash: tmbytes.HexBytes(r.Commit.BlockID.Hash),
-					PartSetHeader: types2.PartSetHeader{
-						Total: uint32(r.Commit.BlockID.PartsHeader.Total),
-						Hash:  tmbytes.HexBytes(r.Commit.BlockID.Hash),
-					},
-				},
-				Signatures: ss,
-			},
-		},
-		CanonicalCommit: r.CanonicalCommit,
-	}
-	rett.Commit.BlockID.Hash=rett.Header.Hash()
-	//if hhash, chash := rett.Header.Hash(), rett.Commit.BlockID.Hash; !bytes.Equal(hhash, chash) {
-	//	fmt.Println("asd")
-	//}
-	return &rett
-}
+
+
+//func ConvResultCommitToTendermint(r *coretypes.ResultCommit) *coretypes2.ResultCommit {
+//	ss := make([]types2.CommitSig, 0)
+//	for _, v := range r.Commit.Signatures {
+//		ss = append(ss, types2.CommitSig{
+//			BlockIDFlag:      types2.BlockIDFlag(v.BlockIDFlag),
+//			ValidatorAddress: types2.Address(v.ValidatorAddress),
+//			Timestamp:        v.Timestamp,
+//			Signature:        v.Signature,
+//		})
+//	}
+//	rett := coretypes2.ResultCommit{
+//		SignedHeader: types2.SignedHeader{
+//			Header: &types2.Header{
+//				Version: tmversion.Consensus{
+//					Block: uint64(r.Header.Version.Block),
+//					App:   uint64(r.Header.Version.App),
+//				},
+//				ChainID: r.SignedHeader.ChainID,
+//				Height:  r.SignedHeader.Height,
+//				Time:    r.SignedHeader.Time,
+//				LastBlockID: types2.BlockID{
+//					Hash: tmbytes.HexBytes(r.SignedHeader.LastBlockID.Hash),
+//					PartSetHeader: types2.PartSetHeader{
+//						Total: uint32(r.LastBlockID.PartsHeader.Total),
+//						Hash:  tmbytes.HexBytes(r.LastBlockID.PartsHeader.Hash),
+//					},
+//				},
+//				LastCommitHash:     tmbytes.HexBytes(r.Header.LastCommitHash),
+//				DataHash:           tmbytes.HexBytes(r.Header.DataHash),
+//				ValidatorsHash:     tmbytes.HexBytes(r.Header.ValidatorsHash),
+//				NextValidatorsHash: tmbytes.HexBytes(r.Header.NextValidatorsHash),
+//				ConsensusHash:      tmbytes.HexBytes(r.Header.ConsensusHash),
+//				AppHash:            tmbytes.HexBytes(r.Header.AppHash),
+//				LastResultsHash:    tmbytes.HexBytes(r.Header.LastResultsHash),
+//				EvidenceHash:       tmbytes.HexBytes(r.Header.EvidenceHash),
+//				ProposerAddress:    types2.Address(r.Header.ProposerAddress),
+//			},
+//			Commit: &types2.Commit{
+//				Height: r.Commit.Height,
+//				Round:  int32(r.Commit.Round),
+//				BlockID: types2.BlockID{
+//					Hash: tmbytes.HexBytes(r.Commit.BlockID.Hash),
+//					PartSetHeader: types2.PartSetHeader{
+//						Total: uint32(r.Commit.BlockID.PartsHeader.Total),
+//						Hash:  tmbytes.HexBytes(r.Commit.BlockID.Hash),
+//					},
+//				},
+//				Signatures: ss,
+//			},
+//		},
+//		CanonicalCommit: r.CanonicalCommit,
+//	}
+//	rett.Commit.BlockID.Hash=rett.Header.Hash()
+//	//if hhash, chash := rett.Header.Hash(), rett.Commit.BlockID.Hash; !bytes.Equal(hhash, chash) {
+//	//	fmt.Println("asd")
+//	//}
+//	return &rett
+//}
+
+
 func ConvSignheaderToIBCSignHeader(h *types.SignedHeader) *types.IBCSignedHeader {
 	ret := &types.IBCSignedHeader{
 		IBCHeader: ConvHeadersToIbcHeader(h.Header),
@@ -102,7 +101,7 @@ func ConvHeadersToIbcHeader(h *types.Header) *types.IBCHeader {
 		Time:    h.Time,
 		LastBlockID: types.IBCBlockID{
 			// TODO
-			Hash: h.IBCHash(),
+			Hash: h.LastBlockID.Hash,
 			PartSetHeader: types.IBCPartSetHeader{
 				Total: uint32(h.LastBlockID.PartsHeader.Total),
 				Hash:  h.LastBlockID.PartsHeader.Hash,
