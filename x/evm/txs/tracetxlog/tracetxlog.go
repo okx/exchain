6570d214d4b1c52e4c5f03251b7ebfb11a6ea158
zhangkai <zhangkai.gis@163.com>
2022-03-02 09:02:31 +0800

    change trace tx log struct receiver to pointer

--- x/evm/txs/tracetxlog/tracetxlog.go
+++ x/evm/txs/tracetxlog/tracetxlog.go
@@ -21,36 +21,36 @@ func NewTx(config base.Config) *tx {
 	}
 }
 
-func (t tx) Prepare(msg *types.MsgEthereumTx) (err error) {
+func (t *tx) Prepare(msg *types.MsgEthereumTx) (err error) {
 	return t.checkTx.Prepare(msg)
 }
 
-func (t tx) SaveTx(msg *types.MsgEthereumTx) {
+func (t *tx) SaveTx(msg *types.MsgEthereumTx) {
 	t.checkTx.SaveTx(msg)
 }
 
-func (t tx) GetChainConfig() (types.ChainConfig, bool) {
+func (t *tx) GetChainConfig() (types.ChainConfig, bool) {
 	return t.checkTx.GetChainConfig()
 }
 
-func (t tx) GetSenderAccount() authexported.Account {
+func (t *tx) GetSenderAccount() authexported.Account {
 	return t.checkTx.GetSenderAccount()
 }
 
-func (t tx) ResetWatcher(account authexported.Account) {
+func (t *tx) ResetWatcher(account authexported.Account) {
 	t.checkTx.ResetWatcher(account)
 }
 
-func (t tx) RefundFeesWatcher(account authexported.Account, coins sdk.Coins, price *big.Int) {
+func (t *tx) RefundFeesWatcher(account authexported.Account, coins sdk.Coins, price *big.Int) {
 	t.checkTx.RefundFeesWatcher(account, coins, price)
 }
 
-func (t tx) Transition(config types.ChainConfig) (result base.Result, err error) {
+func (t *tx) Transition(config types.ChainConfig) (result base.Result, err error) {
 	return t.checkTx.Transition(config)
 }
 
 // DecorateResult trace log tx need modify the result to log, and swallow error
-func (t tx) DecorateResult(inResult *base.Result, inErr error) (result *sdk.Result, err error) {
+func (t *tx) DecorateResult(inResult *base.Result, inErr error) (result *sdk.Result, err error) {
 	if inResult == nil || inResult.ExecResult == nil || inResult.ExecResult.Result == nil {
 		return nil, fmt.Errorf("result is nil")
 	}
@@ -59,26 +59,26 @@ func (t tx) DecorateResult(inResult *base.Result, inErr error) (result *sdk.Resu
 	return inResult.ExecResult.Result, nil
 }
 
-func (t tx) RestoreWatcherTransactionReceipt(msg *types.MsgEthereumTx) {
+func (t *tx) RestoreWatcherTransactionReceipt(msg *types.MsgEthereumTx) {
 	t.checkTx.RestoreWatcherTransactionReceipt(msg)
 }
 
-func (t tx) Commit(msg *types.MsgEthereumTx, result *base.Result) {
+func (t *tx) Commit(msg *types.MsgEthereumTx, result *base.Result) {
 	t.checkTx.Commit(msg, result)
 }
 
-func (t tx) EmitEvent(msg *types.MsgEthereumTx, result *base.Result) {
+func (t *tx) EmitEvent(msg *types.MsgEthereumTx, result *base.Result) {
 	t.checkTx.EmitEvent(msg, result)
 }
 
-func (t tx) FinalizeWatcher(account authexported.Account, err error) {
+func (t *tx) FinalizeWatcher(account authexported.Account, err error) {
 	t.checkTx.FinalizeWatcher(account, err)
 }
 
-func (t tx) AnalyzeStart(tag string) {
+func (t *tx) AnalyzeStart(tag string) {
 	t.checkTx.AnalyzeStart(tag)
 }
 
-func (t tx) AnalyzeStop(tag string) {
+func (t *tx) AnalyzeStop(tag string) {
 	t.checkTx.AnalyzeStop(tag)
 }
