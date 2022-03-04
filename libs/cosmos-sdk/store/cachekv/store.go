5c04f26c71119b4281490ff5977447bc87901d44
cwbhhjl <cwbhhjl@gmail.com>
2022-03-04 17:00:12 +0800

    cachekv Write optimize

--- libs/cosmos-sdk/store/cachekv/store.go
+++ libs/cosmos-sdk/store/cachekv/store.go
@@ -9,6 +9,8 @@ import (
 	"sync"
 	"unsafe"
 
+	"github.com/tendermint/go-amino"
+
 	tmkv "github.com/okex/exchain/libs/tendermint/libs/kv"
 	dbm "github.com/okex/exchain/libs/tm-db"
 
@@ -111,6 +113,12 @@ func (store *Store) Delete(key []byte) {
 
 // Implements Cachetypes.KVStore.
 func (store *Store) Write() {
+	// if parent is cachekv.Store, we can write kv more efficiently
+	if pStore, ok := store.parent.(*Store); ok {
+		store.writeToCacheKv(pStore)
+		return
+	}
+
 	store.mtx.Lock()
 	defer store.mtx.Unlock()
 
@@ -140,7 +148,32 @@ func (store *Store) Write() {
 	}
 
 	// Clear the cache
+	store.clearCache()
+}
 
+// writeToCacheKv will write cached kv to the parent Store, then clear the cache.
+func (store *Store) writeToCacheKv(parent *Store) {
+	store.mtx.Lock()
+	defer store.mtx.Unlock()
+
+	// TODO: Consider allowing usage of Batch, which would allow the write to
+	// at least happen atomically.
+	for key, cacheValue := range store.cache {
+		switch {
+		case cacheValue.deleted:
+			parent.Delete(amino.StrToBytes(key))
+		case cacheValue.value == nil:
+			// Skip, it already doesn't exist in parent.
+		default:
+			parent.Set(amino.StrToBytes(key), cacheValue.value)
+		}
+	}
+
+	// Clear the cache
+	store.clearCache()
+}
+
+func (store *Store) clearCache() {
 	// https://github.com/golang/go/issues/20138
 	for key := range store.cache {
 		delete(store.cache, key)
