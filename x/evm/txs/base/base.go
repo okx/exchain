95f11547a49ac9b290fd09bca672075ddbf43af9
ylsGit <bzwdyls@163.com>
2022-03-14 17:48:27 +0800

    Update base.go

--- x/evm/txs/base/base.go
+++ x/evm/txs/base/base.go
@@ -77,9 +77,9 @@ func (tx *Tx) Transition(config types.ChainConfig) (result Result, err error) {
 		TxHash:            result.ResultData.TxHash,
 		ContractAddress:   result.ResultData.ContractAddress,
 		GasUsed:           result.ExecResult.GasInfo.GasConsumed,
-		BlockHash:         tx.Keeper.GetHeightHash(tx.Ctx, uint64(tx.Ctx.BlockHeight())),
-		BlockNumber:       big.NewInt(tx.Ctx.BlockHeight()),
-		TransactionIndex:  uint(tx.Keeper.TxCount),
+		//BlockHash:         tx.Keeper.GetHeightHash(tx.Ctx, uint64(tx.Ctx.BlockHeight())),
+		//BlockNumber:       big.NewInt(tx.Ctx.BlockHeight()),
+		TransactionIndex: uint(tx.Keeper.TxCount),
 	}
 	_ = receipt
 	//if err = tx.Keeper.CallEvmHooks(tx.Ctx, tx.StateTransition.Sender, tx.StateTransition.Recipient, receipt); err != nil {
