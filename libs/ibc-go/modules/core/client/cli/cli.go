0ef9598ba2d27bb44771b0944a110a38287c0b6c
ylsGit <bzwdyls@163.com>
2022-03-29 11:56:52 +0800

    support query ibc params

--- libs/ibc-go/modules/core/client/cli/cli.go
+++ libs/ibc-go/modules/core/client/cli/cli.go
@@ -1,13 +1,19 @@
 package cli
 
 import (
+	"fmt"
+
 	"github.com/okex/exchain/libs/cosmos-sdk/client"
+	"github.com/okex/exchain/libs/cosmos-sdk/client/context"
+	"github.com/okex/exchain/libs/cosmos-sdk/client/flags"
 	"github.com/okex/exchain/libs/cosmos-sdk/codec"
 	interfacetypes "github.com/okex/exchain/libs/cosmos-sdk/codec/types"
+	"github.com/okex/exchain/libs/cosmos-sdk/version"
 	ibcclient "github.com/okex/exchain/libs/ibc-go/modules/core/02-client"
 	connection "github.com/okex/exchain/libs/ibc-go/modules/core/03-connection"
 	channel "github.com/okex/exchain/libs/ibc-go/modules/core/04-channel"
 	host "github.com/okex/exchain/libs/ibc-go/modules/core/24-host"
+	"github.com/okex/exchain/libs/ibc-go/modules/core/types"
 	"github.com/spf13/cobra"
 )
 
@@ -44,7 +50,30 @@ func GetQueryCmd(codec *codec.CodecProxy, reg interfacetypes.InterfaceRegistry) 
 		ibcclient.GetQueryCmd(codec, reg),
 		connection.GetQueryCmd(codec, reg),
 		channel.GetQueryCmd(codec, reg),
+		GetCmdParams(codec, reg),
 	)
 
 	return ibcQueryCmd
+}
+
+// GetCmdParams returns the command handler for ibc parameter querying.
+func GetCmdParams(m *codec.CodecProxy, reg interfacetypes.InterfaceRegistry) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:     "params",
+		Short:   "Query the current ibc parameters",
+		Long:    "Query the current ibc parameters",
+		Args:    cobra.NoArgs,
+		Example: fmt.Sprintf("%s query %s params", version.ServerName, host.ModuleName),
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			clientCtx := context.NewCLIContext().WithProxy(m).WithInterfaceRegistry(reg)
+			queryClient := types.NewQueryClient(clientCtx)
+
+			res, _ := queryClient.IbcParams(cmd.Context(), &types.QueryIbcParamsRequest{})
+			return clientCtx.PrintProto(res.Params)
+		},
+	}
+
+	flags.AddQueryFlagsToCmd(cmd)
+
+	return cmd
 }
--- libs/ibc-go/modules/core/types/query.go
+++ libs/ibc-go/modules/core/types/query.go
@@ -12,18 +12,20 @@ import (
 	porttypes "github.com/okex/exchain/libs/ibc-go/modules/core/05-port/types"
 )
 
-// QueryServer defines the IBC interfaces that the gRPC query server must implement
-type QueryServer interface {
+// QueryService defines the IBC interfaces that the gRPC query server must implement
+type QueryService interface {
 	clienttypes.QueryServer
 	connectiontypes.QueryServer
 	channeltypes.QueryServer
 	porttypes.QueryServer
+	QueryServer
 }
 
 // RegisterQueryService registers each individual IBC submodule query service
-func RegisterQueryService(server grpc.Server, queryService QueryServer) {
+func RegisterQueryService(server grpc.Server, queryService QueryService) {
 	client.RegisterQueryService(server, queryService)
 	connection.RegisterQueryService(server, queryService)
 	channel.RegisterQueryService(server, queryService)
 	port.RegisterQueryService(server, queryService)
+	RegisterQueryServer(server, queryService)
 }
