dea9438413bf6af279acca12e1d1748dacfe6c13
chengzhinei <chengxue5yuan@163.com>
2022-03-31 15:41:40 +0800

    fix test code

--- libs/tendermint/consensus/common_test.go
+++ libs/tendermint/consensus/common_test.go
@@ -207,7 +207,7 @@ func (cs *State) startTestRoutines(maxSteps int) {
 }
 
 func startTestRound(cs *State, height int64, round int) {
-	cs.enterNewRound(height, round, nil)
+	cs.enterNewRound(height, round)
 	cs.startTestRoutines(0)
 }
 
