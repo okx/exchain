43fec39ac6d48886bdc0b2c39f45205107dc5ee7
itsfunny <18757883747@163.com>
2022-04-01 22:57:45 +0800

    remove notes

--- libs/cosmos-sdk/codec/codec_adapter.go
+++ libs/cosmos-sdk/codec/codec_adapter.go
@@ -148,27 +148,3 @@ func (mp *CodecProxy) GetCdc() *Codec {
 func (mp *CodecProxy) GetProtocMarshal() *ProtoCodec {
 	return mp.protoCodec
 }
-
-//func (mp *CodecProxy) MustUnMarshal(data []byte, ptr interface{}) {
-//	err := mp.UnMarshal(data, ptr)
-//	if nil != err {
-//		err = mp.cdc.UnmarshalBinaryBare(data, ptr)
-//		if nil != err {
-//			panic(err)
-//		}
-//	}
-//}
-
-//func (mp *CodecProxy) UnMarshal(data []byte, ptr interface{}) error {
-//	return mp.protoCodec.UnmarshalInterface(data, ptr)
-//}
-
-//func (mp *CodecProxy) MustMarshal(v proto.Message) []byte {
-//	var ret []byte
-//	var err error
-//	ret, err = mp.protoCodec.MarshalInterface(v)
-//	if nil != err {
-//		panic(err)
-//	}
-//	return ret
-//}
