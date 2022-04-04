30d2b522759ac06024090b28ff10db03ffea6a26
chunfengSun <516108736@qq.com>
2022-04-04 15:17:51 +0800

    Update execution.go

--- libs/tendermint/state/execution.go
+++ libs/tendermint/state/execution.go
@@ -287,9 +287,9 @@ func (blockExec *BlockExecutor) runAbci(block *types.Block, deltaInfo *DeltaInfo
 				db:       blockExec.db,
 				proxyApp: blockExec.proxyApp,
 			}
-			if block.Height == 4667407 {
-				block.Txs = block.Txs[:10]
-			}
+			//if block.Height == 4667407 {
+			//	block.Txs = block.Txs[:10]
+			//}
 			if blockExec.isAsync {
 				abciResponses, err = execBlockOnProxyAppAsync(blockExec.logger, blockExec.proxyApp, block, blockExec.db)
 			} else {
