c0332b5a7fb66447257540651f76349b9e22e845
Ywmet <xiaoliang.zhang@okg.com>
2022-04-09 18:21:10 +0800

    rm no use ut set

--- libs/cosmos-sdk/store/rootmulti/store_test.go
+++ libs/cosmos-sdk/store/rootmulti/store_test.go
@@ -112,7 +112,6 @@ func TestHashStableWithEmptyCommit(t *testing.T) {
 
 	store1 := ms.getStoreByName("store1").(types.KVStore)
 	store1.Set(k, v)
-	tmtypes.SetVenus1HeightForIbcTest(math.MaxInt64 - 2)
 	cID, _ := ms.CommitterCommitMap(nil)
 	require.Equal(t, int64(1), cID.Version)
 	hash := cID.Hash
@@ -124,7 +123,7 @@ func TestHashStableWithEmptyCommit(t *testing.T) {
 }
 
 func TestMultistoreCommitLoad(t *testing.T) {
-	tmtypes.SetVenus1HeightForIbcTest(math.MaxInt64 - 2)
+
 	var db dbm.DB = dbm.NewMemDB()
 	store := newMultiStoreWithMounts(db, types.PruneNothing)
 	err := store.LoadLatestVersion()
