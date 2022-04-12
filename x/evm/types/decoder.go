4264c8fc4a249366d007eb283f87d943a56bc0e7
Ywmet <xiaoliang.zhang@okg.com>
2022-04-12 14:41:15 +0800

    rm log height info

--- x/evm/types/decoder.go
+++ x/evm/types/decoder.go
@@ -74,7 +74,7 @@ type Unmarshaler func(bytes []byte, ptr interface{}) error
 
 func ibcDecoder(cdcWrapper codec.CdcAbstraction, bytes []byte, height int64) (tx sdk.Tx, err error) {
 	if height >= 0 && !types.HigherThanVenus1(height) {
-		err = fmt.Errorf("IbcTxDecoder decode tx err,lower than Venus1 height ,current height %d", height)
+		err = fmt.Errorf("IbcTxDecoder decode tx err,lower than Venus1 height")
 		return
 	}
 	simReq := &typestx.SimulateRequest{}
