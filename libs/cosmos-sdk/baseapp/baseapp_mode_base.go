a93ea7561db695c26982a90422f7ffdd0b4bb5fb
MinZhu <minzhu09@163.com>
2022-04-12 22:48:25 +0800

    print GasConsumed

--- libs/cosmos-sdk/baseapp/baseapp_mode_base.go
+++ libs/cosmos-sdk/baseapp/baseapp_mode_base.go
@@ -3,6 +3,7 @@ package baseapp
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/okex/exchain/libs/tendermint/global"
 
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	sdkerrors "github.com/okex/exchain/libs/cosmos-sdk/types/errors"
@@ -146,6 +147,9 @@ func (m *modeHandlerBase) setGasConsumed(info *runTxInfo) {
 	info.ctx.BlockGasMeter().ConsumeGas(info.ctx.GasMeter().GasConsumedToLimit(), "block gas meter")
 	if info.ctx.BlockGasMeter().GasConsumed() < info.startingGas {
 		panic(sdk.ErrorGasOverflow{Descriptor: "tx gas summation"})
+	}
+	if global.GetGlobalHeight() == 4663637 {
+		fmt.Println("GasConsumed. ", info.ctx.BlockGasMeter().GasConsumed())
 	}
 }
 
