6e7527718ac7c75a04356ab55da0b720de958de5
lisuxiaoqi <lisuxiaoqi@gmail.com>
2022-04-13 16:28:18 +0800

    merge tm issue #4970

--- libs/tendermint/consensus/consensus_commit.go
+++ libs/tendermint/consensus/consensus_commit.go
@@ -11,7 +11,6 @@ import (
 	"github.com/okex/exchain/libs/tendermint/types"
 )
 
-
 func (cs *State) traceDump() {
 	if cs.Logger == nil {
 		return
@@ -294,12 +293,23 @@ func (cs *State) updateToState(state sm.State) {
 
 	// Reset fields based on state.
 	validators := state.Validators
-	lastPrecommits := (*types.VoteSet)(nil)
-	if cs.CommitRound > -1 && cs.Votes != nil {
+	switch {
+	case state.LastBlockHeight == types.GetStartBlockHeight(): // Very first commit should be empty.
+		cs.LastCommit = (*types.VoteSet)(nil)
+	case cs.CommitRound > -1 && cs.Votes != nil: // Otherwise, use cs.Votes
 		if !cs.Votes.Precommits(cs.CommitRound).HasTwoThirdsMajority() {
-			panic("updateToState(state) called but last Precommit round didn't have +2/3")
+			panic(fmt.Sprintf("Wanted to form a Commit, but Precommits (H/R: %d/%d) didn't have 2/3+: %v",
+				state.LastBlockHeight,
+				cs.CommitRound,
+				cs.Votes.Precommits(cs.CommitRound)))
 		}
-		lastPrecommits = cs.Votes.Precommits(cs.CommitRound)
+		cs.LastCommit = cs.Votes.Precommits(cs.CommitRound)
+	case cs.LastCommit == nil:
+		// NOTE: when Tendermint starts, it has no votes. reconstructLastCommit
+		// must be called to reconstruct LastCommit from SeenCommit.
+		panic(fmt.Sprintf("LastCommit cannot be empty in heights > 1 (H:%d)",
+			state.LastBlockHeight+1,
+		))
 	}
 
 	// Next desired block height
@@ -323,7 +333,6 @@ func (cs *State) updateToState(state sm.State) {
 	cs.ValidBlockParts = nil
 	cs.Votes = cstypes.NewHeightVoteSet(state.ChainID, height, validators)
 	cs.CommitRound = -1
-	cs.LastCommit = lastPrecommits
 	cs.LastValidators = state.LastValidators
 	cs.TriggeredTimeoutPrecommit = false
 	cs.state = state
@@ -332,12 +341,10 @@ func (cs *State) updateToState(state sm.State) {
 	cs.newStep()
 }
 
-
 func (cs *State) updateHeight(height int64) {
 	cs.metrics.Height.Set(float64(height))
 	cs.Height = height
 }
-
 
 func (cs *State) pruneBlocks(retainHeight int64) (uint64, error) {
 	base := cs.blockStore.Base()
