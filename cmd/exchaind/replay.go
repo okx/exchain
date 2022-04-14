d808d1bceb796f1c47a9cbcb5ccdb7057febdc11
MinZhu <minzhu09@163.com>
2022-04-14 12:13:47 +0800

    stop replay

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -283,7 +283,7 @@ func doReplay(ctx *server.Context, state sm.State, stateStoreDB dbm.DB,
 	global.SetGlobalHeight(lastBlockHeight + 1)
 	for height := lastBlockHeight + 1; height <= haltheight; height++ {
 		log.Println("replaying ", height)
-		if height >= 4329765 {
+		if height >= 4329764 {
 			break
 		}
 		block := originBlockStore.LoadBlock(height)
