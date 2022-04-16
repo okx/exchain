25d7ae5d79bb74367a826ed9e7936e8cd0408978
MinZhu <minzhu09@163.com>
2022-04-16 11:30:39 +0800

    remove return

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -297,9 +297,9 @@ func doReplay(ctx *server.Context, state sm.State, stateStoreDB dbm.DB,
 		if needSaveBlock {
 			SaveBlock(ctx, originBlockStore, height)
 		}
-		if height == 4040581 {
-			return
-		}
+		//if height == 4040581 {
+		//	return
+		//}
 	}
 }
 
