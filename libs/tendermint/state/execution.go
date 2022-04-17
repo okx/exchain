5041a7e94337ab32d4db1a66b8a28d0c657d8d20
MinZhu <minzhu09@163.com>
2022-04-17 10:50:45 +0800

    add log

--- libs/tendermint/state/execution.go
+++ libs/tendermint/state/execution.go
@@ -405,9 +405,10 @@ func execBlockOnProxyApp(context *executionTask) (*ABCIResponses, error) {
 			// Blocks may include invalid txs.
 			txRes := r.DeliverTx
 			if txRes.Code == abci.CodeTypeOK {
+				logger.Info("Valid tx", "code", txRes.Code, "log", txRes.Log, "index", txIndex)
 				validTxs++
 			} else {
-				logger.Debug("Invalid tx", "code", txRes.Code, "log", txRes.Log, "index", txIndex)
+				logger.Info("Invalid tx", "code", txRes.Code, "log", txRes.Log, "index", txIndex)
 				invalidTxs++
 			}
 			abciResponses.DeliverTxs[txIndex] = txRes
