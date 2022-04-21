c15cf7be2ab2a4d74c32ccd665ecba9f21c4084a
zhangkai <zhangkai.gis@163.com>
2022-04-21 13:44:59 +0800

    test

--- x/evm/watcher/tx.go
+++ x/evm/watcher/tx.go
@@ -22,7 +22,7 @@ func (w *Watcher) RecordABCIMessage(deliverTx *DeliverTx, txDecoder sdk.TxDecode
 
 	atomic.AddInt64(&w.recordingTxsCount, 1)
 	index := w.txIndexInBlock
-	w.dispatchTxJob(func() {
+	w.dispatchJob(func() {
 		w.recordTxsAndReceipts(deliverTx, index, txDecoder)
 	})
 	w.txIndexInBlock++
