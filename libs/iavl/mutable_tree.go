4c8d1eba7174f1c732b4ea10f613ccee51ecee30
Ray Green <48553537+banishee@users.noreply.github.com>
2022-04-23 00:12:07 +0800

    optimize judge

--- libs/iavl/mutable_tree.go
+++ libs/iavl/mutable_tree.go
@@ -710,9 +710,9 @@ func (tree *MutableTree) DeleteVersions(versions ...int64) error {
 // DeleteVersionsRange removes versions from an interval from the MutableTree (not inclusive).
 // An error is returned if any single version has active readers.
 // All writes happen in a single batch with a single commit.
-func (tree *MutableTree) DeleteVersionsRange(fromVersion, toVersion int64, enforce ...bool) error {
+func (tree *MutableTree) DeleteVersionsRange(fromVersion, toVersion int64, ignore ...bool) error {
 	batch := tree.NewBatch()
-	if err := tree.ndb.DeleteVersionsRange(batch, fromVersion, toVersion, enforce...); err != nil {
+	if err := tree.ndb.DeleteVersionsRange(batch, fromVersion, toVersion, ignore...); err != nil {
 		return err
 	}
 
