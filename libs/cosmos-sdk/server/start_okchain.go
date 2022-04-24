dad6e6522d00f0aae0f18075a7f0f2f07bd5df5a
MinZhu <minzhu09@163.com>
2022-04-24 18:18:55 +0800

    reset FlagMultiCache

--- libs/cosmos-sdk/server/start_okchain.go
+++ libs/cosmos-sdk/server/start_okchain.go
@@ -208,7 +208,7 @@ func RegisterServerFlags(cmd *cobra.Command) *cobra.Command {
 
 	cmd.Flags().Float64Var(&baseapp.GasUsedFactor, baseapp.FlagGasUsedFactor, 0.4, "factor to calculate history gas used")
 
-	cmd.Flags().Bool(sdk.FlagMultiCache, false, "Enable multi cache")
+	cmd.Flags().Bool(sdk.FlagMultiCache, true, "Enable multi cache")
 	cmd.Flags().Int(sdk.MaxAccInMultiCache, 0, "max acc in multi cache")
 	cmd.Flags().Int(sdk.MaxStorageInMultiCache, 0, "max storage in multi cache")
 	cmd.Flags().Bool(flatkv.FlagEnable, false, "Enable flat kv storage for read performance")
