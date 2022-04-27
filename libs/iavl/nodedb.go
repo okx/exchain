502aa99bcfba100f8024ea407dee3c8165d4a2ae
zhongqiuwood <zhongqiuwood@gmail.com>
2022-04-28 07:29:36 +0800

    ndb.lruNodeCache, err = lru.New(cacheSize)

--- libs/iavl/nodedb.go
+++ libs/iavl/nodedb.go
@@ -120,6 +120,7 @@ func newNodeDB(db dbm.DB, cacheSize int, opts *Options) *nodeDB {
 
 	var err error
 	ndb.lruNodeCache, err = lru.New(config.DynamicConfig.GetIavlCacheSize())
+	ndb.lruNodeCache, err = lru.New(cacheSize)
 	if err != nil {
 		panic(err)
 	}
