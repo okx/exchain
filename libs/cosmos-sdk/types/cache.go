62d6cfb7d2c3763b9735790c33b70e972a0693f3
cwbhhjl <cwbhhjl@gmail.com>
2022-04-27 11:02:59 +0800

    multicache optimize

--- libs/cosmos-sdk/types/cache.go
+++ libs/cosmos-sdk/types/cache.go
@@ -220,7 +220,7 @@ func (c *Cache) Write(updateDirty bool) {
 func (c *Cache) writeStorage(updateDirty bool) {
 	for addr, storages := range c.storageMap {
 		if _, ok := c.parent.storageMap[addr]; !ok {
-			c.parent.storageMap[addr] = make(map[ethcmn.Hash]*storageWithCache, 0)
+			c.parent.storageMap[addr] = make(map[ethcmn.Hash]*storageWithCache, len(storages))
 		}
 
 		for key, v := range storages {
@@ -229,7 +229,9 @@ func (c *Cache) writeStorage(updateDirty bool) {
 			}
 		}
 	}
-	c.storageMap = make(map[ethcmn.Address]map[ethcmn.Hash]*storageWithCache)
+	for key := range c.storageMap {
+		delete(c.storageMap, key)
+	}
 }
 
 func (c *Cache) setAcc(addr ethcmn.Address, v *accountWithCache) {
