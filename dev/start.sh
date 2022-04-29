7aabdf11d5d21970570dbcf748296773e6956bb4
zhongqiuwood <zhongqiuwood@gmail.com>
2022-04-30 05:46:57 +0800

    ndb.addOrphanItem(version, rootHash)

--- dev/start.sh
+++ dev/start.sh
@@ -23,6 +23,7 @@ killbyname() {
 run() {
     LOG_LEVEL=main:debug,iavl:info,*:error,state:info,provider:info
 
+#    exchaind start --pruning=nothing --rpc.unsafe \
     exchaind start --rpc.unsafe \
       --local-rpc-port 26657 \
       --log_level $LOG_LEVEL \
@@ -33,8 +34,8 @@ run() {
       --iavl-enable-async-commit \
       --enable-gid \
       --append-pid=true \
-      --iavl-commit-interval-height 3 \
-      --iavl-output-modules evm=1,acc=0 \
+      --iavl-commit-interval-height 5 \
+      --iavl-output-modules evm=0,acc=0 \
       --trace --home $HOME_SERVER --chain-id $CHAINID \
       --elapsed Round=1,CommitRound=1,Produce=1 \
       --rest.laddr "tcp://localhost:8545" > okc.txt 2>&1 &
