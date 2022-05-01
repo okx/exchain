f26c43afa7d9d9f51ec043119801645f0e78ae22
MinZhu <minzhu09@163.com>
2022-05-01 12:03:39 +0800

    remove for test

--- app/app_partial_concurrent.go
+++ app/app_partial_concurrent.go
@@ -1,12 +1,10 @@
 package app
 
 import (
-	"encoding/hex"
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	"github.com/okex/exchain/libs/cosmos-sdk/x/auth"
 	authante "github.com/okex/exchain/libs/cosmos-sdk/x/auth/ante"
 	evmtypes "github.com/okex/exchain/x/evm/types"
-	"strings"
 )
 
 // getTxFeeAndFromHandler get tx fee and from
@@ -19,18 +17,18 @@ func getTxFeeAndFromHandler(ak auth.AccountKeeper) sdk.GetTxFeeAndFromHandler {
 				return
 			}
 			fee = evmTx.GetFee()
-			from = evmTx.BaseTx.From
-			if len(from) > 2 {
-				from = strings.ToLower(from[2:])
-			}
-			if evmTx.To() != nil {
-				to = strings.ToLower(evmTx.To().String()[2:])
-			}
+			//from = evmTx.BaseTx.From
+			//if len(from) > 2 {
+			//	from = strings.ToLower(from[2:])
+			//}
+			//if evmTx.To() != nil {
+			//	to = strings.ToLower(evmTx.To().String()[2:])
+			//}
 		} else if feeTx, ok := tx.(authante.FeeTx); ok {
 			fee = feeTx.GetFee()
-			feePayer := feeTx.FeePayer(ctx)
-			feePayerAcc := ak.GetAccount(ctx, feePayer)
-			from = hex.EncodeToString(feePayerAcc.GetAddress())
+			//feePayer := feeTx.FeePayer(ctx)
+			//feePayerAcc := ak.GetAccount(ctx, feePayer)
+			//from = hex.EncodeToString(feePayerAcc.GetAddress())
 		}
 
 		return
