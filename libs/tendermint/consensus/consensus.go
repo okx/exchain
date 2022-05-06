37d9e2d6c0ecf55c8d05cb034a2126cf64bb6149
lisuxiaoqi <lisuxiaoqi@gmail.com>
2022-05-06 14:31:16 +0800

    add lock to avoid crash in switching fastsync

--- libs/tendermint/consensus/consensus.go
+++ libs/tendermint/consensus/consensus.go
@@ -105,8 +105,9 @@ type State struct {
 	evpool evidencePool
 
 	// internal state
-	mtx      sync.RWMutex
-	stateMtx sync.RWMutex
+	csmtx sync.RWMutex
+	mtx   sync.RWMutex
+
 	cstypes.RoundState
 	state sm.State // State until height-1.
 	// privValidator pubkey, memoized for the duration of one block
@@ -240,8 +241,8 @@ func (cs *State) String() string {
 
 // GetState returns a copy of the chain state.
 func (cs *State) GetState() sm.State {
-	cs.mtx.RLock()
-	defer cs.mtx.RUnlock()
+	cs.csmtx.RLock()
+	defer cs.csmtx.RUnlock()
 	return cs.state.Copy()
 }
 
@@ -727,8 +728,8 @@ func (cs *State) receiveRoutine(maxSteps int) {
 
 // state transitions on complete-proposal, 2/3-any, 2/3-one
 func (cs *State) handleMsg(mi msgInfo) {
-	//cs.mtx.Lock()
-	//defer cs.mtx.Unlock()
+	cs.csmtx.Lock()
+	defer cs.csmtx.Unlock()
 
 	var (
 		added bool
@@ -826,8 +827,8 @@ func (cs *State) handleTimeout(ti timeoutInfo, rs cstypes.RoundState) {
 	}
 
 	// the timeout will now cause a state transition
-	//cs.mtx.Lock()
-	//defer cs.mtx.Unlock()
+	cs.csmtx.Lock()
+	defer cs.csmtx.Unlock()
 
 	switch ti.Step {
 	case cstypes.RoundStepNewHeight:
@@ -869,8 +870,8 @@ func (cs *State) initNewHeight() {
 }
 
 func (cs *State) handleTxsAvailable() {
-	//cs.mtx.Lock()
-	//defer cs.mtx.Unlock()
+	cs.csmtx.Lock()
+	defer cs.csmtx.Unlock()
 
 	// We only need to do this for round 0.
 	if cs.Round != 0 {
@@ -936,9 +937,7 @@ func (cs *State) enterNewRound(height int64, round int) {
 	// but we fire an event, so update the round step first
 	cs.mtx.Lock()
 	cs.updateRoundStep(round, cstypes.RoundStepNewRound)
-	cs.stateMtx.Lock()
 	cs.Validators = validators
-	cs.stateMtx.Unlock()
 	if round == 0 {
 		// We've already reset these upon new height,
 		// and meanwhile we might have received a proposal
@@ -2248,7 +2247,7 @@ func (cs *State) signAddVote(msgType types.SignedMsgType, hash []byte, header ty
 	vote, err := cs.signVote(msgType, hash, header)
 	if err == nil {
 		//broadcast vote immediately
-		cs.evsw.FireEvent(types.EventSignVote, vote)
+		//cs.evsw.FireEvent(types.EventSignVote, vote)
 		cs.sendInternalMessage(msgInfo{&VoteMessage{vote}, ""})
 		cs.Logger.Info("Signed and pushed vote", "height", cs.Height, "round", cs.Round, "vote", vote, "err", err)
 		return vote
