f871feb938158c39a0075744ce7999f81bcc57cf
itsfunny <18757883747@163.com>
2022-05-06 21:09:18 +0800

    ref

--- libs/tendermint/rpc/jsonrpc/client/cm39_http_json_client.go
+++ libs/tendermint/rpc/jsonrpc/client/cm39_http_json_client.go
@@ -3,30 +3,20 @@ package client
 import (
 	"bytes"
 	"encoding/json"
+	"github.com/okex/exchain/libs/cosmos-sdk/codec"
 	"github.com/okex/exchain/libs/cosmos-sdk/types/errors"
 	coretypes "github.com/okex/exchain/libs/tendermint/rpc/core/types"
 	"github.com/okex/exchain/libs/tendermint/rpc/jsonrpc/types"
 	"io/ioutil"
 	"net/http"
-	"strings"
 )
 
-var (
-	defaultCM39ErrorHit = func(e error) bool {
-		return strings.Contains(e.Error(), "error unmarshalling result")
-	}
-	defaultCM39ResultTXBefore  = func() interface{} { return new(CM39ResultTx) }
-	defaultCM39ResultABCIQuery = func() interface{} { return new(CM39ResultABCIQuery) }
-)
-
-type couple struct {
-	onHitError func(e error) bool
-	before     func() interface{}
-	after      func(beforeReturnV interface{}, originV interface{})
+type decoder interface {
+	decode(cdc *codec.Codec, id types.JSONRPCIntID, data []byte, result interface{}) (interface{}, error)
 }
 
 type Cm39HttpJSONClientAdapter struct {
-	planb map[string]*couple
+	decoders map[string]decoder
 	*Client
 }
 
@@ -40,44 +30,66 @@ func NewCm39HttpJSONClient(remote string, client *http.Client) (*Cm39HttpJSONCli
 	return ret, nil
 }
 
+var (
+	_ decoder = (*defaultFuncDecoder)(nil)
+)
+
+type defaultFuncDecoder struct {
+	secondChance      func() interface{}
+	convSecondToFirst func(s interface{}, f interface{})
+}
+
+func newDefaultFuncDecoder(secondChance func() interface{}, convSecondToFirst func(s interface{}, f interface{})) *defaultFuncDecoder {
+	return &defaultFuncDecoder{secondChance: secondChance, convSecondToFirst: convSecondToFirst}
+}
+
+func (d defaultFuncDecoder) decode(cdc *codec.Codec, id types.JSONRPCIntID, data []byte, result interface{}) (ret interface{}, err error) {
+	ret, err = unmarshalResponseBytes(cdc, data, id, result)
+	if nil == err {
+		return
+	}
+	another := d.secondChance()
+	ret2, err2 := unmarshalResponseBytes(cdc, data, id, another)
+	if nil != err2 {
+		err = errors.Wrap(err, "second error:"+err2.Error())
+		return
+	}
+	d.convSecondToFirst(ret2, result)
+	ret = result
+	err = nil
+	return
+}
+
 func (c *Cm39HttpJSONClientAdapter) seal() {
-	c.planb = make(map[string]*couple)
-	c.planb["tx"] = &couple{
-		onHitError: defaultCM39ErrorHit,
-		before:     defaultCM39ResultTXBefore,
-		after: func(beforeReturnV interface{}, originV interface{}) {
-			cm39 := beforeReturnV.(*CM39ResultTx)
-			cm4 := originV.(*coretypes.ResultTx)
-			ConvTCM392CM4(cm39, cm4)
-		},
-	}
-	c.planb["abci_query"] = &couple{
-		onHitError: defaultCM39ErrorHit,
-		before:     defaultCM39ResultABCIQuery,
-		after: func(beforeReturnV interface{}, originV interface{}) {
-			cm39 := beforeReturnV.(*CM39ResultABCIQuery)
-			cm4 := originV.(*coretypes.ResultABCIQuery)
-			ConvTCM39ResultABCIQuery2CM4(cm39, cm4)
-		},
-	}
-	c.planb["broadcast_tx_commit"] = &couple{
-		onHitError: defaultCM39ErrorHit,
-		before: func() interface{} {
-			return new(CM39ResultBroadcastTxCommit)
-		},
-		after: func(beforeReturnV interface{}, originV interface{}) {
-			cm39 := beforeReturnV.(*CM39ResultBroadcastTxCommit)
-			cm4 := originV.(*coretypes.ResultBroadcastTxCommit)
-			ConvTCM39BroadcastCommitTx2CM4(cm39, cm4)
-		},
-	}
+	c.decoders = make(map[string]decoder)
+	c.decoders["tx"] = newDefaultFuncDecoder(func() interface{} {
+		return new(CM39ResultTx)
+	}, func(s interface{}, f interface{}) {
+		cm39 := s.(*CM39ResultTx)
+		cm4 := f.(*coretypes.ResultTx)
+		ConvTCM392CM4(cm39, cm4)
+	})
+	c.decoders["abci_query"] = newDefaultFuncDecoder(func() interface{} {
+		return new(CM39ResultABCIQuery)
+	}, func(s interface{}, f interface{}) {
+		cm39 := s.(*CM39ResultABCIQuery)
+		cm4 := f.(*coretypes.ResultABCIQuery)
+		ConvTCM39ResultABCIQuery2CM4(cm39, cm4)
+	})
+	c.decoders["broadcast_tx_commit"] = newDefaultFuncDecoder(func() interface{} {
+		return new(CM39ResultBroadcastTxCommit)
+	}, func(s interface{}, f interface{}) {
+		cm39 := s.(*CM39ResultBroadcastTxCommit)
+		cm4 := f.(*coretypes.ResultBroadcastTxCommit)
+		ConvTCM39BroadcastCommitTx2CM4(cm39, cm4)
+	})
 }
 
 func (c *Cm39HttpJSONClientAdapter) Call(method string, params map[string]interface{}, result interface{}) (ret interface{}, err error) {
-	return c.call(method, params, result, c.planb[method])
+	return c.call(method, params, result, c.decoders[method])
 }
 
-func (c *Cm39HttpJSONClientAdapter) call(method string, params map[string]interface{}, result interface{}, coup *couple) (interface{}, error) {
+func (c *Cm39HttpJSONClientAdapter) call(method string, params map[string]interface{}, result interface{}, dec decoder) (interface{}, error) {
 	id := c.nextRequestID()
 
 	request, err := types.MapToRequest(c.cdc, id, method, params)
@@ -110,15 +122,8 @@ func (c *Cm39HttpJSONClientAdapter) call(method string, params map[string]interf
 		return nil, errors.Wrap(err, "failed to read response body")
 	}
 
-	ret, err := unmarshalResponseBytes(c.cdc, responseBytes, id, result)
-	if nil != err && coup != nil && coup.onHitError(err) {
-		bef := coup.before()
-		if ret2, err2 := unmarshalResponseBytes(c.cdc, responseBytes, id, bef); nil != err2 {
-			err = errors.Wrap(err, "second error:"+err2.Error())
-		} else {
-			coup.after(ret2, result)
-			err = nil
-		}
+	if dec != nil {
+		return dec.decode(c.cdc, id, responseBytes, result)
 	}
-	return ret, err
+	return unmarshalResponseBytes(c.cdc, responseBytes, id, result)
 }
