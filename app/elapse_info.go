47a7cd9c9bc8d711221cbb336a62eed4227dae03
chengzhinei <chengxue5yuan@163.com>
2022-05-09 17:26:02 +0800

    fix ut

--- app/elapse_info.go
+++ app/elapse_info.go
@@ -103,7 +103,7 @@ func (e *ElapsedTimeInfos) Dump(input interface{}) {
 		}
 	}
 
-	info := fmt.Sprintf("%s<%s>, %s<%s>, %s<%s>, %s<%s>, %s<%s>, %s<%s>, %s[%s], %s[%s], %s<%s>, %s<%s>, %s<%s>, %s<%s>",
+	info := fmt.Sprintf("%s<%s>, %s<%s>, %s<%s>, %s<%s>, %s<%s>, %s[%s], %s[%s], %s<%s>, %s<%s>, %s<%s>, %s<%s>",
 		trace.Height, e.infoMap[trace.Height],
 		trace.Tx, e.infoMap[trace.Tx],
 		trace.BlockSize, e.infoMap[trace.BlockSize],
