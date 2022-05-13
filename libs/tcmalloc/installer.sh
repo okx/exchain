23f88cbd5c8bf662cdc2181e97d75091a7b877ea
zhangkai <zhangkai.gis@163.com>
2022-05-14 07:23:37 +0800

    add linux tcmalloc the name script name should not be install.sh becasue of libtoolize

--- libs/tcmalloc/installer.sh
+++ libs/tcmalloc/installer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#set -e
+set -e
 #set -x
 VERSION_NUM=2.9.1
 VERSION=gperftools-$VERSION_NUM
@@ -16,27 +16,23 @@ while [ $# -gt 0 ]; do
 	shift $(( $# > 0 ? 1 : 0 ))
 done
 
-echo $VERSION
-
 command_exists() {
 	command -v "$@" > /dev/null 2>&1
 }
 
-exit
-
 is_wsl() {
 	case "$(uname -r)" in
-	*microsoft* ) true ;; # WSL 2
-	*Microsoft* ) true ;; # WSL 1
-	* ) false;;
+		*microsoft* ) true ;; # WSL 2
+		*Microsoft* ) true ;; # WSL 1
+		* ) false;;
 	esac
 }
 
 is_darwin() {
 	case "$(uname -s)" in
-	*darwin* ) true ;;
-	*Darwin* ) true ;;
-	* ) false;;
+		*darwin* ) true ;;
+		*Darwin* ) true ;;
+		* ) false;;
 	esac
 }
 
@@ -52,15 +48,19 @@ get_distribution() {
 }
 
 install_linux() {
-  $sh_c "git clone https://github.com/gperftools/gperftools.git"
-  $sh_c "cd gperftools && git checkout ${VERSION}"
-  $sh_c "cd gperftools && sh autogen.sh"
-  $sh_c "cd gperftools && ./configure --disable-cpu-profiler --disable-heap-profiler --disable-heap-checker --disable-debugalloc --enable-minimal"
-  $sh_c "cd gperftools && make install"
-  $sh_c "ldconfig"
+	$sh_c "rm -rf gperftools"
+	$sh_c "git clone https://github.com/gperftools/gperftools.git"
+	$sh_c "cd gperftools && git checkout ${VERSION}"
+	$sh_c "cd gperftools && ./autogen.sh"
+	$sh_c "cd gperftools && ./configure --disable-cpu-profiler --disable-heap-profiler --disable-heap-checker --disable-debugalloc --enable-minimal --prefix=/usr --libdir=/usr/lib"
+	$sh_c "cd gperftools && make uninstall"
+	$sh_c "cd gperftools && make install"
+	$sh_c "ldconfig"
+	$sh_c "rm -rf gperftools"
 }
 
 install_macos(){
+	$sh_c "ldconfig"
 }
 
 do_install() {
@@ -90,39 +90,39 @@ do_install() {
 	# Run setup for each distro accordingly
 	case "$lsb_dist" in
 		ubuntu)
-			pre_reqs="git make autoconf automake libtool libunwind"
-      $sh_c 'apt-get update -qq >/dev/null'
-      $sh_c "apt-get install -y -qq $pre_reqs >/dev/null"
-      install_linux
+			pre_reqs="git make autoconf automake libtool"
+			$sh_c 'apt-get update -qq >/dev/null'
+			$sh_c "apt-get install -y -qq $pre_reqs >/dev/null"
+			install_linux
 			exit 0
 			;;
 		centos)
-		  pre_reqs="git make autoconf automake libtool libunwind"
-      $sh_c "yum install -y -q $pre_reqs"
-      install_linux
+			pre_reqs="git make autoconf automake libtool libunwind"
+			$sh_c "yum install -y -q $pre_reqs"
+			install_linux
 			exit 0
 			;;
 		*)
 			if [ -z "$lsb_dist" ]; then
 				if is_darwin; then
-				  pre_reqs="git make"
-          $sh_c "xcode-select --install"
-          $sh_c "brew install $pre_reqs"
-          install_macos
-          exit 0
+					pre_reqs="git make"
+					$sh_c "xcode-select --install"
+					$sh_c "brew install $pre_reqs"
+					install_macos
+					exit 0
 				fi
 				if is_wsl; then
-          echo
-          echo "ERROR: Unsupported OS 'Windows'"
-          echo "Please install tcmalloc from https://github.com/gperftools/gperftools/tree/gperftools-2.9.1"
-          echo
-          exit 1
-        fi
+					echo
+					echo "ERROR: Unsupported OS 'Windows'"
+					echo "Please install tcmalloc from https://github.com/gperftools/gperftools/tree/gperftools-2.9.1"
+					echo
+					exit 1
+				fi
 			fi
-      echo
-      echo "ERROR: Unsupported distribution '$lsb_dist'"
-      echo
-      exit 1
+			echo
+			echo "ERROR: Unsupported distribution '$lsb_dist'"
+			echo
+			exit 1
 			;;
 	esac
 	exit 1
