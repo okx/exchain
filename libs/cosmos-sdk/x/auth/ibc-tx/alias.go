fb9a02f5024b4ba6973bcc47d07269fd3c8a6106
Ywmet <xiaoliang.zhang@okg.com>
2022-05-17 17:05:14 +0800

    add miss

---
+++ libs/cosmos-sdk/x/auth/ibc-tx/alias.go
@@ -0,0 +1,12 @@
+package ibc_tx
+
+import (
+	"github.com/okex/exchain/libs/cosmos-sdk/x/auth/ibc-tx/internal/adapter"
+	ibccodec "github.com/okex/exchain/libs/cosmos-sdk/x/auth/ibc-tx/internal/ibc-codec"
+)
+
+var (
+	PubKeyRegisterInterfaces = ibccodec.RegisterInterfaces
+	LagacyKey2PbKey          = adapter.LagacyPubkey2ProtoBuffPubkey
+	PbKey2LagacyKey          = adapter.ProtoBufPubkey2LagacyPubkey
+)
