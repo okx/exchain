72ff3c87ebbee50999821e20fe398da52e7a82b2
864685021@qq.com <864685021@qq.com>
2022-05-26 15:31:00 +0800

    update address some method for code standard

--- x/wasm/types/address_adapter.go
+++ x/wasm/types/address_adapter.go
@@ -2,8 +2,8 @@ package types
 
 import (
 	"crypto/sha256"
-	"reflect"
-	"unsafe"
+	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
+	"github.com/okex/exchain/libs/cosmos-sdk/types/errors"
 )
 
 // Module is a specialized version of a composed address for modules. Each module account
@@ -11,47 +11,21 @@ import (
 func Module(moduleName string, key []byte) []byte {
 	mKey := append([]byte(moduleName), 0)
 
-	return Hash("module", append(mKey, key...))
+	return hash("module", append(mKey, key...))
 }
 
 // Hash creates a new address from address type and key
-func Hash(typ string, key []byte) []byte {
+func hash(typ string, key []byte) []byte {
 	hasher := sha256.New()
-	_, err := hasher.Write(UnsafeStrToBytes(typ))
+	_, err := hasher.Write(sdk.UnsafeStrToBytes(typ))
 	// the error always nil, it's here only to satisfy the io.Writer interface
-	AssertNil(err)
+	errors.AssertNil(err)
 	th := hasher.Sum(nil)
 
 	hasher.Reset()
 	_, err = hasher.Write(th)
-	AssertNil(err)
+	errors.AssertNil(err)
 	_, err = hasher.Write(key)
-	AssertNil(err)
+	errors.AssertNil(err)
 	return hasher.Sum(nil)
-}
-
-func AssertNil(data interface{}) {
-	if data != nil {
-		panic(data)
-	}
-}
-
-// UnsafeStrToBytes uses unsafe to convert string into byte array. Returned bytes
-// must not be altered after this function is called as it will cause a segmentation fault.
-func UnsafeStrToBytes(s string) []byte {
-	var buf []byte
-	sHdr := (*reflect.StringHeader)(unsafe.Pointer(&s))
-	bufHdr := (*reflect.SliceHeader)(unsafe.Pointer(&buf))
-	bufHdr.Data = sHdr.Data
-	bufHdr.Cap = sHdr.Len
-	bufHdr.Len = sHdr.Len
-	return buf
-}
-
-// UnsafeBytesToStr is meant to make a zero allocation conversion
-// from []byte -> string to speed up operations, it is not meant
-// to be used generally, but for a specific pattern to delete keys
-// from a map.
-func UnsafeBytesToStr(b []byte) string {
-	return *(*string)(unsafe.Pointer(&b))
 }
