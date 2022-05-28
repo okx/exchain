435a3fdd88b8b6dace0cf29bc59b52c6c3a13dd4
Evan Han <iloversevan@gmail.com>
2022-05-28 17:24:41 +0800

    Merge PR: optimize performance of eth_getBlockByNum and eth_getBlockByHash (#2108)
    * optimize performance of eth_getBlockByNum and eth_getBlockByHash
    
    * fix ut
    
    * fix ut
    
    Co-authored-by: KamiD <44460798+KamiD@users.noreply.github.com>

--- app/rpc/backend/cache.go
+++ app/rpc/backend/cache.go
@@ -6,9 +6,9 @@ import (
 )
 
 type Cache interface {
-	GetBlockByNumber(number uint64) (*watcher.Block, error)
-	GetBlockByHash(hash common.Hash) (*watcher.Block, error)
-	AddOrUpdateBlock(hash common.Hash, block *watcher.Block)
+	GetBlockByNumber(number uint64, fullTx bool) (*watcher.Block, error)
+	GetBlockByHash(hash common.Hash, fullTx bool) (*watcher.Block, error)
+	AddOrUpdateBlock(hash common.Hash, block *watcher.Block, fullTx bool)
 	GetTransaction(hash common.Hash) (*watcher.Transaction, error)
 	AddOrUpdateTransaction(hash common.Hash, tx *watcher.Transaction)
 	GetBlockHash(number uint64) (common.Hash, error)
