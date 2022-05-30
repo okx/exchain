2a5cfd3937a9a8cdd31230f6a41113b51ec293b7
xiangjianmeng <805442788@qq.com>
2022-05-30 14:36:56 +0800

    fix ibc header validate

--- libs/tendermint/lite2/ibc_verify.go
+++ libs/tendermint/lite2/ibc_verify.go
@@ -1,9 +1,6 @@
 package lite
 
 import (
-	"bytes"
-	"errors"
-	errpkg "github.com/pkg/errors"
 	"time"
 
 	tmmath "github.com/okex/exchain/libs/tendermint/libs/math"
@@ -40,45 +37,9 @@ func IBCVerifyNonAdjacent(
 	maxClockDrift time.Duration,
 	trustLevel tmmath.Fraction) error {
 
-	if untrustedHeader.Height == trustedHeader.Height+1 {
-		return errors.New("headers must be non adjacent in height")
-	}
-
-	if HeaderExpired(trustedHeader, trustingPeriod, now) {
-		return ErrOldHeaderExpired{trustedHeader.Time.Add(trustingPeriod), now}
-	}
-
-	if err := ibcVerifyNewHeaderAndVals(
-		chainID,
-		untrustedHeader, untrustedVals,
-		trustedHeader,
-		now, maxClockDrift); err != nil {
-		return ErrInvalidHeader{err}
-	}
-
-	// Ensure that +`trustLevel` (default 1/3) or more of last trusted validators signed correctly.
-	err := trustedVals.IBCVerifyCommitLightTrusting(chainID, untrustedHeader.Commit.BlockID, untrustedHeader.Height,
-		untrustedHeader.Commit, trustLevel)
-	if err != nil {
-		switch e := err.(type) {
-		case types.ErrNotEnoughVotingPowerSigned:
-			return ErrNewValSetCantBeTrusted{e}
-		default:
-			return e
-		}
-	}
-
-	// Ensure that +2/3 of new validators signed correctly.
-	//
-	// NOTE: this should always be the last check because untrustedVals can be
-	// intentionally made very large to DOS the light client. not the case for
-	// VerifyAdjacent, where validator set is known in advance.
-	if err := untrustedVals.IBCVerifyCommitLight(chainID, untrustedHeader.Commit.BlockID, untrustedHeader.Height,
-		untrustedHeader.Commit); err != nil {
-		return ErrInvalidHeader{err}
-	}
-
-	return nil
+	return commonVerifyNonAdjacent(
+		chainID, trustedHeader, trustedVals, untrustedHeader,
+		untrustedVals, trustingPeriod, now, maxClockDrift, trustLevel, true)
 }
 
 func IBCVerifyAdjacent(
@@ -90,78 +51,12 @@ func IBCVerifyAdjacent(
 	now time.Time,
 	maxClockDrift time.Duration) error {
 
-	if untrustedHeader.Height != trustedHeader.Height+1 {
-		return errors.New("headers must be adjacent in height")
-	}
-
-	if HeaderExpired(trustedHeader, trustingPeriod, now) {
-		return ErrOldHeaderExpired{trustedHeader.Time.Add(trustingPeriod), now}
-	}
-
-	if err := ibcVerifyNewHeaderAndVals(
+	return commonVerifyAdjacent(
 		chainID,
-		untrustedHeader, untrustedVals,
-		trustedHeader,
-		now, maxClockDrift); err != nil {
-		return ErrInvalidHeader{err}
-	}
-
-	// Check the validator hashes are the same
-	if !bytes.Equal(untrustedHeader.ValidatorsHash, trustedHeader.NextValidatorsHash) {
-		err := errpkg.Errorf("expected old header next validators (%X) to match those from new header (%X)",
-			trustedHeader.NextValidatorsHash,
-			untrustedHeader.ValidatorsHash,
-		)
-		return err
-	}
-
-	// Ensure that +2/3 of new validators signed correctly.
-	if err := untrustedVals.IBCVerifyCommitLight(chainID, untrustedHeader.Commit.BlockID, untrustedHeader.Height,
-		untrustedHeader.Commit); err != nil {
-		return ErrInvalidHeader{err}
-	}
-
-	return nil
-}
-
-func ibcVerifyNewHeaderAndVals(
-	chainID string,
-	untrustedHeader *types.SignedHeader,
-	untrustedVals *types.ValidatorSet,
-	trustedHeader *types.SignedHeader,
-	now time.Time,
-	maxClockDrift time.Duration) error {
-
-	if err := untrustedHeader.ValidateBasicForIBC(chainID); err != nil {
-		return errpkg.Wrap(err, "untrustedHeader.ValidateBasic failed")
-	}
-
-	if untrustedHeader.Height <= trustedHeader.Height {
-		return errpkg.Errorf("expected new header height %d to be greater than one of old header %d",
-			untrustedHeader.Height,
-			trustedHeader.Height)
-	}
-
-	if !untrustedHeader.Time.After(trustedHeader.Time) {
-		return errpkg.Errorf("expected new header time %v to be after old header time %v",
-			untrustedHeader.Time,
-			trustedHeader.Time)
-	}
-
-	if !untrustedHeader.Time.Before(now.Add(maxClockDrift)) {
-		return errpkg.Errorf("new header has a time from the future %v (now: %v; max clock drift: %v)",
-			untrustedHeader.Time,
-			now,
-			maxClockDrift)
-	}
-
-	if !bytes.Equal(untrustedHeader.ValidatorsHash, untrustedVals.IBCHash()) {
-		return errpkg.Errorf("expected new header validators (%X) to match those that were supplied (%X) at height %d",
-			untrustedHeader.ValidatorsHash,
-			untrustedVals.Hash(untrustedHeader.Height),
-			untrustedHeader.Height,
-		)
-	}
-
-	return nil
+		trustedHeader,   // height=X
+		untrustedHeader, // height=X+1
+		untrustedVals,   // height=X+1
+		trustingPeriod,
+		now,
+		maxClockDrift, true)
 }
--- libs/tendermint/types/validator_set_ibc.go
+++ libs/tendermint/types/validator_set_ibc.go
@@ -2,70 +2,14 @@ package types
 
 import (
 	"bytes"
-	"fmt"
 	"github.com/okex/exchain/libs/tendermint/crypto/merkle"
 	tmmath "github.com/okex/exchain/libs/tendermint/libs/math"
-	"github.com/pkg/errors"
 )
 
 func (vals *ValidatorSet) IBCVerifyCommitLightTrusting(chainID string, blockID BlockID,
 	height int64, commit *Commit, trustLevel tmmath.Fraction) error {
 
-	// sanity check
-	if trustLevel.Numerator*3 < trustLevel.Denominator || // < 1/3
-		trustLevel.Numerator > trustLevel.Denominator { // > 1
-		panic(fmt.Sprintf("trustLevel must be within [1/3, 1], given %v", trustLevel))
-	}
-
-	if err := verifyCommitBasic(commit, height, blockID); err != nil {
-		return err
-	}
-
-	var (
-		talliedVotingPower int64
-		seenVals           = make(map[int]int, len(commit.Signatures)) // validator index -> commit index
-	)
-
-	// Safely calculate voting power needed.
-	totalVotingPowerMulByNumerator, overflow := safeMul(vals.TotalVotingPower(), trustLevel.Numerator)
-	if overflow {
-		return errors.New("int64 overflow while calculating voting power needed. please provide smaller trustLevel numerator")
-	}
-	votingPowerNeeded := totalVotingPowerMulByNumerator / trustLevel.Denominator
-
-	for idx, commitSig := range commit.Signatures {
-		// No need to verify absent or nil votes.
-		if !commitSig.ForBlock() {
-			continue
-		}
-
-		// We don't know the validators that committed this block, so we have to
-		// check for each vote if its validator is already known.
-		valIdx, val := vals.IBCGetByAddress(commitSig.ValidatorAddress)
-
-		if val != nil {
-			// check for double vote of validator on the same commit
-			if firstIndex, ok := seenVals[valIdx]; ok {
-				secondIndex := idx
-				return errors.Errorf("double vote from %v (%d and %d)", val, firstIndex, secondIndex)
-			}
-			seenVals[valIdx] = idx
-
-			// Validate signature.
-			voteSignBytes := commit.IBCVoteSignBytes(chainID, idx)
-			if !val.PubKey.VerifyBytes(voteSignBytes, commitSig.Signature) {
-				return errors.Errorf("wrong signature (#%d): %X", idx, commitSig.Signature)
-			}
-
-			talliedVotingPower += val.VotingPower
-
-			if talliedVotingPower > votingPowerNeeded {
-				return nil
-			}
-		}
-	}
-
-	return ErrNotEnoughVotingPowerSigned{Got: talliedVotingPower, Needed: votingPowerNeeded}
+	return vals.commonVerifyCommitLightTrusting(chainID, blockID, height, commit, trustLevel, true)
 }
 
 func (vals *ValidatorSet) IBCGetByAddress(address []byte) (index int, val *Validator) {
@@ -91,44 +35,5 @@ func (vals *ValidatorSet) IBCHash() []byte {
 func (vals *ValidatorSet) IBCVerifyCommitLight(chainID string, blockID BlockID,
 	height int64, commit *Commit) error {
 
-	if vals.Size() != len(commit.Signatures) {
-		return NewErrInvalidCommitSignatures(vals.Size(), len(commit.Signatures))
-	}
-
-	// Validate Height and BlockID.
-	if height != commit.Height {
-		return NewErrInvalidCommitHeight(height, commit.Height)
-	}
-	if !blockID.Equals(commit.BlockID) {
-		return fmt.Errorf("invalid commit -- wrong block ID: want %v, got %v",
-			blockID, commit.BlockID)
-	}
-
-	talliedVotingPower := int64(0)
-	votingPowerNeeded := vals.TotalVotingPower() * 2 / 3
-	for idx, commitSig := range commit.Signatures {
-		// No need to verify absent or nil votes.
-		if !commitSig.ForBlock() {
-			continue
-		}
-
-		// The vals and commit have a 1-to-1 correspondance.
-		// This means we don't need the validator address or to do any lookup.
-		val := vals.Validators[idx]
-
-		// Validate signature.
-		voteSignBytes := commit.IBCVoteSignBytes(chainID, idx)
-		if !val.PubKey.VerifyBytes(voteSignBytes, commitSig.Signature) {
-			return fmt.Errorf("wrong signature (#%d): %X", idx, commitSig.Signature)
-		}
-
-		talliedVotingPower += val.VotingPower
-
-		// return as soon as +2/3 of the signatures are verified
-		if talliedVotingPower > votingPowerNeeded {
-			return nil
-		}
-	}
-
-	return ErrNotEnoughVotingPowerSigned{Got: talliedVotingPower, Needed: votingPowerNeeded}
+	return vals.commonVerifyCommitLight(chainID, blockID, height, commit, true)
 }
