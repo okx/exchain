46074eeb4ba7b66370f8ba88addf8b83a1c2b2aa
cwbhhjl <cwbhhjl@gmail.com>
2022-06-06 15:55:56 +0800

    leafhash & innerhash with sync.Pool

--- libs/tendermint/crypto/merkle/hash.go
+++ libs/tendermint/crypto/merkle/hash.go
@@ -1,21 +1,44 @@
 package merkle
 
 import (
+	"bytes"
 	"github.com/okex/exchain/libs/tendermint/crypto/tmhash"
+	"sync"
 )
 
 // TODO: make these have a large predefined capacity
 var (
 	leafPrefix  = []byte{0}
 	innerPrefix = []byte{1}
+
+	hashBytesPool = &sync.Pool{
+		New: func() interface{} {
+			return &bytes.Buffer{}
+		},
+	}
 )
 
 // returns tmhash(0x00 || leaf)
 func leafHash(leaf []byte) []byte {
-	return tmhash.Sum(append(leafPrefix, leaf...))
+	buf := hashBytesPool.Get().(*bytes.Buffer)
+	buf.Reset()
+	buf.Grow(len(leafPrefix) + len(leaf))
+	buf.Write(leafPrefix)
+	buf.Write(leaf)
+	h := tmhash.Sum(buf.Bytes())
+	hashBytesPool.Put(buf)
+	return h
 }
 
 // returns tmhash(0x01 || left || right)
 func innerHash(left []byte, right []byte) []byte {
-	return tmhash.Sum(append(innerPrefix, append(left, right...)...))
+	buf := hashBytesPool.Get().(*bytes.Buffer)
+	buf.Reset()
+	buf.Grow(len(innerPrefix) + len(left) + len(right))
+	buf.Write(innerPrefix)
+	buf.Write(left)
+	buf.Write(right)
+	h := tmhash.Sum(buf.Bytes())
+	hashBytesPool.Put(buf)
+	return h
 }
