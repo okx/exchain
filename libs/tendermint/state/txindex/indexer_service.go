4bc2779d7af8659fafcdd11d9666f32ba249aead
worm <lcmmhcc@126.com>
2022-06-07 16:37:33 +0800

    graceful exit for indexer_service

--- libs/tendermint/state/txindex/indexer_service.go
+++ libs/tendermint/state/txindex/indexer_service.go
@@ -19,6 +19,7 @@ type IndexerService struct {
 
 	idr      TxIndexer
 	eventBus *types.EventBus
+	quit     chan struct{}
 }
 
 // NewIndexerService returns a new service instance.
@@ -49,7 +50,7 @@ func (is *IndexerService) OnStart() error {
 	}
 
 	go func() {
-		for {
+		for is.IsRunning() {
 			msg := <-blockHeadersSub.Out()
 			eventDataHeader := msg.Data().(types.EventDataNewBlockHeader)
 			height := eventDataHeader.Header.Height
@@ -70,6 +71,7 @@ func (is *IndexerService) OnStart() error {
 				is.Logger.Info("Indexed block", "height", height)
 			}
 		}
+		close(is.quit)
 	}()
 	return nil
 }
@@ -79,4 +81,8 @@ func (is *IndexerService) OnStop() {
 	if is.eventBus.IsRunning() {
 		_ = is.eventBus.UnsubscribeAll(context.Background(), subscriber)
 	}
+}
+
+func (is *IndexerService) Wait() {
+	<-is.quit
 }
