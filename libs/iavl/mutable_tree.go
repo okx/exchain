bd729294673b49c6854be1ecc2ce93fdc2c82e91
worm <lcmmhcc@126.com>
2022-06-07 16:14:10 +0800

    rename VersionExistsInDb

--- libs/iavl/mutable_tree.go
+++ libs/iavl/mutable_tree.go
@@ -126,7 +126,7 @@ func (tree *MutableTree) VersionExists(version int64) bool {
 	}
 	return tree.versions.Get(version)
 }
-func (tree *MutableTree) VersionExistsInNdb(version int64) bool {
+func (tree *MutableTree) VersionExistsInDb(version int64) bool {
 	treeVer := tree.ndb.getPreviousVersion(version + 1)
 	return treeVer == version
 }
