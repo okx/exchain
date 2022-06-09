d2d3e4356acd19103a38be75ead9fdcd944d31a1
zhangkai <zhangkai.gis@163.com>
2022-06-09 21:26:32 +0800

    init new

--- x/evm/types/statedb_pool.go
+++ x/evm/types/statedb_pool.go
@@ -1,9 +1,22 @@
 package types
 
-import "sync"
+import (
+	"sync"
+
+	ethcmn "github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+)
 
 var CommitStateDBPool = sync.Pool{
 	New: func() interface{} {
-		return new(CommitStateDB)
+		return &CommitStateDB{
+			stateObjects:        make(map[ethcmn.Address]*stateObject),
+			stateObjectsPending: make(map[ethcmn.Address]struct{}),
+			stateObjectsDirty:   make(map[ethcmn.Address]struct{}),
+			preimages:           make(map[ethcmn.Hash][]byte),
+			logs:                make(map[ethcmn.Hash][]*ethtypes.Log),
+			codeCache:           make(map[ethcmn.Address]CacheCode, 0),
+			updatedAccount:      make(map[ethcmn.Address]struct{}),
+		}
 	},
 }
