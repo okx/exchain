7cf8ec3d3ef02c049246b76a41fd53108af50271
evan.han <iloversevan@gmail.com>
2022-06-20 18:15:30 +0800

    use sync.Pool to cache MultiStore

--- app/rpc/namespaces/eth/simulation/evm.go
+++ app/rpc/namespaces/eth/simulation/evm.go
@@ -1,6 +1,7 @@
 package simulation
 
 import (
+	"sync"
 	"time"
 
 	"github.com/ethereum/go-ethereum/common"
@@ -22,11 +23,17 @@ type EvmFactory struct {
 	WrappedQuerier *watcher.Querier
 	storeKey       *sdk.KVStoreKey
 	cms            sdk.CommitMultiStore
+	storePool      sync.Pool
 }
 
 func NewEvmFactory(chainId string, q *watcher.Querier) EvmFactory {
 	ef := EvmFactory{ChainId: chainId, WrappedQuerier: q, storeKey: sdk.NewKVStoreKey(evm.StoreKey)}
 	ef.cms = initCommitMultiStore(ef.storeKey)
+	ef.storePool = sync.Pool{
+		New: func() interface{} {
+			return ef.cms.CacheMultiStore()
+		},
+	}
 	return ef
 }
 
@@ -42,6 +49,11 @@ func initCommitMultiStore(storeKey *sdk.KVStoreKey) sdk.CommitMultiStore {
 	cms.MountStoreWithDB(paramsTKey, sdk.StoreTypeTransient, db)
 	cms.LoadLatestVersion()
 	return cms
+}
+
+func (ef *EvmFactory) PutBackStorePool(multiStore sdk.CacheMultiStore) {
+	multiStore.Clear()
+	ef.storePool.Put(multiStore)
 }
 
 func (ef EvmFactory) BuildSimulator(qoc QueryOnChainProxy) *EvmSimulator {
@@ -75,7 +87,8 @@ func (ef EvmFactory) BuildSimulator(qoc QueryOnChainProxy) *EvmSimulator {
 		Hash: hash.Bytes(),
 	}
 
-	ctx := ef.makeContext(req.Header)
+	multiStore := ef.storePool.Get().(sdk.CacheMultiStore)
+	ctx := ef.makeContext(multiStore, req.Header)
 
 	keeper.BeginBlock(ctx, req)
 
@@ -91,14 +104,15 @@ type EvmSimulator struct {
 }
 
 // DoCall call simulate tx. we pass the sender by args to reduce address convert
-func (es *EvmSimulator) DoCall(msg *evmtypes.MsgEthereumTx, sender string, overridesBytes []byte) (*sdk.SimulationResponse, error) {
+func (es *EvmSimulator) DoCall(msg *evmtypes.MsgEthereumTx, sender string, overridesBytes []byte, callBack func(sdk.CacheMultiStore)) (*sdk.SimulationResponse, error) {
+	defer callBack(es.ctx.MultiStore().(sdk.CacheMultiStore))
 	es.ctx.SetFrom(sender)
 	if overridesBytes != nil {
 		es.ctx.SetOverrideBytes(overridesBytes)
 	}
-	r, e := es.handler(es.ctx, msg)
-	if e != nil {
-		return nil, e
+	r, err := es.handler(es.ctx, msg)
+	if err != nil {
+		return nil, err
 	}
 	return &sdk.SimulationResponse{
 		GasInfo: sdk.GasInfo{
@@ -116,8 +130,8 @@ func (ef EvmFactory) makeEvmKeeper(qoc QueryOnChainProxy) *evm.Keeper {
 	return evm.NewSimulateKeeper(cdc, ef.storeKey, NewSubspaceProxy(), NewAccountKeeperProxy(qoc), SupplyKeeperProxy{}, NewBankKeeperProxy(), NewInternalDba(qoc), tmlog.NewNopLogger())
 }
 
-func (ef EvmFactory) makeContext(header abci.Header) sdk.Context {
-	ctx := sdk.NewContext(ef.cms.CacheMultiStore(), header, true, tmlog.NewNopLogger())
+func (ef EvmFactory) makeContext(multiStore sdk.CacheMultiStore, header abci.Header) sdk.Context {
+	ctx := sdk.NewContext(multiStore, header, true, tmlog.NewNopLogger())
 	ctx.SetGasMeter(sdk.NewInfiniteGasMeter())
 	return ctx
 }
