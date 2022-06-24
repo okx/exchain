6337d4b64efc3baa412b81444b6057e8e00df497
Ywmet <xiaoliang.zhang@okg.com>
2022-06-24 08:19:36 +0800

    rm no use

--- x/erc20/watcher/watcher.go
+++ x/erc20/watcher/watcher.go
@@ -2,9 +2,7 @@ package watcher
 
 import (
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
-	clientcontext "github.com/okex/exchain/libs/cosmos-sdk/client/context"
 	"github.com/okex/exchain/x/erc20/types"
 	"github.com/spf13/viper"
 	"strings"
@@ -53,30 +51,6 @@ func (w *Watcher) LoadTokenMapping(f func(func(denom, contract string) bool)) {
 	if atomic.CompareAndSwapInt32(&isLoadHistory, 0, 1) {
 		f(w.LoadItem)
 	}
-}
-
-func (w *Watcher) LoadHistoryTokenMapping(ctx clientcontext.CLIContext) error {
-	if atomic.CompareAndSwapInt32(&isLoadHistory, 0, 1) {
-		queryPath := fmt.Sprintf("custom/erc20/token-mapping")
-		res, _, err := ctx.QueryWithData(queryPath, []byte{})
-		if err != nil {
-			return err
-		}
-		var mapping []types.TokenMapping
-		err = json.Unmarshal(res, &mapping)
-		if err != nil {
-			return err
-		}
-		for _, v := range mapping {
-			//rm 0x prefix
-			contract, err := hex.DecodeString(v.Contract[2:])
-			if err != nil {
-				return err
-			}
-			w.SetContractToDenom(contract, []byte(v.Denom))
-		}
-	}
-	return nil
 }
 
 func (w *Watcher) SetContractToDenom(key, value []byte) {
