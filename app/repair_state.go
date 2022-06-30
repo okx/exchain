7aa9ddb989847470e07a594b0fc74cdad1b65bf2
evan.han <iloversevan@gmail.com>
2022-06-30 16:05:45 +0800

    fix repair-state with async saving state

--- app/repair_state.go
+++ app/repair_state.go
@@ -196,6 +196,8 @@ func doRepair(ctx *server.Context, state sm.State, stateStoreDB dbm.DB,
 	}()
 	blockExec := sm.NewBlockExecutor(stateStoreDB, ctx.Logger, proxyApp.Consensus(), mock.Mempool{}, sm.MockEvidencePool{})
 	blockExec.SetEventBus(eventBus)
+	//Sync save db during replay
+	blockExec.SetIsAsyncSaveDB(false)
 	global.SetGlobalHeight(startHeight + 1)
 	for height := startHeight + 1; height <= latestHeight; height++ {
 		repairBlock, repairBlockMeta := loadBlock(height, dataDir)
