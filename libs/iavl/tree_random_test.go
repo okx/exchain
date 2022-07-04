2b56040c7bc04fe62fc57d84ff05ed76bddff008
zhangkai <zhangkai.gis@163.com>
2022-07-04 21:29:08 +0800

    fix ut

--- libs/iavl/tree_random_test.go
+++ libs/iavl/tree_random_test.go
@@ -7,6 +7,8 @@ import (
 	"math/rand"
 	"os"
 	"sort"
+	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/require"
@@ -333,21 +335,51 @@ func assertEmptyDatabase(t *testing.T, tree *MutableTree) {
 	require.NoError(t, err)
 
 	var (
-		firstKey []byte
-		count    int
+		foundKeys []string
 	)
 	for ; iter.Valid(); iter.Next() {
-		count++
-		if firstKey == nil {
-			firstKey = iter.Key()
-		}
+		foundKeys = append(foundKeys, string(iter.Key()))
 	}
 	require.NoError(t, iter.Error())
-	require.EqualValues(t, 1, count, "Found %v database entries, expected 1", count)
+	require.EqualValues(t, 2, len(foundKeys), "Found %v database entries, expected 1", len(foundKeys)) // 1 for storage version and 1 for root
+
+	firstKey := foundKeys[0]
+	secondKey := foundKeys[1]
+
+	require.True(t, strings.HasPrefix(firstKey, metadataKeyFormat.Prefix()))
+	require.True(t, strings.HasPrefix(secondKey, rootKeyFormat.Prefix()))
+
+	require.Equal(t, string(metadataKeyFormat.KeyBytes([]byte(storageVersionKey))), firstKey, "Unexpected storage version key")
+
+	storageVersionValue, err := tree.ndb.db.Get([]byte(firstKey))
+	require.NoError(t, err)
+	latestVersion := tree.ndb.getLatestVersion()
+	require.Equal(t, fastStorageVersionValue+fastStorageVersionDelimiter+strconv.Itoa(int(latestVersion)), string(storageVersionValue))
 
 	var foundVersion int64
-	rootKeyFormat.Scan(firstKey, &foundVersion)
+	rootKeyFormat.Scan([]byte(secondKey), &foundVersion)
 	require.Equal(t, version, foundVersion, "Unexpected root version")
+
+	//	version := tree.Version()
+	//	iter, err := tree.ndb.db.Iterator(nil, nil)
+	//	require.NoError(t, err)
+	//
+	//	var (
+	//		firstKey []byte
+	//		count    int
+	//	)
+	//	for ; iter.Valid(); iter.Next() {
+	//		count++
+	//		if firstKey == nil {
+	//			firstKey = iter.Key()
+	//		}
+	//	}
+	//	require.NoError(t, iter.Error())
+	//	require.EqualValues(t, 1, count, "Found %v database entries, expected 1", count)
+	//
+	//	var foundVersion int64
+	//	rootKeyFormat.Scan(firstKey, &foundVersion)
+	//	require.Equal(t, version, foundVersion, "Unexpected root version")
 }
 
 // Checks that the tree has the given number of orphan nodes.
