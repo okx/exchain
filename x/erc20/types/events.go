4c4c91f91ab62bf2af1c3156ad23974a8b0bf061
ylsGit <bzwdyls@163.com>
2022-07-11 15:35:58 +0800

    rename

--- x/erc20/types/events.go
+++ x/erc20/types/events.go
@@ -3,10 +3,10 @@ package types
 const (
 	EventTypDeployModuleERC20 = "deploy_erc20_contract"
 	EventTypCallModuleERC20   = "call_erc20_contract"
-	EventTypLock              = "lock"
-	EventTypUnlock            = "unlock"
-	EventTypMint              = "mint"
-	EventTypBurn              = "burn"
+	EventTypLock              = "erc20_lock"
+	EventTypUnlock            = "erc20_unlock"
+	EventTypMint              = "erc20_mint"
+	EventTypBurn              = "erc20_burn"
 
 	AttributeKeyContractAddr   = "contract_address"
 	AttributeKeyContractMethod = "contract_method"
