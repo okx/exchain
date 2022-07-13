84685febd13f6fb2c083a4499d5c799a7d011187
zhangkai <zhangkai.gis@163.com>
2022-07-13 15:24:00 +0800

    add enable fast storage

--- libs/iavl/mutable_tree_oec.go
+++ libs/iavl/mutable_tree_oec.go
@@ -79,21 +79,23 @@ func (tree *MutableTree) SaveVersionAsync(version int64, useDeltas bool) ([]byte
 
 	tree.ndb.updateLatestVersion4FastNode(version)
 	if shouldPersist {
-		tree.unsavedFastNodeAdditionsDelKey = tree.unsavedFastNodeAdditionsDelKey[:0]
+		if EnableFastStorage {
+			tree.unsavedFastNodeAdditionsDelKey = tree.unsavedFastNodeAdditionsDelKey[:0]
 
-		tree.mtxUnSavedFastNodeAdditions.Lock()
-		for _, v := range tree.unsavedFastNodeAdditions {
-			tree.unsavedFastNodeAdditionsDelKey = append(tree.unsavedFastNodeAdditionsDelKey, &FastNode{key: v.key, versionLastUpdatedAt: v.versionLastUpdatedAt})
+			tree.mtxUnSavedFastNodeAdditions.Lock()
+			for _, v := range tree.unsavedFastNodeAdditions {
+				tree.unsavedFastNodeAdditionsDelKey = append(tree.unsavedFastNodeAdditionsDelKey, &FastNode{key: v.key, versionLastUpdatedAt: v.versionLastUpdatedAt})
+			}
+			tree.mtxUnSavedFastNodeAdditions.Unlock()
+
+			tree.unsavedFastNodeRemovalsDelKey = tree.unsavedFastNodeRemovalsDelKey[:0]
+
+			tree.mtxUnsavedFastNodeRemovalsDelKey.Lock()
+			for k, _ := range tree.unsavedFastNodeRemovals {
+				tree.unsavedFastNodeRemovalsDelKey = append(tree.unsavedFastNodeRemovalsDelKey, k)
+			}
+			tree.mtxUnsavedFastNodeRemovalsDelKey.Unlock()
 		}
-		tree.mtxUnSavedFastNodeAdditions.Unlock()
-
-		tree.unsavedFastNodeRemovalsDelKey = tree.unsavedFastNodeRemovalsDelKey[:0]
-
-		tree.mtxUnsavedFastNodeRemovalsDelKey.Lock()
-		for k, _ := range tree.unsavedFastNodeRemovals {
-			tree.unsavedFastNodeRemovalsDelKey = append(tree.unsavedFastNodeRemovalsDelKey, k)
-		}
-		tree.mtxUnsavedFastNodeRemovalsDelKey.Unlock()
 
 		tree.ndb.saveNewOrphans(version, tree.orphans, true)
 		tree.persist(version)
@@ -389,25 +391,27 @@ func (tree *MutableTree) persistTpp(event *commitEvent, trc *trace.Tracer) {
 	if err := tree.saveFastNodeVersion(batch); err != nil {
 		panic(err)
 	}
-	tree.mtxUnSavedFastNodeAdditions.Lock()
-	tree.mtxUnsavedFastNodeAdditionsDelKey.Lock()
-	for _, v := range tree.unsavedFastNodeAdditionsDelKey {
-		if fastNode, ok := tree.unsavedFastNodeAdditions[string(v.key)]; ok {
-			if fastNode.versionLastUpdatedAt == v.versionLastUpdatedAt {
-				delete(tree.unsavedFastNodeAdditions, string(v.key))
+	if EnableFastStorage {
+		tree.mtxUnSavedFastNodeAdditions.Lock()
+		tree.mtxUnsavedFastNodeAdditionsDelKey.Lock()
+		for _, v := range tree.unsavedFastNodeAdditionsDelKey {
+			if fastNode, ok := tree.unsavedFastNodeAdditions[string(v.key)]; ok {
+				if fastNode.versionLastUpdatedAt == v.versionLastUpdatedAt {
+					delete(tree.unsavedFastNodeAdditions, string(v.key))
+				}
 			}
 		}
-	}
-	tree.mtxUnsavedFastNodeAdditionsDelKey.Unlock()
-	tree.mtxUnSavedFastNodeAdditions.Unlock()
+		tree.mtxUnsavedFastNodeAdditionsDelKey.Unlock()
+		tree.mtxUnSavedFastNodeAdditions.Unlock()
 
-	tree.mtxUnSavedFastNodeRemovals.Lock()
-	tree.mtxUnsavedFastNodeRemovalsDelKey.Lock()
-	for _, v := range tree.unsavedFastNodeRemovalsDelKey {
-		delete(tree.unsavedFastNodeRemovals, v)
+		tree.mtxUnSavedFastNodeRemovals.Lock()
+		tree.mtxUnsavedFastNodeRemovalsDelKey.Lock()
+		for _, v := range tree.unsavedFastNodeRemovalsDelKey {
+			delete(tree.unsavedFastNodeRemovals, v)
+		}
+		tree.mtxUnsavedFastNodeRemovalsDelKey.Unlock()
+		tree.mtxUnSavedFastNodeRemovals.Unlock()
 	}
-	tree.mtxUnsavedFastNodeRemovalsDelKey.Unlock()
-	tree.mtxUnSavedFastNodeRemovals.Unlock()
 
 	trc.Pin("batchCommit")
 	if err := ndb.Commit(batch); err != nil {
