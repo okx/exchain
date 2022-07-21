20a2bae430f8017e70b1ced738e2d971831b1451
jianguo <zjg555543@163.com>
2022-07-21 23:19:12 +0800

    add ut for withdraw reward enabled proposal

--- x/distribution/keeper/proposal_handler_test.go
+++ x/distribution/keeper/proposal_handler_test.go
@@ -31,3 +31,32 @@ func TestHandleChangeDistributionTypeProposal(t *testing.T) {
 	require.Nil(t, err)
 	require.Equal(t, types.DistributionTypeOnChain, dk.GetDistributionType(ctx))
 }
+
+func TestHandleWithdrawRewardEnabledProposal(t *testing.T) {
+	communityTax := sdk.NewDecWithPrec(2, 2)
+	tmtypes.UnittestOnlySetMilestoneVenus3Height(0)
+	ctx, _, _, dk, sk, _, _ := CreateTestInputAdvanced(t, false, 1000, communityTax)
+	// create validator
+	DoCreateValidator(t, ctx, sk, valOpAddr1, valConsPk1)
+	// next block
+	ctx.SetBlockHeight(ctx.BlockHeight() + 1)
+	require.Equal(t, true, dk.GetWithdrawRewardEnabled(ctx))
+
+	//set withdraw reward proposal false
+	proposal := types.NewWithdrawRewardEnabledProposal("title", "description", false)
+	tmtypes.UnittestOnlySetMilestoneVenus3Height(-1)
+	err := HandleWithdrawRewardEnabledProposal(ctx, dk, proposal)
+	require.Nil(t, err)
+	require.Equal(t, false, dk.GetWithdrawRewardEnabled(ctx))
+
+	//set withdraw reward proposal true
+	proposal.Enabled = true
+	err = HandleWithdrawRewardEnabledProposal(ctx, dk, proposal)
+	require.Nil(t, err)
+	require.Equal(t, true, dk.GetWithdrawRewardEnabled(ctx))
+
+	//set withdraw reward proposal true, same
+	err = HandleWithdrawRewardEnabledProposal(ctx, dk, proposal)
+	require.Nil(t, err)
+	require.Equal(t, true, dk.GetWithdrawRewardEnabled(ctx))
+}
