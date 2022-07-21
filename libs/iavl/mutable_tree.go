a7396c89f6f112f6e4c8f872b32205d98fc5ccbb
zhangkai <zhangkai.gis@163.com>
2022-07-21 13:01:50 +0800

    chore rm print and restore reuse memory

--- libs/iavl/mutable_tree.go
+++ libs/iavl/mutable_tree.go
@@ -180,10 +180,12 @@ func (tree *MutableTree) prepareOrphansSlice() []*Node {
 
 // Set sets a key in the working tree. Nil values are invalid. The given key/value byte slices must
 // not be modified after this call, since they point to slices stored within IAVL.
-func (tree *MutableTree) Set(key, value []byte) (updated bool) {
-	var orphaned []*Node
-	orphaned, updated = tree.set(key, value)
+func (tree *MutableTree) Set(key, value []byte) bool {
+	// orphaned, updated := tree.set(key, value) // old code
+	orphaned := tree.makeOrphansSliceReady()
+	updated := tree.setWithOrphansSlice(key, value, &orphaned)
 	tree.addOrphans(orphaned)
+
 	return updated
 }
 
@@ -313,7 +315,9 @@ func (tree *MutableTree) recursiveSet(node *Node, key []byte, value []byte, orph
 // Remove removes a key from the working tree. The given key byte slice should not be modified
 // after this call, since it may point to data stored inside IAVL.
 func (tree *MutableTree) Remove(key []byte) ([]byte, bool) {
-	val, orphaned, removed := tree.remove(key)
+	// val, orphaned, removed := tree.remove(key) // old code
+	orphaned := tree.makeOrphansSliceReady()
+	val, removed := tree.removeWithOrphansSlice(key, &orphaned)
 	tree.addOrphans(orphaned)
 
 	return val, removed
