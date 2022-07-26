63f644364d929fdfa0366e3dbdc81b132a35ea88
cwbhhjl <cwbhhjl@gmail.com>
2022-07-26 16:34:42 +0800

    update

--- libs/tendermint/mempool/reactor.go
+++ libs/tendermint/mempool/reactor.go
@@ -238,7 +238,7 @@ func (memR *Reactor) GetChannels() []*p2p.ChannelDescriptor {
 // It starts a broadcast routine ensuring all txs are forwarded to the given peer.
 func (memR *Reactor) AddPeer(peer p2p.Peer) {
 	go memR.broadcastTxRoutine(peer)
-	memR.sendTxReceiverInfo(peer)
+	go memR.sendTxReceiverInfo(peer)
 }
 
 // RemovePeer implements Reactor.
@@ -502,9 +502,24 @@ func (memR *Reactor) sendTxReceiverInfo(peer p2p.Peer) {
 		memR.Logger.Error("sendTxReceiverInfo:marshal", "error", err)
 		return
 	}
-	ok := peer.Send(TxReceiverChannel, bz)
-	if !ok {
-		memR.Logger.Error("sendTxReceiverInfo:send not ok")
+
+	for {
+		// make sure the peer is up to date
+		_, ok := peer.Get(types.PeerStateKey).(PeerState)
+		if !ok {
+			// Peer does not have a state yet. We set it in the consensus reactor, but
+			// when we add peer in Switch, the order we call reactors#AddPeer is
+			// different every time due to us using a map. Sometimes other reactors
+			// will be initialized before the consensus reactor. We should wait a few
+			// milliseconds and retry.
+			time.Sleep(peerCatchupSleepIntervalMS * time.Millisecond)
+			continue
+		}
+		ok = peer.Send(TxReceiverChannel, bz)
+		if !ok {
+			memR.Logger.Error("sendTxReceiverInfo:send not ok")
+		}
+		return
 	}
 }
 
