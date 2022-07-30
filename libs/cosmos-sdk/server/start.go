c1b799195076dbad517d0788e43876b69d870964
KamiD <44460798+KamiD@users.noreply.github.com>
2022-07-30 11:57:26 +0800

    revert ac commit swich

--- libs/cosmos-sdk/server/start.go
+++ libs/cosmos-sdk/server/start.go
@@ -252,7 +252,7 @@ func SetExternalPackageValue(cmd *cobra.Command) {
 	tmiavl.MinCommitItemCount = viper.GetInt64(tmiavl.FlagIavlMinCommitItemCount)
 	tmiavl.HeightOrphansCacheSize = viper.GetInt(tmiavl.FlagIavlHeightOrphansCacheSize)
 	tmiavl.MaxCommittedHeightNum = viper.GetInt(tmiavl.FlagIavlMaxCommittedHeightNum)
-	tmiavl.EnableAsyncCommit = true
+	tmiavl.EnableAsyncCommit = viper.GetBool(tmiavl.FlagIavlEnableAsyncCommit)
 	system.EnableGid = viper.GetBool(system.FlagEnableGid)
 
 	state.ApplyBlockPprofTime = viper.GetInt(state.FlagApplyBlockPprofTime)
