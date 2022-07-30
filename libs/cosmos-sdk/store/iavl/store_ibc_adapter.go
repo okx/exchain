a0e7b9a30fddb273ac4848e25eb89cfe0da9394f
itsfunny <18757883747@163.com>
2022-07-30 12:11:02 +0800

    revert store refactor

--- libs/cosmos-sdk/store/iavl/store_ibc_adapter.go
+++ libs/cosmos-sdk/store/iavl/store_ibc_adapter.go
@@ -18,25 +18,36 @@ func (st *Store) queryKeyForIBC(req abci.RequestQuery) (res abci.ResponseQuery) 
 		return sdkerrors.QueryResult(sdkerrors.Wrap(sdkerrors.ErrTxDecode, "query cannot be zero length"))
 	}
 
+	tree := st.tree
+
 	// store the height we chose in the response, with 0 being changed to the
 	// latest height
-	res.Height = getHeight(st.tree, req)
-	tree, err := st.tree.GetImmutable(req.Height)
-	if err != nil {
-		return sdkerrors.QueryResult(sdkerrors.Wrapf(iavl.ErrVersionDoesNotExist, "request height %d", req.Height))
-	}
+	res.Height = getHeight(tree, req)
+
 	switch req.Path {
 	case "/key": // get by key
 		key := req.Data // data holds the key bytes
-		res.Key = key
 
-		_, res.Value = tree.Get(key)
+		res.Key = key
+		if !st.VersionExists(res.Height) {
+			res.Log = iavl.ErrVersionDoesNotExist.Error()
+			break
+		}
+
+		_, res.Value = tree.GetVersioned(key, res.Height)
 		if !req.Prove {
 			break
 		}
 
+		// Continue to prove existence/absence of value
+		// Must convert store.Tree to iavl.MutableTree with given version to use in CreateProof
+		iTree, err := tree.GetImmutable(res.Height)
+		if err != nil {
+			// sanity check: If value for given version was retrieved, immutable tree must also be retrievable
+			panic(fmt.Sprintf("version exists in store but could not retrieve corresponding versioned tree in store, %s", err.Error()))
+		}
 		mtree := &iavl.MutableTree{
-			ImmutableTree: tree,
+			ImmutableTree: iTree,
 		}
 
 		// get proof from tree and convert to merkle.Proof before adding to result
