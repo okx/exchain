28a1afb0eb9a793836df8ba1517cbe5714f6b9ac
zhangkai <zhangkai.gis@163.com>
2022-08-02 21:45:41 +0800

    del useless

--- libs/iavl/mutable_tree.go
+++ libs/iavl/mutable_tree.go
@@ -875,6 +875,7 @@ func (tree *MutableTree) SaveVersionSync(version int64, useDeltas bool) ([]byte,
 		}
 	}
 	tree.ndb.updateLatestMemoryVersion(version)
+
 	fnc := &fastNodeChanges{additions: tree.unsavedFastNodeAdditions,
 		removals: tree.unsavedFastNodeRemovals}
 
--- libs/iavl/mutable_tree_oec.go
+++ libs/iavl/mutable_tree_oec.go
@@ -100,7 +100,6 @@ func (tree *MutableTree) SaveVersionAsync(version int64, useDeltas bool) ([]byte
 
 	shouldPersist := ((version%CommitGapHeight == 0) && (version-tree.lastPersistHeight >= CommitGapHeight)) ||
 		(treeMap.totalPpncSize >= MinCommitItemCount)
-	tree.ndb.updateLatestMemoryVersion(version)
 
 	if GetEnableFastStorage() {
 		tree.ndb.updateLatestMemoryVersion(version)
