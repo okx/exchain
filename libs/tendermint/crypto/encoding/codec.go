28f263525c3c955e5d0cc1576079908a023ae008
itsfunny <18757883747@163.com>
2022-08-26 20:36:40 +0800

    fix ibctxdecoder failed to decode secp256k1PubKey add global height change errorcode

--- libs/tendermint/crypto/encoding/codec.go
+++ libs/tendermint/crypto/encoding/codec.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/okex/exchain/libs/tendermint/crypto"
 	"github.com/okex/exchain/libs/tendermint/crypto/ed25519"
+	"github.com/okex/exchain/libs/tendermint/crypto/secp256k1"
 	pc "github.com/okex/exchain/libs/tendermint/proto/crypto/keys"
 )
 
@@ -22,6 +23,12 @@ func PubKeyToProto(k crypto.PubKey) (pc.PublicKey, error) {
 				Ed25519: k[:],
 			},
 		}
+	case secp256k1.PubKeySecp256k1:
+		kp = pc.PublicKey{
+			Sum: &pc.PublicKey_Secp256K1{
+				Secp256K1: k[:],
+			},
+		}
 	default:
 		return kp, fmt.Errorf("toproto: key type %v is not supported", k)
 	}
@@ -29,20 +36,29 @@ func PubKeyToProto(k crypto.PubKey) (pc.PublicKey, error) {
 }
 
 // PubKeyFromProto takes a protobuf Pubkey and transforms it to a crypto.Pubkey
-func PubKeyFromProto(k *pc.PublicKey) (crypto.PubKey, error) {
+// Return one more parameter to prevent from slowing down the procedure
+func PubKeyFromProto(k *pc.PublicKey) (crypto.PubKey, bool, error) {
 	if k == nil {
-		return nil, errors.New("nil PublicKey")
+		return nil, false, errors.New("nil PublicKey")
 	}
 	switch k := k.Sum.(type) {
 	case *pc.PublicKey_Ed25519:
 		if len(k.Ed25519) != ed25519.PubKeyEd25519Size {
-			return nil, fmt.Errorf("invalid size for PubKeyEd25519. Got %d, expected %d",
+			return nil, false, fmt.Errorf("invalid size for PubKeyEd25519. Got %d, expected %d",
 				len(k.Ed25519), ed25519.PubKeyEd25519Size)
 		}
 		var pk ed25519.PubKeyEd25519
 		copy(pk[:], k.Ed25519)
-		return pk, nil
+		return pk, false, nil
+	case *pc.PublicKey_Secp256K1:
+		if len(k.Secp256K1) != secp256k1.PubKeySecp256k1Size {
+			return nil, false, fmt.Errorf("invalid size for PubKeySecp256k1. Got %d, expected %d",
+				len(k.Secp256K1), secp256k1.PubKeySecp256k1Size)
+		}
+		var pk secp256k1.PubKeySecp256k1
+		copy(pk[:], k.Secp256K1)
+		return pk, true, nil
 	default:
-		return nil, fmt.Errorf("fromproto: key type %v is not supported", k)
+		return nil, false, fmt.Errorf("fromproto: key type %v is not supported", k)
 	}
 }
