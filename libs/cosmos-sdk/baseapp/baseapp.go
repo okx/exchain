61dfada986635e042dba22afe3f1772d0656afb9
cwbhhjl <cwbhhjl@gmail.com>
2022-09-06 14:35:51 +0800

    get header from checkState first

--- libs/cosmos-sdk/baseapp/baseapp.go
+++ libs/cosmos-sdk/baseapp/baseapp.go
@@ -713,19 +713,21 @@ func (app *BaseApp) getContextForSimTx(txBytes []byte, height int64) (sdk.Contex
 		return sdk.Context{}, err
 	}
 
-	var abciHeader abci.Header
-	if app.tmClient != nil {
-		var heightForQuery = height
-		var blockMeta *tmtypes.BlockMeta
-		blockMeta, err = app.tmClient.BlockInfo(&heightForQuery)
-		if err == nil {
-			abciHeader = blockHeaderToABCIHeader(blockMeta.Header)
+	var abciHeader = app.checkState.ctx.BlockHeader()
+	if abciHeader.Height != height {
+		if app.tmClient != nil {
+			var heightForQuery = height
+			var blockMeta *tmtypes.BlockMeta
+			blockMeta, err = app.tmClient.BlockInfo(&heightForQuery)
+			if err == nil {
+				abciHeader = blockHeaderToABCIHeader(blockMeta.Header)
+			}
 		}
-	}
-	if app.tmClient == nil || err != nil {
-		abciHeader, err = GetABCIHeader(height)
-		if err != nil {
-			return sdk.Context{}, err
+		if app.tmClient == nil || err != nil {
+			abciHeader, err = GetABCIHeader(height)
+			if err != nil {
+				return sdk.Context{}, err
+			}
 		}
 	}
 
