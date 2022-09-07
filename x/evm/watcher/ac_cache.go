fc8164863af342757dd48454dbe7bd5293dca939
lyh169 <liuyuhang203@sina.com>
2022-09-07 18:51:59 +0800

    1. for test

--- x/evm/watcher/ac_cache.go
+++ x/evm/watcher/ac_cache.go
@@ -4,7 +4,6 @@ import (
 	"container/list"
 	"encoding/hex"
 	"fmt"
-	"reflect"
 	"sync"
 )
 
@@ -258,34 +257,6 @@ func Statistic(wmg WatchMessage, stat map[string]*Stat) {
 		stat["prefixStdTxHash"] = v
 	default:
 	}
-}
-
-func getSize(v interface{}) int {
-	size := int(reflect.TypeOf(v).Size())
-	switch reflect.TypeOf(v).Kind() {
-	case reflect.Slice:
-		s := reflect.ValueOf(v)
-		for i := 0; i < s.Len(); i++ {
-			size += getSize(s.Index(i).Interface())
-		}
-	case reflect.Map:
-		s := reflect.ValueOf(v)
-		keys := s.MapKeys()
-		size += int(float64(len(keys)) * 10.79) // approximation from https://golang.org/src/runtime/hashmap.go
-		for i := range keys {
-			size += getSize(keys[i].Interface()) + getSize(s.MapIndex(keys[i]).Interface())
-		}
-	case reflect.String:
-		size += reflect.ValueOf(v).Len()
-	case reflect.Struct:
-		s := reflect.ValueOf(v)
-		for i := 0; i < s.NumField(); i++ {
-			if s.Field(i).CanInterface() {
-				size += getSize(s.Field(i).Interface())
-			}
-		}
-	}
-	return size
 }
 
 type MessageCacheEvent struct {
