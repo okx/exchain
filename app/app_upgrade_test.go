b6374586dcf8a16d5091b4165da8b650513d4231
ylsGit <bzwdyls@163.com>
2022-09-08 16:18:17 +0800

    fix UT

--- app/app_upgrade_test.go
+++ app/app_upgrade_test.go
@@ -559,7 +559,7 @@ func newTestOkcChainApp(
 	app.SetEndBlocker(app.EndBlocker)
 	app.SetGasRefundHandler(refund.NewGasRefundHandler(app.AccountKeeper, app.SupplyKeeper))
 	app.SetAccNonceHandler(NewAccNonceHandler(app.AccountKeeper))
-	app.SetParallelTxHandlers(updateFeeCollectorHandler(app.BankKeeper, app.SupplyKeeper), fixLogForParallelTxHandler(app.EvmKeeper))
+	app.SetParallelTxHandlers(updateFeeCollectorHandler(app.BankKeeper, app.SupplyKeeper, app.feeSplitCollector), fixLogForParallelTxHandler(app.EvmKeeper))
 	app.SetEvmWatcherCollector(app.EvmKeeper.Watcher.Collect)
 
 	if loadLatest {
