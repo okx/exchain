db085f19ce50074d2ac2f66abc82155d714bd034
itsfunny <18757883747@163.com>
2022-09-15 10:54:13 +0800

    need fee module

--- libs/ibc-go/modules/apps/27-interchain-accounts/module.go
+++ libs/ibc-go/modules/apps/27-interchain-accounts/module.go
@@ -1,7 +1,15 @@
 package ica
 
 import (
+	"context"
 	"encoding/json"
+
+	"github.com/okex/exchain/libs/ibc-go/modules/core/base"
+
+	"github.com/okex/exchain/libs/ibc-go/modules/apps/27-interchain-accounts/client/cli"
+
+	controllertypes "github.com/okex/exchain/libs/ibc-go/modules/apps/27-interchain-accounts/controller/types"
+	hosttypes "github.com/okex/exchain/libs/ibc-go/modules/apps/27-interchain-accounts/host/types"
 
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	"github.com/okex/exchain/libs/ibc-go/modules/apps/27-interchain-accounts/host"
@@ -9,7 +17,7 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
-	"github.com/okex/exchain/libs/cosmos-sdk/client/context"
+	cliCtx "github.com/okex/exchain/libs/cosmos-sdk/client/context"
 	"github.com/okex/exchain/libs/cosmos-sdk/codec"
 	anytypes "github.com/okex/exchain/libs/cosmos-sdk/codec/types"
 	"github.com/okex/exchain/libs/cosmos-sdk/types/module"
@@ -28,65 +36,47 @@ var (
 	_ porttypes.IBCModule = host.IBCModule{}
 )
 
-// TODO,这里需要进行整个适配
-
 // AppModuleBasic is the IBC interchain accounts AppModuleBasic
 type AppModuleBasic struct{}
 
-func (b AppModuleBasic) RegisterCodec(codec *codec.Codec) {
-	//TODO implement me
-	panic("implement me")
-}
+func (b AppModuleBasic) RegisterCodec(codec *codec.Codec) {}
 
 func (b AppModuleBasic) DefaultGenesis() json.RawMessage {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (b AppModuleBasic) ValidateGenesis(message json.RawMessage) error {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
-func (b AppModuleBasic) RegisterRESTRoutes(context context.CLIContext, router *mux.Router) {
-	//TODO implement me
-	panic("implement me")
-}
+func (b AppModuleBasic) RegisterRESTRoutes(context cliCtx.CLIContext, router *mux.Router) {}
 
 func (b AppModuleBasic) GetTxCmd(codec *codec.Codec) *cobra.Command {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (b AppModuleBasic) GetQueryCmd(codec *codec.Codec) *cobra.Command {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (b AppModuleBasic) RegisterInterfaces(registry anytypes.InterfaceRegistry) {
-	//TODO implement me
-	panic("implement me")
+	types.RegisterInterfaces(registry)
 }
 
-func (b AppModuleBasic) RegisterGRPCGatewayRoutes(context context.CLIContext, mux *runtime.ServeMux) {
-	//TODO implement me
-	panic("implement me")
+func (b AppModuleBasic) RegisterGRPCGatewayRoutes(ctx cliCtx.CLIContext, mux *runtime.ServeMux) {
+	controllertypes.RegisterQueryHandlerClient(context.Background(), mux, controllertypes.NewQueryClient(ctx))
+	hosttypes.RegisterQueryHandlerClient(context.Background(), mux, hosttypes.NewQueryClient(ctx))
 }
 
 func (b AppModuleBasic) GetTxCmdV2(cdc *codec.CodecProxy, reg anytypes.InterfaceRegistry) *cobra.Command {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (b AppModuleBasic) GetQueryCmdV2(cdc *codec.CodecProxy, reg anytypes.InterfaceRegistry) *cobra.Command {
-	//TODO implement me
-	panic("implement me")
+	return cli.GetQueryCmd(cdc, reg)
 }
 
-func (b AppModuleBasic) RegisterRouterForGRPC(cliCtx context.CLIContext, r *mux.Router) {
-	//TODO implement me
-	panic("implement me")
-}
+func (b AppModuleBasic) RegisterRouterForGRPC(cliCtx cliCtx.CLIContext, r *mux.Router) {}
 
 // Name implements AppModuleBasic interface
 func (AppModuleBasic) Name() string {
@@ -95,57 +85,66 @@ func (AppModuleBasic) Name() string {
 
 // AppModule is the application module for the IBC interchain accounts module
 type AppModule struct {
+	*base.BaseIBCUpgradeModule
 	AppModuleBasic
 	controllerKeeper *controllerkeeper.Keeper
 	hostKeeper       *hostkeeper.Keeper
 }
 
-func (a AppModule) InitGenesis(s sdk.Context, message json.RawMessage) []abci.ValidatorUpdate {
-	//TODO implement me
-	panic("implement me")
+// NewAppModule creates a new 20-transfer module
+func NewAppModule(m *codec.CodecProxy, ck *controllerkeeper.Keeper, hk *hostkeeper.Keeper) AppModule {
+	ret := AppModule{
+		controllerKeeper: ck,
+		hostKeeper:       hk,
+	}
+	ret.BaseIBCUpgradeModule = base.NewBaseIBCUpgradeModule(ret.AppModuleBasic)
+	return ret
+}
+
+func (am AppModule) InitGenesis(s sdk.Context, message json.RawMessage) []abci.ValidatorUpdate {
+	var genesisState types.GenesisState
+	ModuleCdc.MustUnmarshalJSON(message, &genesisState)
+
+	if am.controllerKeeper != nil {
+		controllerkeeper.InitGenesis(s, *am.controllerKeeper, genesisState.ControllerGenesisState)
+	}
+
+	if am.hostKeeper != nil {
+		hostkeeper.InitGenesis(s, *am.hostKeeper, genesisState.HostGenesisState)
+	}
+
+	return nil
 }
 
 func (a AppModule) ExportGenesis(s sdk.Context) json.RawMessage {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
 func (a AppModule) Route() string {
-	//TODO implement me
-	panic("implement me")
+	return types.RouterKey
 }
 
 func (a AppModule) NewHandler() sdk.Handler {
-	//TODO implement me
-	panic("implement me")
+	return NewHandler(a.hostKeeper, a.controllerKeeper)
 }
 
 func (a AppModule) QuerierRoute() string {
-	//TODO implement me
-	panic("implement me")
+	return types.QuerierRoute
 }
 
 func (a AppModule) NewQuerierHandler() sdk.Querier {
-	//TODO implement me
-	panic("implement me")
+	return nil
 }
 
-func (a AppModule) BeginBlock(s sdk.Context, block abci.RequestBeginBlock) {
-	//TODO implement me
-	panic("implement me")
-}
+func (a AppModule) BeginBlock(s sdk.Context, block abci.RequestBeginBlock) {}
 
 func (a AppModule) EndBlock(s sdk.Context, block abci.RequestEndBlock) []abci.ValidatorUpdate {
-	//TODO implement me
-	panic("implement me")
+	return []abci.ValidatorUpdate{}
 }
 
-func (a AppModule) RegisterInvariants(registry sdk.InvariantRegistry) {
-	//TODO implement me
-	panic("implement me")
-}
+func (a AppModule) RegisterInvariants(registry sdk.InvariantRegistry) {}
 
-func (a AppModule) RegisterServices(configurator module.Configurator) {
-	//TODO implement me
-	panic("implement me")
+func (a AppModule) RegisterServices(cfg module.Configurator) {
+	controllertypes.RegisterQueryServer(cfg.QueryServer(), a.controllerKeeper)
+	hosttypes.RegisterQueryServer(cfg.QueryServer(), a.hostKeeper)
 }
