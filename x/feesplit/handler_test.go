487c2ca8a930ec2e2209e83ce3ce2250cce9ec34
ylsGit <bzwdyls@163.com>
2022-09-20 11:05:57 +0800

    fix UT

--- x/feesplit/handler_test.go
+++ x/feesplit/handler_test.go
@@ -14,6 +14,7 @@ import (
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
 	authtypes "github.com/okex/exchain/libs/cosmos-sdk/x/auth/types"
 	abci "github.com/okex/exchain/libs/tendermint/abci/types"
+	tmtypes "github.com/okex/exchain/libs/tendermint/types"
 	"github.com/okex/exchain/x/feesplit"
 	"github.com/okex/exchain/x/feesplit/types"
 	"github.com/stretchr/testify/suite"
@@ -33,6 +34,7 @@ func TestFeeSplitTestSuite(t *testing.T) {
 
 func (suite *FeeSplitTestSuite) SetupTest() {
 	checkTx := false
+	tmtypes.UnittestOnlySetMilestoneVenus3Height(1)
 
 	suite.app = app.Setup(false)
 	suite.ctx = suite.app.BaseApp.NewContext(checkTx, abci.Header{Height: 1, ChainID: "ethermint-3", Time: time.Now().UTC()})
