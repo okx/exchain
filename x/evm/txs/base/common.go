79863439e84099c51c78ffb6280cf2de8293ca13
cwbhhjl <cwbhhjl@gmail.com>
2022-09-24 13:42:02 +0800

    reduce txhash

--- x/evm/txs/base/common.go
+++ x/evm/txs/base/common.go
@@ -39,7 +39,10 @@ func msg2st(ctx *sdk.Context, k *Keeper, msg *types.MsgEthereumTx, st *types.Sta
 		return
 	}
 
-	txHash := tmtypes.Tx(ctx.TxBytes()).Hash(ctx.BlockHeight())
+	txHash := msg.TxHash()
+	if len(txHash) == 0 {
+		txHash = tmtypes.Tx(ctx.TxBytes()).Hash(ctx.BlockHeight())
+	}
 	ethHash := common.BytesToHash(txHash)
 
 	st.AccountNonce = msg.Data.AccountNonce
