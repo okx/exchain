eac46466816869314e1eec02afae28a2e60415cf
cwbhhjl <cwbhhjl@gmail.com>
2022-09-29 21:38:35 +0800

    config for demo

---
+++ x/match/config.go
@@ -0,0 +1,12 @@
+package match
+
+import "math/big"
+
+var (
+	OperatorPrivKey string
+	ChainID         *big.Int
+	EthRpcUrl       = "http://localhost:8545"
+
+	PerpetualV1ContractAddress string
+	P1OrdersContractAddress    string
+)
