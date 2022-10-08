dd92f3684da618edd6c1f24c377bbb9952db42ab
cwbhhjl <cwbhhjl@gmail.com>
2022-10-08 16:51:59 +0800

    update DydxClient

--- libs/dydx/client.go
+++ libs/dydx/client.go
@@ -29,14 +29,27 @@ type DydxClient struct {
 	//perpetualV1EventCh  chan types.Log
 	//perpetualV1EventErr error
 
-	perpetualV1EventLogTradeCh  chan *contracts.PerpetualV1LogTrade
-	perpetualV1EventLogTradeErr chan error
+	perpetualV1EventLogTradeCh chan *contracts.PerpetualV1LogTrade
+	p1OrdersEventLogFilledCh   chan *contracts.P1OrdersLogOrderFilled
+	subErr                     chan error
 
 	closeCh chan struct{}
 }
 
 func (c *DydxClient) Stop() {
 	close(c.closeCh)
+}
+
+func (c *DydxClient) Err() <-chan error {
+	return c.subErr
+}
+
+func (c *DydxClient) LogTrade() <-chan *contracts.PerpetualV1LogTrade {
+	return c.perpetualV1EventLogTradeCh
+}
+
+func (c *DydxClient) LogFilled() <-chan *contracts.P1OrdersLogOrderFilled {
+	return c.p1OrdersEventLogFilledCh
 }
 
 func (c *DydxClient) Trade(order1, order2 *SignedOrder, amount *big.Int, price Price, fee Fee) (*types.Transaction, error) {
@@ -82,7 +95,22 @@ func (c *DydxClient) ethEventLogTradeRoutine(sub ethereum.Subscription) {
 		select {
 		// case log := <-c.perpetualV1EventLogTradeCh:
 		case err := <-sub.Err():
-			c.perpetualV1EventLogTradeErr <- err
+			c.subErr <- err
+			c.Stop()
+			return
+		case <-c.closeCh:
+			sub.Unsubscribe()
+			return
+		}
+	}
+}
+
+func (c *DydxClient) ethEventLogFilledRoutine(sub ethereum.Subscription) {
+	for {
+		select {
+		case err := <-sub.Err():
+			c.subErr <- err
+			c.Stop()
 			return
 		case <-c.closeCh:
 			sub.Unsubscribe()
@@ -133,12 +161,22 @@ func NewDydxClient(chainID *big.Int, ethRpcUrl string, fromBlockNum *big.Int,
 
 	start := fromBlockNum.Uint64()
 	watchOps := &bind.WatchOpts{Start: &start, Context: context.Background()}
+
+	client.subErr = make(chan error, 2)
+
 	client.perpetualV1EventLogTradeCh = make(chan *contracts.PerpetualV1LogTrade, 128)
-	sub, err := client.contracts.PerpetualV1.WatchLogTrade(watchOps, client.perpetualV1EventLogTradeCh, nil, nil)
+	tradeSub, err := client.contracts.PerpetualV1.WatchLogTrade(watchOps, client.perpetualV1EventLogTradeCh, nil, nil)
 	if err != nil {
 		return nil, fmt.Errorf("failed to watch LogTrade, err: %w", err)
 	}
-	go client.ethEventLogTradeRoutine(sub)
+	go client.ethEventLogTradeRoutine(tradeSub)
+
+	client.p1OrdersEventLogFilledCh = make(chan *contracts.P1OrdersLogOrderFilled, 128)
+	filledSub, err := client.contracts.P1Orders.WatchLogOrderFilled(watchOps, client.p1OrdersEventLogFilledCh)
+	if err != nil {
+		return nil, fmt.Errorf("failed to watch LogOrderFilled, err: %w", err)
+	}
+	go client.ethEventLogFilledRoutine(filledSub)
 
 	//client.closeCh = make(chan struct{})
 	//client.perpetualV1EventCh = make(chan types.Log, 512)
