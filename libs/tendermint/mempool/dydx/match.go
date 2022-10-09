6f095a80710da0eb2be2f900d7d6563af9ff5cbc
cwbhhjl <cwbhhjl@gmail.com>
2022-10-09 17:09:02 +0800

    update MatchEngine

--- libs/tendermint/mempool/dydx/match.go
+++ libs/tendermint/mempool/dydx/match.go
@@ -1,12 +1,74 @@
 package dydx
 
 import (
+	"crypto/ecdsa"
 	"fmt"
 	"math/big"
+
+	"github.com/ethereum/go-ethereum/accounts/abi/bind"
+	"github.com/ethereum/go-ethereum/common"
+	ethtypes "github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	dydxlib "github.com/okex/exchain/libs/dydx"
+	"github.com/okex/exchain/libs/dydx/contracts"
 )
 
 type MatchEngine struct {
 	depthBook *DepthBook
+
+	contracts *dydxlib.Contracts
+	privKey   *ecdsa.PrivateKey
+	from      common.Address
+	chainID   *big.Int
+	ethCli    *ethclient.Client
+
+	config DydxConfig
+}
+
+type DydxConfig struct {
+	PrivKeyHex                 string
+	ChainID                    string
+	EthWsRpcUrl                string
+	PerpetualV1ContractAddress string
+	P1OrdersContractAddress    string
+}
+
+// chainID *big.Int, ethRpcUrl string, fromBlockNum *big.Int,
+//	privKey, perpetualV1ContractAddress, p1OrdersContractAddress string
+
+func NewMatchEngine(depthBook *DepthBook, config DydxConfig) (*MatchEngine, error) {
+	var engine = &MatchEngine{
+		depthBook: depthBook,
+		config:    config,
+	}
+
+	var err error
+	engine.privKey, err = crypto.HexToECDSA(config.PrivKeyHex)
+	if err != nil {
+		return nil, fmt.Errorf("invalid private key: %w", err)
+	}
+	engine.from = crypto.PubkeyToAddress(engine.privKey.PublicKey)
+	engine.chainID, _ = new(big.Int).SetString(config.ChainID, 10)
+	if engine.chainID == nil {
+		return nil, fmt.Errorf("invalid chain id")
+	}
+	engine.ethCli, err = ethclient.Dial(config.EthWsRpcUrl)
+	if err != nil {
+		return nil, fmt.Errorf("failed to dial eth rpc url: %s, err: %w", config.EthWsRpcUrl, err)
+	}
+	txOps, err := bind.NewKeyedTransactorWithChainID(engine.privKey, engine.chainID)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create txOps, err: %w", err)
+	}
+	engine.contracts, err = dydxlib.NewContracts(
+		common.HexToAddress(config.PerpetualV1ContractAddress),
+		common.HexToAddress(config.P1OrdersContractAddress),
+		txOps,
+		engine.ethCli,
+	)
+
+	return engine, nil
 }
 
 type MatchResult struct {
@@ -38,6 +100,23 @@ func (m *MatchEngine) Match(order *WrapOrder) (*MatchResult, error) {
 	} else {
 		return nil, fmt.Errorf("invalid order type")
 	}
+}
+
+func (m *MatchEngine) trade(order1, order2 *dydxlib.SignedSolOrder, fill *contracts.P1OrdersFill) (*ethtypes.Transaction, error) {
+	op := dydxlib.NewTradeOperation(m.contracts)
+	err := op.FillSignedSolOrderWithTaker(m.from, order1, fill)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fill order1, err: %w", err)
+	}
+	err = op.FillSignedSolOrderWithTaker(m.from, order2, fill)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fill order2, err: %w", err)
+	}
+	tx, err := op.Commit(nil)
+	if err != nil {
+		return nil, fmt.Errorf("failed to commit, err: %w", err)
+	}
+	return tx, nil
 }
 
 func processOrder(takerOrder *WrapOrder, makerBook *OrderList, takerBook *OrderList) (*MatchResult, error) {
