6e96ca962d73d42948e382907c341ed86d81a941
ylsGit <bzwdyls@163.com>
2022-10-11 12:16:56 +0800

    Update milestone.go

--- libs/tendermint/types/milestone.go
+++ libs/tendermint/types/milestone.go
@@ -199,7 +199,7 @@ func HigherThanVenus2(h int64) bool {
 	if milestoneVenus2Height == 0 {
 		return false
 	}
-	return h >= milestoneVenus2Height
+	return h > milestoneVenus2Height
 }
 
 func UnittestOnlySetMilestoneVenus2Height(h int64) {
