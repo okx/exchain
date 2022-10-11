095c0bd946620147ffb2f42ab94f1c17cf27747e
itsfunny <18757883747@163.com>
2022-10-11 15:34:14 +0800

    rm codes

--- libs/cosmos-sdk/baseapp/abci.go
+++ libs/cosmos-sdk/baseapp/abci.go
@@ -359,7 +359,6 @@ func (app *BaseApp) halt() {
 // Query implements the ABCI interface. It delegates to CommitMultiStore if it
 // implements Queryable.
 func (app *BaseApp) Query(req abci.RequestQuery) abci.ResponseQuery {
-	fmt.Println("path:", req.Path)
 	ceptor := app.interceptors[req.Path]
 	if nil != ceptor {
 		// interceptor is like `aop`,it may record the request or rewrite the data in the request
