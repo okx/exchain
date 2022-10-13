3b678b201e5070380b54e09e80f9340a82af7668
ylsGit <bzwdyls@163.com>
2022-10-13 22:55:07 +0800

    change feesplit type

--- x/feesplit/handler.go
+++ x/feesplit/handler.go
@@ -168,7 +168,7 @@ func handleMsgUpdateFeeSplit(
 	}
 
 	// error if the msg deployer address is not the same as the fee's deployer
-	if msg.DeployerAddress != feeSplit.DeployerAddress {
+	if msg.DeployerAddress != feeSplit.DeployerAddress.String() {
 		return nil, sdkerrors.Wrapf(
 			sdkerrors.ErrUnauthorized,
 			"%s is not the contract deployer", msg.DeployerAddress,
@@ -176,12 +176,12 @@ func handleMsgUpdateFeeSplit(
 	}
 
 	// check if updating feesplit to default withdrawer
-	if msg.WithdrawerAddress == feeSplit.DeployerAddress {
+	if msg.WithdrawerAddress == feeSplit.DeployerAddress.String() {
 		msg.WithdrawerAddress = ""
 	}
 
 	// fee split with the given withdraw address is already registered
-	if msg.WithdrawerAddress == feeSplit.WithdrawerAddress {
+	if msg.WithdrawerAddress == feeSplit.WithdrawerAddress.String() {
 		return nil, sdkerrors.Wrapf(
 			types.ErrFeeSplitAlreadyRegistered,
 			"fee split with withdraw address %s", msg.WithdrawerAddress,
@@ -189,16 +189,18 @@ func handleMsgUpdateFeeSplit(
 	}
 
 	// only delete withdrawer map if is not default
-	if feeSplit.WithdrawerAddress != "" {
-		k.DeleteWithdrawerMap(ctx, sdk.MustAccAddressFromBech32(feeSplit.WithdrawerAddress), contract)
+	if !feeSplit.WithdrawerAddress.Empty() {
+		k.DeleteWithdrawerMap(ctx, feeSplit.WithdrawerAddress, contract)
 	}
 
+	var withdrawer sdk.AccAddress
 	// only add withdrawer map if new entry is not default
 	if msg.WithdrawerAddress != "" {
-		k.SetWithdrawerMap(ctx, sdk.MustAccAddressFromBech32(msg.WithdrawerAddress), contract)
+		withdrawer = sdk.MustAccAddressFromBech32(msg.WithdrawerAddress)
+		k.SetWithdrawerMap(ctx, withdrawer, contract)
 	}
 	// update fee split
-	feeSplit.WithdrawerAddress = msg.WithdrawerAddress
+	feeSplit.WithdrawerAddress = withdrawer
 	k.SetFeeSplit(ctx, feeSplit)
 
 	ctx.EventManager().EmitEvents(
@@ -235,7 +237,7 @@ func handleMsgCancelFeeSplit(
 		)
 	}
 
-	if msg.DeployerAddress != fee.DeployerAddress {
+	if msg.DeployerAddress != fee.DeployerAddress.String() {
 		return nil, sdkerrors.Wrapf(
 			sdkerrors.ErrUnauthorized,
 			"%s is not the contract deployer", msg.DeployerAddress,
@@ -243,11 +245,11 @@ func handleMsgCancelFeeSplit(
 	}
 
 	k.DeleteFeeSplit(ctx, fee)
-	k.DeleteDeployerMap(ctx, fee.GetDeployerAddr(), contract)
+	k.DeleteDeployerMap(ctx, fee.DeployerAddress, contract)
 
 	// delete entry from withdrawer map if not default
-	if fee.WithdrawerAddress != "" {
-		k.DeleteWithdrawerMap(ctx, fee.GetWithdrawerAddr(), contract)
+	if !fee.WithdrawerAddress.Empty() {
+		k.DeleteWithdrawerMap(ctx, fee.WithdrawerAddress, contract)
 	}
 
 	ctx.EventManager().EmitEvents(
--- x/feesplit/types/fee_split.go
+++ x/feesplit/types/fee_split.go
@@ -1,73 +1,79 @@
 package types
 
 import (
+	"bytes"
+
 	"github.com/ethereum/go-ethereum/common"
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
+	sdkerrors "github.com/okex/exchain/libs/cosmos-sdk/types/errors"
 )
 
 // FeeSplit defines an instance that organizes fee distribution conditions for
 // the owner of a given smart contract
 type FeeSplit struct {
 	// hex address of registered contract
-	ContractAddress string `json:"contract_address,omitempty"`
+	ContractAddress common.Address `json:"contract_address,omitempty"`
 	// bech32 address of contract deployer
-	DeployerAddress string `json:"deployer_address,omitempty"`
+	DeployerAddress sdk.AccAddress `json:"deployer_address,omitempty"`
 	// bech32 address of account receiving the transaction fees it defaults to
 	// deployer_address
-	WithdrawerAddress string `json:"withdrawer_address,omitempty"`
+	WithdrawerAddress sdk.AccAddress `json:"withdrawer_address,omitempty"`
 }
 
 // NewFeeSplit returns an instance of FeeSplit. If the provided withdrawer
 // address is empty, it sets the value to an empty string.
 func NewFeeSplit(contract common.Address, deployer, withdrawer sdk.AccAddress) FeeSplit {
-	withdrawerAddr := ""
-	if len(withdrawer) > 0 {
-		withdrawerAddr = withdrawer.String()
+	var withdrawerAddr sdk.AccAddress
+	if !withdrawer.Empty() {
+		withdrawerAddr = withdrawer
 	}
 
 	return FeeSplit{
-		ContractAddress:   contract.String(),
-		DeployerAddress:   deployer.String(),
+		ContractAddress:   contract,
+		DeployerAddress:   deployer,
 		WithdrawerAddress: withdrawerAddr,
 	}
 }
 
 // GetContractAddr returns the contract address
-func (fs FeeSplit) GetContractAddr() common.Address {
-	return common.HexToAddress(fs.ContractAddress)
-}
+//func (fs FeeSplit) GetContractAddr() common.Address {
+//	return common.HexToAddress(fs.ContractAddress)
+//}
 
 // GetDeployerAddr returns the contract deployer address
-func (fs FeeSplit) GetDeployerAddr() sdk.AccAddress {
-	return sdk.MustAccAddressFromBech32(fs.DeployerAddress)
-}
+//func (fs FeeSplit) GetDeployerAddr() sdk.AccAddress {
+//	return sdk.MustAccAddressFromBech32(fs.DeployerAddress)
+//}
 
 // GetWithdrawerAddr returns the account address to where the funds proceeding
 // from the fees will be received. If the withdraw address is not defined, it
 // defaults to the deployer address.
-func (fs FeeSplit) GetWithdrawerAddr() sdk.AccAddress {
-	if fs.WithdrawerAddress == "" {
-		return nil
-	}
-
-	return sdk.MustAccAddressFromBech32(fs.WithdrawerAddress)
-}
+//func (fs FeeSplit) GetWithdrawerAddr() sdk.AccAddress {
+//	if fs.WithdrawerAddress == "" {
+//		return nil
+//	}
+//
+//	return sdk.MustAccAddressFromBech32(fs.WithdrawerAddress)
+//}
 
 // Validate performs a stateless validation of a FeeSplit
 func (fs FeeSplit) Validate() error {
-	if err := ValidateNonZeroAddress(fs.ContractAddress); err != nil {
-		return err
+	if bytes.Equal(fs.ContractAddress.Bytes(), common.Address{}.Bytes()) {
+		return sdkerrors.Wrapf(
+			sdkerrors.ErrInvalidAddress, "address '%s' is not a valid ethereum hex address",
+			fs.ContractAddress.String(),
+		)
 	}
 
-	if _, err := sdk.AccAddressFromBech32(fs.DeployerAddress); err != nil {
-		return err
-	}
+	//if _, err := sdk.AccAddressFromBech32(fs.DeployerAddress); err != nil {
+	//	return err
+	//}
 
-	if fs.WithdrawerAddress != "" {
-		if _, err := sdk.AccAddressFromBech32(fs.WithdrawerAddress); err != nil {
-			return err
-		}
-	}
+	//if fs.WithdrawerAddress != "" {
+	//	if _, err := sdk.AccAddressFromBech32(fs.WithdrawerAddress); err != nil {
+	//		return err
+	//	}
+	//}
 
 	return nil
 }
