eee46ebb07a2226d7e9a3eb793f827b853ce263c
zhangkai <zhangkai.gis@163.com>
2022-10-15 17:11:20 +0800

    empty tree still need save fast-storage version
    delete useless functions
    
    empty tree still need save fast-storage version
    
    restore ut
    
    restore ut

--- libs/iavl/mutable_tree_oec.go
+++ libs/iavl/mutable_tree_oec.go
@@ -159,7 +159,7 @@ func (tree *MutableTree) persist(version int64) {
 	batch := tree.NewBatch()
 	tree.commitCh <- commitEvent{-1, nil, nil, nil, nil, 0, nil}
 	var tpp map[string]*Node = nil
-	var fnc *fastNodeChanges
+	fnc := newFastNodeChanges()
 	if EnablePruningHistoryState {
 		tree.ndb.saveCommitOrphans(batch, version, tree.commitOrphans)
 	}
