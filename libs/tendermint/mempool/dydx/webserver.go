d1baaf4fb78e0462d3eedf972a9867ae8504487d
xingqiang.yuan <xingqiang.yuan@okg.com>
2022-11-06 01:53:47 +0800

    use placeorder contract on testnet

--- libs/tendermint/mempool/dydx/webserver.go
+++ libs/tendermint/mempool/dydx/webserver.go
@@ -23,7 +23,7 @@ const (
 	addrKey    = "addr"
 	timeFormat = "15:04:05"
 
-	placeOrderContractAddr = "0x2594E83A94F89Ffb923773ddDfF723BbE017b80D" //0x4Ef308B36E9f75C97a38594acbFa9FBe1B847Da5 testnet
+	placeOrderContractAddr = "0x4Ef308B36E9f75C97a38594acbFa9FBe1B847Da5" //0x4Ef308B36E9f75C97a38594acbFa9FBe1B847Da5 testnet
 )
 
 var oneWeekSeconds = int64(time.Hour/time.Second) * 24 * 7
