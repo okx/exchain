92bfa31f992b92326e578741effd92ec591a7d36
cwbhhjl <cwbhhjl@gmail.com>
2022-11-08 11:40:33 +0800

    more log

--- libs/tendermint/mempool/dydx/match.go
+++ libs/tendermint/mempool/dydx/match.go
@@ -179,6 +179,10 @@ type MatchRecord struct {
 	Maker *WrapOrder
 }
 
+func (record MatchRecord) String() string {
+	return fmt.Sprintf("taker %s maker %s,price, %s, amount %s;", record.Taker.Hash(), record.Maker.Hash(), record.Fill.Price, record.Fill.Amount)
+}
+
 func (m *MatchEngine) UpdateState(txsResps []*abci.ResponseDeliverTx) {
 	if len(txsResps) == 0 {
 		return
@@ -222,18 +226,23 @@ func (m *MatchEngine) MatchAndTrade(order *WrapOrder) (*MatchResult, error) {
 	}
 	matched, err := m.Match(order, marketPrice)
 	if err != nil {
-		return nil, err
+		m.logger.Error("failed to match order", "err", err)
+		return nil, fmt.Errorf("failed to match order, err: %w", err)
 	}
 
 	if matched.IsEmpty() {
+		m.logger.Debug("no match", "order", order.Hash())
 		return nil, nil
 	}
 
-	m.logger.Debug("match result", "matched", matched.MatchedRecords)
+	for i, record := range matched.MatchedRecords {
+		m.logger.Debug("match result", "index", i, "record", record)
+	}
 
 	var needRollback bool
 	defer func() {
 		if needRollback {
+			m.logger.Debug("match and trade rollback", "order", order.Hash())
 			matched.Unfreeze()
 		}
 	}()
