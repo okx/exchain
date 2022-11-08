0a3c45e3024c08bf86ae13cff4d5ed10bbc32280
xingqiang.yuan <xingqiang.yuan@okg.com>
2022-11-08 19:33:34 +0800

    update place order contract addr

--- libs/tendermint/mempool/dydx/webserver.go
+++ libs/tendermint/mempool/dydx/webserver.go
@@ -24,7 +24,7 @@ const (
 	addrKey    = "addr"
 	timeFormat = "15:04:05"
 
-	placeOrderContractAddr = "0x2594E83A94F89Ffb923773ddDfF723BbE017b80D" //0x4Ef308B36E9f75C97a38594acbFa9FBe1B847Da5 testnet
+	placeOrderContractAddr = "0x293a0231e57ee599DB33745bEF9Bfcc320B43de1" //0x4Ef308B36E9f75C97a38594acbFa9FBe1B847Da5 testnet
 )
 
 var oneWeekSeconds = int64(time.Hour/time.Second) * 24 * 7
