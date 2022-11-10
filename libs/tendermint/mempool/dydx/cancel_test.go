85d862a7bb5d54640d5b778f2bf135cf84da307d
xingqiang.yuan <xingqiang.yuan@okg.com>
2022-11-10 21:18:50 +0800

    add ut

--- libs/tendermint/mempool/dydx/cancel_test.go
+++ libs/tendermint/mempool/dydx/cancel_test.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/ethereum/go-ethereum/ethclient"
 	"github.com/okex/exchain/libs/dydx/contracts"
+	"github.com/okex/exchain/libs/tendermint/mempool/placeorder"
 	"github.com/stretchr/testify/require"
 	"log"
 	"math/big"
@@ -88,4 +89,24 @@ func TestWithdraw(t *testing.T) {
 	balance, err = perpetualV1.GetAccountBalance(nil, addr)
 	require.NoError(t, err)
 	log.Printf("balance of %s: %v\n", addr, balance)
+}
+
+func TestPlaceOrder(t *testing.T) {
+	cli, err := ethclient.Dial(Config.EthHttpRpcUrl) //"http://3.113.237.222:26659"
+	require.NoError(t, err)
+	caller, err := placeorder.NewPlaceorderCaller(common.HexToAddress(placeOrderContractAddr), cli)
+	require.NoError(t, err)
+	maker := common.HexToAddress("0xbbE4733d85bc2b90682147779DA49caB38C0aA1F")
+	order := placeorder.OrdersOrder{
+		Amount:       big.NewInt(1),
+		LimitPrice:   big.NewInt(18200),
+		TriggerPrice: big.NewInt(0),
+		LimitFee:     big.NewInt(0),
+		Maker:        maker,
+		Expiration:   big.NewInt(time.Now().Unix() + oneWeekSeconds),
+	}
+	msg, err := caller.GetOrderMessage(&bind.CallOpts{From: maker}, order)
+	require.NoError(t, err)
+	t.Log(hex.EncodeToString(msg))
+
 }
