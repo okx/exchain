48a22dc6ba7026cbd786bc8d218939cf1f6e23f2
itsfunny <18757883747@163.com>
2022-11-16 12:43:15 +0800

    mil

--- libs/tendermint/types/milestone.go
+++ libs/tendermint/types/milestone.go
@@ -78,7 +78,7 @@ func init() {
 		milestoneVenus3Height = string2number(MILESTONE_VENUS3_HEIGHT)
 		milestoneEarthHeight = string2number(MILESTONE_EARTH_HEIGHT)
 		milestoneVenus4Height = string2number(MILESTONE_VENUS4_HEIGHT)
-		milestoneVenus4Height = 4750
+		milestoneVenus4Height = 6000
 		milestoneVenus1Height = 1
 	})
 }
