76c69ff286561127289e3acf94fb9a2553540644
Ywmet <xiaoliang.zhang@okg.com>
2022-11-18 16:43:35 +0800

    add bank

--- libs/cosmos-sdk/baseapp/msg_service_router_test.go
+++ libs/cosmos-sdk/baseapp/msg_service_router_test.go
@@ -3,22 +3,17 @@ package baseapp_test
 import (
 	okexchaincodec "github.com/okex/exchain/app/codec"
 	"github.com/okex/exchain/libs/ibc-go/testing/simapp"
-	//tmproto "github.com/okex/exchain/libs/tendermint/proto/types"
 	"github.com/okex/exchain/x/evm"
 	"os"
 	"testing"
 
-	//abci "github.com/okex/exchain/libs/tendermint/abci/types"
 	"github.com/okex/exchain/libs/tendermint/libs/log"
-	//tmproto "github.com/okex/exchain/libs/tendermint/proto/tendermint/types"
+
 	dbm "github.com/okex/exchain/libs/tm-db"
 	"github.com/stretchr/testify/require"
 
 	"github.com/okex/exchain/libs/cosmos-sdk/baseapp"
-	//"github.com/okex/exchain/libs/cosmos-sdk/client/tx"
-	//"github.com/okex/exchain/libs/cosmos-sdk/simapp"
-	//"github.com/okex/exchain/libs/cosmos-sdk/types/tx/signing"
-	//authsigning "github.com/okex/exchain/libs/cosmos-sdk/x/auth/signing"
+
 	"github.com/okex/exchain/x/evm/types/testdata"
 )
 
