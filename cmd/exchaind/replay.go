80d00d98cf44d2893fb8e2ed7950d169874d8183
Ray Green <48553537+banishee@users.noreply.github.com>
2022-11-23 17:06:57 +0800

    reverse

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -134,7 +134,7 @@ type PairList []Pair
 
 func (p PairList) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
 func (p PairList) Len() int           { return len(p) }
-func (p PairList) Less(i, j int) bool { return p[i].Value < p[j].Value }
+func (p PairList) Less(i, j int) bool { return p[i].Value > p[j].Value }
 
 func sortMapByValue(m map[common.Address]int) PairList {
 	p := make(PairList, len(m))
