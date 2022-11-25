e0d8866f9ec3809bdffcfaac8bbf4418d7ae862d
itsfunny <18757883747@163.com>
2022-11-25 09:21:32 +0800

    opt

--- libs/iavl/mutable_tree_oec.go
+++ libs/iavl/mutable_tree_oec.go
@@ -100,7 +100,7 @@ func (tree *MutableTree) SaveVersionAsync(version int64, useDeltas bool) ([]byte
 		}
 	}
 
-	shouldPersist := version%2 == 0
+	shouldPersist := version%CommitGapHeight == 0
 
 	tree.ndb.updateLatestMemoryVersion(version)
 
