12526be66b973bfe4a166476920ab30848fe9e40
zhangkai <zhangkai.gis@163.com>
2022-11-27 22:28:56 +0800

    add persist statistics
    add the condition to statistic
    
    restore write
    
    write sync when ac occur
    
    add the persistDetails
    
    correct the print
    
    defer
    
    restore flush meta

---
+++ libs/system/trace/statistics.go
@@ -0,0 +1,38 @@
+package trace
+
+type BaseStatistics interface {
+	Init(tags ...string)
+	Accumulate(tag string, elapse int64)
+	GetTags() []string
+	GetValue(tag string) int64
+}
+
+type Summary struct {
+	statisticMap map[string]int64
+	keys         []string
+}
+
+func NewSummary() *Summary {
+	return &Summary{
+		statisticMap: make(map[string]int64),
+	}
+}
+
+func (s *Summary) Init(tags ...string) {
+	for _, k := range tags {
+		s.statisticMap[k] = 0
+	}
+	s.keys = tags
+}
+
+func (s *Summary) Accumulate(tag string, elapse int64) {
+	s.statisticMap[tag] += elapse
+}
+
+func (s *Summary) GetTags() []string {
+	return s.keys
+}
+
+func (s *Summary) GetValue(tag string) int64 {
+	return s.statisticMap[tag]
+}
