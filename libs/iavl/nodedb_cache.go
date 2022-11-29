1145c91b4bab7570da55fe13b6928975dcd4deb0
lyh169 <liuyuhang203@sina.com>
2022-11-29 17:20:19 +0800

    1. add modify cache

--- libs/iavl/nodedb_cache.go
+++ libs/iavl/nodedb_cache.go
@@ -1,143 +1,165 @@
 package iavl
 
 import (
-	"container/list"
-	"fmt"
-	"sync"
-
-	"github.com/okex/exchain/libs/iavl/config"
-	"github.com/tendermint/go-amino"
+	lru "github.com/hashicorp/golang-lru"
 )
 
 type NodeCache struct {
-	nodeCache      map[string]*list.Element // Node cache.
-	nodeCacheSize  int                      // Node cache size limit in elements.
-	nodeCacheQueue *syncList                // LRU queue of cache elements. Used for deletion.
-	nodeCacheMutex sync.RWMutex             // Mutex for node cache.
+	nodeCache *lru.Cache
+	//nodeCache      map[string]*list.Element // Node cache.
+	//nodeCacheSize  int                      // Node cache size limit in elements.
+	//nodeCacheQueue *syncList                // LRU queue of cache elements. Used for deletion.
+	//nodeCacheMutex sync.RWMutex             // Mutex for node cache.
 
 	// for test
 	count int
 }
 
 func newNodeCache(dbName string, cacheSize int) *NodeCache {
-	if dbName == "evm" {
-		return &NodeCache{
-			nodeCache:      makeNodeCacheMap(cacheSize, IavlCacheInitRatio),
-			nodeCacheSize:  cacheSize,
-			nodeCacheQueue: newSyncList(),
-		}
-	} else {
-		return &NodeCache{
-			nodeCache:      make(map[string]*list.Element),
-			nodeCacheSize:  cacheSize,
-			nodeCacheQueue: newSyncList(),
-		}
+	//if dbName == "evm" {
+	//	return &NodeCache{
+	//		nodeCache:      makeNodeCacheMap(cacheSize, IavlCacheInitRatio),
+	//		nodeCacheSize:  cacheSize,
+	//		nodeCacheQueue: newSyncList(),
+	//	}
+	//} else {
+	//	return &NodeCache{
+	//		nodeCache:      make(map[string]*list.Element),
+	//		nodeCacheSize:  cacheSize,
+	//		nodeCacheQueue: newSyncList(),
+	//	}
+	//}
+	cache, err := lru.New(cacheSize)
+	if err != nil {
+		panic(err)
+	}
+	return &NodeCache{
+		nodeCache: cache,
 	}
 }
 
-func makeNodeCacheMap(cacheSize int, initRatio float64) map[string]*list.Element {
-	if initRatio <= 0 {
-		return make(map[string]*list.Element)
-	}
-	if initRatio >= 1 {
-		return make(map[string]*list.Element, cacheSize)
-	}
-	cacheSize = int(float64(cacheSize) * initRatio)
-	return make(map[string]*list.Element, cacheSize)
-}
-
+//func makeNodeCacheMap(cacheSize int, initRatio float64) map[string]*list.Element {
+//	if initRatio <= 0 {
+//		return make(map[string]*list.Element)
+//	}
+//	if initRatio >= 1 {
+//		return make(map[string]*list.Element, cacheSize)
+//	}
+//	cacheSize = int(float64(cacheSize) * initRatio)
+//	return make(map[string]*list.Element, cacheSize)
+//}
+//
 // ===================================================
 // ======= map[string]*list.Element implementation
 // ===================================================
 
 func (ndb *NodeCache) uncache(hash []byte) {
-	ndb.nodeCacheMutex.Lock()
-	if elem, ok := ndb.nodeCache[string(hash)]; ok {
-		ndb.count--
-		ndb.nodeCacheQueue.Remove(elem)
-		delete(ndb.nodeCache, string(hash))
-	}
-	ndb.nodeCacheMutex.Unlock()
+	//ndb.nodeCacheMutex.Lock()
+	//if elem, ok := ndb.nodeCache[string(hash)]; ok {
+	//	ndb.count--
+	//	ndb.nodeCacheQueue.Remove(elem)
+	//	delete(ndb.nodeCache, string(hash))
+	//}
+	//ndb.nodeCacheMutex.Unlock()
+	ndb.nodeCache.Remove(string(hash))
 }
 
 // Add a node to the cache and pop the least recently used node if we've
 // reached the cache size limit.
 func (ndb *NodeCache) cache(node *Node) {
-	ndb.nodeCacheMutex.Lock()
-	ndb.count++
-	if ele, ok := ndb.nodeCache[string(node.hash)]; ok {
-		ndb.nodeCacheQueue.MoveToBack(ele)
-	} else {
-		elem := ndb.nodeCacheQueue.PushBack(node)
-		ndb.nodeCache[string(node.hash)] = elem
-
-		for ndb.nodeCacheQueue.Len() > config.DynamicConfig.GetIavlCacheSize() {
-			oldest := ndb.nodeCacheQueue.Front()
-			hash := ndb.nodeCacheQueue.Remove(oldest).(*Node).hash
-			delete(ndb.nodeCache, amino.BytesToStr(hash))
-		}
-	}
-	ndb.nodeCacheMutex.Unlock()
+	ndb.nodeCache.Add(string(node.hash), node)
+	//ndb.nodeCacheMutex.Lock()
+	//ndb.count++
+	//if ele, ok := ndb.nodeCache[string(node.hash)]; ok {
+	//	ndb.nodeCacheQueue.MoveToBack(ele)
+	//} else {
+	//	elem := ndb.nodeCacheQueue.PushBack(node)
+	//	ndb.nodeCache[string(node.hash)] = elem
+	//
+	//	for ndb.nodeCacheQueue.Len() > config.DynamicConfig.GetIavlCacheSize() {
+	//		oldest := ndb.nodeCacheQueue.Front()
+	//		hash := ndb.nodeCacheQueue.Remove(oldest).(*Node).hash
+	//		delete(ndb.nodeCache, amino.BytesToStr(hash))
+	//	}
+	//}
+	//ndb.nodeCacheMutex.Unlock()
 }
 
 func (ndb *NodeCache) cacheWithKey(key string, node *Node) {
-	ndb.nodeCacheMutex.Lock()
-	ndb.count++
-	elem := ndb.nodeCacheQueue.PushBack(node)
-	ndb.nodeCache[key] = elem
-
-	for ndb.nodeCacheQueue.Len() > config.DynamicConfig.GetIavlCacheSize() {
-		fmt.Println("")
-		oldest := ndb.nodeCacheQueue.Front()
-		hash := ndb.nodeCacheQueue.Remove(oldest).(*Node).hash
-		delete(ndb.nodeCache, amino.BytesToStr(hash))
-	}
-	ndb.nodeCacheMutex.Unlock()
+	ndb.nodeCache.Add(key, node)
+	//ndb.nodeCacheMutex.Lock()
+	//ndb.count++
+	//elem := ndb.nodeCacheQueue.PushBack(node)
+	//ndb.nodeCache[key] = elem
+	//
+	//for ndb.nodeCacheQueue.Len() > config.DynamicConfig.GetIavlCacheSize() {
+	//	fmt.Println("")
+	//	oldest := ndb.nodeCacheQueue.Front()
+	//	hash := ndb.nodeCacheQueue.Remove(oldest).(*Node).hash
+	//	delete(ndb.nodeCache, amino.BytesToStr(hash))
+	//}
+	//ndb.nodeCacheMutex.Unlock()
 }
 
 func (ndb *NodeCache) get(hash []byte, promoteRecentNode bool) (n *Node) {
-	// Check the cache.
-	ndb.nodeCacheMutex.RLock()
-	elem, ok := ndb.nodeCache[string(hash)]
-	if ok {
-		if promoteRecentNode {
-			// Already exists. Move to back of nodeCacheQueue.
-			ndb.nodeCacheQueue.MoveToBack(elem)
-		}
-		n = elem.Value.(*Node)
+	//// Check the cache.
+	//ndb.nodeCacheMutex.RLock()
+	//elem, ok := ndb.nodeCache[string(hash)]
+	//if ok {
+	//	if promoteRecentNode {
+	//		// Already exists. Move to back of nodeCacheQueue.
+	//		ndb.nodeCacheQueue.MoveToBack(elem)
+	//	}
+	//	n = elem.Value.(*Node)
+	//}
+	//ndb.nodeCacheMutex.RUnlock()
+
+	var ok bool
+	var res interface{}
+	if promoteRecentNode {
+		res, ok = ndb.nodeCache.Get(string(hash))
+	} else {
+		res, ok = ndb.nodeCache.Peek(string(hash))
 	}
-	ndb.nodeCacheMutex.RUnlock()
+
+	if ok {
+		n = res.(*Node)
+	}
+	return
 	return
 }
 
 func (ndb *NodeCache) nodeCacheLen() int {
-	return len(ndb.nodeCache)
+	return ndb.nodeCache.Len()
 }
 
 // =========================================================
 // ======= github.com/hashicorp/golang-lru implementation
 // =========================================================
 
-//func (ndb *nodeDB) cacheNode(node *Node) {
-//	ndb.lruNodeCache.Add(string(node.hash), node)
+//func (ndb *nodeDB) cache(node *Node) {
+//	ndb.nc.nodeCache.Add(string(node.hash), node)
+//}
+//
+//func (ndb *nodeDB) cacheWithKey(key string, node *Node) {
+//	ndb.nc.nodeCache.Add(key, node)
 //}
 //
 //func (ndb *nodeDB) cacheNodeByCheck(node *Node) {
-//	if ndb.lruNodeCache.Contains(string(node.hash)) {
+//	if ndb.nc.nodeCache.Contains(string(node.hash)) {
 //		return
 //	}
 //	ndb.cacheNode(node)
 //}
-//
 //
 //func (ndb *nodeDB) getNodeFromCache(hash []byte, promoteRecentNode bool) (n *Node) {
 //
 //	var ok bool
 //	var res interface{}
 //	if promoteRecentNode {
-//		res, ok = ndb.lruNodeCache.Get(string(hash))
+//		res, ok = ndb.nc.nodeCache.Get(string(hash))
 //	} else {
-//		res, ok = ndb.lruNodeCache.Peek(string(hash))
+//		res, ok = ndb.nc.nodeCache.Peek(string(hash))
 //	}
 //
 //	if ok {
@@ -146,12 +168,10 @@ func (ndb *NodeCache) nodeCacheLen() int {
 //	return
 //}
 //
-//
 //func (ndb *nodeDB) uncacheNode(hash []byte) {
-//	ndb.lruNodeCache.Remove(string(hash))
+//	ndb.nc.nodeCache.Remove(string(hash))
 //}
 //
-//
 //func (ndb *nodeDB) nodeCacheLen() int {
-//	return ndb.lruNodeCache.Len()
+//	return ndb.nc.nodeCache.Len()
 //}
