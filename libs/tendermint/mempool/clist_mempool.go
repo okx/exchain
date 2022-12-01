67ccf2af53ad866ad0c76b3f67e77ba15e680f69
BananaLF <864685021@qq.com>
2022-12-01 22:20:01 +0800

    change to rlock

--- libs/tendermint/mempool/clist_mempool.go
+++ libs/tendermint/mempool/clist_mempool.go
@@ -1012,8 +1012,8 @@ func (mem *CListMempool) ReapEssentialTx(tx types.Tx) abci.TxEssentials {
 
 // Safe for concurrent use by multiple goroutines.
 func (mem *CListMempool) ReapMaxBytesMaxGas(maxBytes, maxGas int64) []types.Tx {
-	mem.updateMtx.Lock()
-	defer mem.updateMtx.Unlock()
+	mem.updateMtx.RLock()
+	defer mem.updateMtx.RUnlock()
 
 	var (
 		totalBytes int64
