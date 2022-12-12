60d00ccc4269b7eacb90e85c69afef28d2f6fa3e
zhangkai <zhangkai.gis@163.com>
2022-12-12 17:11:56 +0800

    Merge PR: Persist timing-cost statistics (#2824)
    move prechange to iavl
    
    rm the useless tag
    
    restore the prechange
    
    if the store's StatisticsCell is not nil then start timing,to do this avoid alloc the EmptyStatisticsCell
    
    delete commitBlockCache section
    
    when reset set the interface nil
    
    modify the Accumulate the second field to last pin time
    
    Co-authored-by: cwbhhjl <cwbhhjl@gmail.com>

---
+++ libs/system/trace/persist/statistics.go
@@ -0,0 +1,32 @@
+package persist
+
+import (
+	"fmt"
+
+	"github.com/okex/exchain/libs/system/trace"
+)
+
+var stats *statistics
+
+func init() {
+	stats = &statistics{
+		BaseStatistics: trace.NewSummary(),
+	}
+}
+
+type statistics struct {
+	trace.BaseStatistics
+}
+
+func GetStatistics() *statistics {
+	return stats
+}
+
+func (s *statistics) Format() string {
+	var res string
+	for _, tag := range s.GetTags() {
+		res += fmt.Sprintf("%s<%dms>, ", tag, s.GetValue(tag)/1e6)
+	}
+
+	return res[0 : len(res)-2]
+}
