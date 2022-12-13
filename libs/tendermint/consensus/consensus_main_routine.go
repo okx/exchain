718fe1cc196d5f88e6a76d2072d6bbc116bdcc96
chengzhinei <chengxue5yuan@163.com>
2022-12-13 15:02:35 +0800

    Merge PR: add btInterval (#2862)

--- libs/tendermint/consensus/consensus_main_routine.go
+++ libs/tendermint/consensus/consensus_main_routine.go
@@ -3,7 +3,6 @@ package consensus
 import (
 	"bytes"
 	"fmt"
-	"github.com/okex/exchain/libs/system/trace"
 	cfg "github.com/okex/exchain/libs/tendermint/config"
 	cstypes "github.com/okex/exchain/libs/tendermint/consensus/types"
 	"github.com/okex/exchain/libs/tendermint/libs/fail"
@@ -248,10 +247,6 @@ func (cs *State) handleMsg(mi msgInfo) (added bool) {
 
 func (cs *State) handleTimeout(ti timeoutInfo, rs cstypes.RoundState) {
 	cs.Logger.Debug("Received tock", "timeout", ti.Duration, "height", ti.Height, "round", ti.Round, "step", ti.Step)
-
-	if ti.Step == cstypes.RoundStepNewHeight {
-		cs.dumpElapsed(cs.timeoutIntervalTrc, trace.TimeoutInterval)
-	}
 
 	// timeouts must be for current height, round, step
 	if ti.Height != rs.Height || ti.Round < rs.Round || (ti.Round == rs.Round && ti.Step < rs.Step) {
