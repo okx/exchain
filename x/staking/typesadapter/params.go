6ff36e44584221b8de30819f58571817c86a5741
itsfunny <18757883747@163.com>
2022-12-13 10:29:17 +0800

    params 1000 to 10000

--- x/staking/typesadapter/params.go
+++ x/staking/typesadapter/params.go
@@ -8,7 +8,7 @@ func (p *Params) From(pp types.Params) {
 	p.MaxValidators = uint32(pp.MaxValidators)
 	p.UnbondingTime = pp.UnbondingTime
 	if pp.HistoricalEntries == 0 {
-		p.HistoricalEntries = 1000
+		p.HistoricalEntries = 10000
 	} else {
 		p.HistoricalEntries = pp.HistoricalEntries
 	}
