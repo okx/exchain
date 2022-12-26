d3258305926dd2faa44838f78996eeefe44fbf79
zhangkai <zhangkai.gis@163.com>
2022-12-26 16:03:11 +0800

    add the log

--- x/evm/statistics/mysqldb/claim.go
+++ x/evm/statistics/mysqldb/claim.go
@@ -2,16 +2,31 @@ package mysqldb
 
 import (
 	"github.com/okex/exchain/x/evm/statistics/orm/model"
+	"log"
+	"runtime/debug"
 )
 
 func (mdb *mysqlDB) InsertClaim(claim model.Claim) {
-	if len(mdb.claimBatch) > batchSize {
-		tx := mdb.db.Table("claim").CreateInBatches(mdb.claimBatch, len(mdb.claimBatch))
-		if tx.Error != nil {
-			panic(tx.Error)
+	if *claim.Useraddr == "0xacf041fc5a59978016e3b6c339b61a65762d10e2" {
+		debug.PrintStack()
+	}
+	if len(mdb.claimBatch) >= batchSize {
+		//		tx := mdb.db.Table("claim").CreateInBatches(mdb.claimBatch, len(mdb.claimBatch))
+		//		if tx.Error != nil {
+		//			panic(tx.Error)
+		//		}
+		//
+		//		mdb.claimBatch = nil
+		for i, v := range mdb.claimBatch {
+			if *v.Useraddr == "0xacf041fc5a59978016e3b6c339b61a65762d10e2" {
+				log.Printf("---giskook %v %v \n", i, v)
+			}
+			tx := mdb.db.Table("claim").Create(&v)
+			if tx.Error != nil {
+				panic(tx.Error)
+			}
 		}
 
-		mdb.claimBatch = nil
 	}
 	mdb.claimBatch = append(mdb.claimBatch, claim)
 }
