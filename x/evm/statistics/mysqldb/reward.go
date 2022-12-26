72ffc766bff66791f1bb129e43f4512d5d66b310
zhangkai <zhangkai.gis@163.com>
2022-12-26 16:39:53 +0800

    add debug info

--- x/evm/statistics/mysqldb/reward.go
+++ x/evm/statistics/mysqldb/reward.go
@@ -1,29 +1,33 @@
 package mysqldb
 
 import (
+	"fmt"
 	"github.com/okex/exchain/x/evm/statistics/orm/model"
+	"log"
 )
 
 func (mdb *mysqlDB) InsertReward(reward model.Reward) {
-	if len(mdb.rewardBatch) >= batchSize {
-		tx := mdb.db.CreateInBatches(mdb.rewardBatch, len(mdb.rewardBatch))
-		if tx.Error != nil {
-			panic(tx.Error)
-		}
-		var claims []model.Claim
-		useraddr := ""
-		//for _, _ = range mdb.rewardBatch {
-		useraddr = "0xacf041fc5a59978016e3b6c339b61a65762d10e2"
-		mdb.db.Table("claim").Where("useraddr=? and reward=0", useraddr).Find(&claims)
-		//		if len(claims) > 1 || len(claims) == 0 {
-		//			panic(fmt.Sprintf("error claims %v %v", len(claims), claims))
-		//		}
-		mdb.db.Table("claim").Where("useraddr=? and reward=0", useraddr).Update("reward", 1)
-		//}
+	tx := mdb.db.Table("reward").Create(&reward)
+	if tx.Error != nil {
+		panic(tx.Error)
+	}
+	var dbReward model.Reward
+	tx.Last(&dbReward)
+	log.Printf("%v %v %v\n", dbReward.ID, dbReward.Txhash, dbReward.Useraddr)
 
-		mdb.rewardBatch = mdb.rewardBatch[:0]
-	} else {
-		mdb.rewardBatch = append(mdb.rewardBatch, reward)
-		//mdb.db.Model(model.Claim{}).Where("useraddr=? and reward=0", reward.Useraddr)
+	userAddr := "0xacf041fc5a59978016e3b6c339b61a65762d10e2"
+	//useraddr := *dbReward.Useraddr
+	var claims []model.Reward
+	tx = mdb.db.Table("claim").Where("useraddr=? and reward=0", userAddr).Find(&claims)
+	if tx.Error != nil {
+		panic(tx.Error)
+	}
+	if len(claims) != 1 {
+		panic(fmt.Sprintf("useraddr %v dup or empty %v", userAddr, len(claims)))
+	}
+	var r int64 = 1
+	tx = mdb.db.Table("claim").Model(&claims[0]).Updates(&model.Claim{Reward: &r, RewardID: &dbReward.ID})
+	if tx.Error != nil {
+		panic(tx.Error)
 	}
 }
