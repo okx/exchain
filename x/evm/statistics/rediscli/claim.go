016ce22abf36f813f266ce8f9404a351999f0a5a
zhangkai <zhangkai.gis@163.com>
2022-12-28 17:58:07 +0800

    add time limit

--- x/evm/statistics/rediscli/claim.go
+++ x/evm/statistics/rediscli/claim.go
@@ -3,9 +3,18 @@ package rediscli
 import (
 	"fmt"
 	"github.com/gomodule/redigo/redis"
+	"time"
+)
+
+const (
+	// ExpireDate date --date='2023-02-01 00:00:00' +%s
+	TermThreshold = 1675209600
 )
 
 func (r *redisCli) InsertClaim(claim *XenMint) {
+	if (claim.BlockTime).Add(time.Duration(claim.Term)*time.Duration(24)*time.Hour).Unix() > TermThreshold {
+		return
+	}
 	exists, err := redis.Int(r.client.Do("EXISTS", claim.UserAddr))
 	if err != nil || exists == 1 {
 		panic(fmt.Sprintf("error %v or exists %v", err, claim.UserAddr))
