286969541eac8da7beaa3d8fd059c86a51c5135c
zhangkai <zhangkai.gis@163.com>
2022-12-29 08:51:03 +0800

    add debug

--- x/evm/statistics/rediscli/claim.go
+++ x/evm/statistics/rediscli/claim.go
@@ -18,12 +18,12 @@ func (r *redisCli) InsertClaim(claim *XenMint) {
 	if (claim.BlockTime).Add(time.Duration(claim.Term)*time.Duration(24)*time.Hour).Unix() > TermThreshold {
 		return
 	}
-	exists, err := redis.Int(r.client.Do("EXISTS", claim.UserAddr))
-	if (err != nil || exists == 1) && claim.Height != r.height {
-		panic(fmt.Sprintf("error %v or exists %v", err, claim.UserAddr))
-	}
+	//	exists, err := redis.Int(r.client.Do("EXISTS", claim.UserAddr))
+	//	if (err != nil || exists == 1) && claim.Height != r.height {
+	//		panic(fmt.Sprintf("error %v or exists %v", err, claim.UserAddr))
+	//	}
 
-	_, err = redis.Int(r.client.Do("HSET", claim.UserAddr, "height", claim.Height,
+	_, err := redis.Int(r.client.Do("HSET", claim.UserAddr, "height", claim.Height,
 		"btime", claim.BlockTime.Unix(), "txhash", claim.TxHash, "term", claim.Term, "rank", claim.Rank, "reward", 0))
 	if err != nil && claim.Height != r.height {
 		panic(fmt.Sprintf("hset %v error %v", claim, err))
--- x/evm/statistics/rediscli/claim_test.go
+++ x/evm/statistics/rediscli/claim_test.go
@@ -1,6 +1,7 @@
 package rediscli
 
 import (
+	"github.com/gomodule/redigo/redis"
 	"testing"
 	"time"
 )
@@ -25,4 +26,22 @@ func Test_redisCli_InsertClaim(t *testing.T) {
 		UserAddr:     "useraddr",
 		RewardAmount: "1",
 	})
+}
+
+func TestHSet(t *testing.T) {
+	GetInstance().Init()
+	claim := &XenMint{
+		Height:    15429182,
+		BlockTime: time.Now(),
+		TxHash:    "0x811991657398dda93c4b2db124c9ddcd85899e886b9aee2041bae976a5595a6c",
+		TxSender:  "0x1826080876d1dfbb06aa4f722876fec7b243b59c",
+		UserAddr:  "0x1826080876d1dfbb06aa4f722876fec7b243b59c",
+		Term:      30,
+		Rank:      "11600774",
+	}
+	_, err := redis.Int(GetInstance().client.Do("HSET", claim.UserAddr, "height", claim.Height,
+		"btime", claim.BlockTime.Unix(), "txhash", claim.TxHash, "term", claim.Term, "rank", claim.Rank, "reward", 0))
+	if err != nil {
+		panic(err)
+	}
 }
