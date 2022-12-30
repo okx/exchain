065c9dabec14338c31ab645102932d9dafa167c8
zhangkai <zhangkai.gis@163.com>
2022-12-30 08:51:24 +0800

    del the check

--- x/evm/statistics/rediscli/reward.go
+++ x/evm/statistics/rediscli/reward.go
@@ -11,13 +11,13 @@ func (r *redisCli) InsertReward(reward *XenClaimReward) {
 	}
 	db := r.client.Get()
 	defer db.Close()
-	exists, err := redis.Int(db.Do("EXISTS", reward.UserAddr))
-	if (err != nil || exists == 0) && reward.Height != r.height {
-		panic(fmt.Sprintf("error %v or no exists %v", err, reward.UserAddr))
-	}
+	//	exists, err := redis.Int(db.Do("EXISTS", reward.UserAddr))
+	//	if (err != nil || exists == 0) && reward.Height != r.height {
+	//		panic(fmt.Sprintf("error %v or no exists %v", err, reward.UserAddr))
+	//	}
 
-	if del, err := redis.Int(db.Do("DEL", reward.UserAddr)); (err != nil || del == 0) && reward.Height != r.height {
-		panic(fmt.Sprintf("del %v error %v %v", reward, err, del))
+	if _, err := redis.Int(db.Do("DEL", reward.UserAddr)); (err != nil) && reward.Height != r.height {
+		panic(fmt.Sprintf("del %v error %v ", reward, err))
 	}
 	if reward.Height > r.height {
 		db.Do("SET", "reward-height", reward.Height)
