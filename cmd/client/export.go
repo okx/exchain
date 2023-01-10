64ae2b054f56339cb0d0ccdfa488ab8f1079b642
BananaLF <864685021@qq.com>
2023-01-10 23:24:18 +0800

    support import eth keystore file

--- cmd/client/export.go
+++ cmd/client/export.go
@@ -84,6 +84,72 @@ func UnsafeExportEthKeyCommand() *cobra.Command {
 func ExportEthCompCommand() *cobra.Command {
 	return &cobra.Command{
 		Use:   "export-eth-comp [name] [dir]",
+		Short: "Export an Ethereum private keystore directory (This command has been deprecated)",
+		Long: `Export an Ethereum private keystore file encrypted to use in eth client import.
+
+	The parameters of scrypt encryption algorithm is StandardScryptN and StandardScryptN`,
+		Args: cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			inBuf := bufio.NewReader(cmd.InOrStdin())
+			accountName := args[0]
+			dir := args[1]
+
+			kb, err := keys.NewKeyring(
+				sdk.KeyringServiceName(),
+				viper.GetString(flags.FlagKeyringBackend),
+				viper.GetString(flags.FlagHome),
+				inBuf,
+				hd.EthSecp256k1Options()...,
+			)
+			if err != nil {
+				return err
+			}
+
+			decryptPassword := ""
+			conf := true
+			keyringBackend := viper.GetString(flags.FlagKeyringBackend)
+			switch keyringBackend {
+			case keys.BackendFile:
+				decryptPassword, err = input.GetPassword(
+					"Enter passphrase to decrypt your key:",
+					inBuf)
+			case keys.BackendOS:
+				conf, err = input.GetConfirmation(
+					"Decrypt your key by os passphrase. Are you sure?",
+					inBuf)
+			}
+			if err != nil || !conf {
+				return err
+			}
+
+			// Get keystore password
+			encryptPassword, err := input.GetPassword("Enter passphrase to encrypt the exported keystore file:", inBuf)
+			if err != nil {
+				return err
+			}
+
+			// exports private key from keybase using password
+			privKey, err := kb.ExportPrivateKeyObject(accountName, decryptPassword)
+			if err != nil {
+				return err
+			}
+
+			// Exports private key from keybase using password
+			fileName, err := ethkeystore.CreateKeystoreByTmKey(privKey, dir, encryptPassword)
+			if err != nil {
+				return err
+			}
+
+			fmt.Printf("The keystore has exported to: %s \n", fileName)
+			return nil
+		},
+	}
+}
+
+// ExportEthKeyStoreCommand exports a key with the given name as a keystore file.
+func ExportEthKeyStoreCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:   "export-eth-keystore [name] [dir]",
 		Short: "Export an Ethereum private keystore directory",
 		Long: `Export an Ethereum private keystore file encrypted to use in eth client import.
 
@@ -144,4 +210,48 @@ func ExportEthCompCommand() *cobra.Command {
 			return nil
 		},
 	}
+}
+
+// ImportEthKeyStoreCommand import keystore file.
+func ImportEthKeyStoreCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "import-eth-keystore [name] [file]",
+		Short: "Import an Ethereum private keystore",
+		Long:  `Import an Ethereum private keystore file.`,
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			algo, err := cmd.Flags().GetString(flagKeyAlgo)
+			if err != nil {
+				return err
+			}
+			inBuf := bufio.NewReader(cmd.InOrStdin())
+			accountName := args[0]
+			file := args[1]
+
+			// Get keystore password
+			decryptPassword, err := input.GetPassword("Enter passphrase to decrypt keystore file:", inBuf)
+			if err != nil {
+				return err
+			}
+
+			passphrase, err := input.GetPassword("Enter passphrase to encrypt your key:", inBuf)
+			if err != nil {
+				return err
+			}
+
+			privkeyArmor, err := ethkeystore.ImportKeyStoreFile(decryptPassword, passphrase, file, keys.SigningAlgo(algo))
+			if err != nil {
+				return err
+			}
+			buf := bufio.NewReader(cmd.InOrStdin())
+			kb, err := keys.NewKeyring(sdk.KeyringServiceName(), viper.GetString(flags.FlagKeyringBackend), viper.GetString(flags.FlagHome), buf)
+			if err != nil {
+				return err
+			}
+
+			return kb.ImportPrivKey(accountName, privkeyArmor, passphrase)
+		},
+	}
+	cmd.Flags().String(flagKeyAlgo, string(keys.Secp256k1), "Key signing algorithm to generate keys for")
+	return cmd
 }
