9e921818621fd7778f16850ab5e068726769f3b8
BananaLF <864685021@qq.com>
2023-01-11 12:20:10 +0800

    disable cmd 'export-eth-comp '

--- cmd/client/export.go
+++ cmd/client/export.go
@@ -83,65 +83,15 @@ func UnsafeExportEthKeyCommand() *cobra.Command {
 // ExportEthCompCommand exports a key with the given name as a keystore file.
 func ExportEthCompCommand() *cobra.Command {
 	return &cobra.Command{
-		Use:   "export-eth-comp [name] [dir]",
-		Short: "Export an Ethereum private keystore directory (This command has been deprecated)",
+		Use:    "export-eth-comp [name] [dir]",
+		Short:  "Export an Ethereum private keystore directory (This command has been deprecated,please use 'exchaincli keys export-eth-comp')",
+		Hidden: true,
 		Long: `Export an Ethereum private keystore file encrypted to use in eth client import.
 
 	The parameters of scrypt encryption algorithm is StandardScryptN and StandardScryptN`,
 		Args: cobra.ExactArgs(2),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			inBuf := bufio.NewReader(cmd.InOrStdin())
-			accountName := args[0]
-			dir := args[1]
-
-			kb, err := keys.NewKeyring(
-				sdk.KeyringServiceName(),
-				viper.GetString(flags.FlagKeyringBackend),
-				viper.GetString(flags.FlagHome),
-				inBuf,
-				hd.EthSecp256k1Options()...,
-			)
-			if err != nil {
-				return err
-			}
-
-			decryptPassword := ""
-			conf := true
-			keyringBackend := viper.GetString(flags.FlagKeyringBackend)
-			switch keyringBackend {
-			case keys.BackendFile:
-				decryptPassword, err = input.GetPassword(
-					"Enter passphrase to decrypt your key:",
-					inBuf)
-			case keys.BackendOS:
-				conf, err = input.GetConfirmation(
-					"Decrypt your key by os passphrase. Are you sure?",
-					inBuf)
-			}
-			if err != nil || !conf {
-				return err
-			}
-
-			// Get keystore password
-			encryptPassword, err := input.GetPassword("Enter passphrase to encrypt the exported keystore file:", inBuf)
-			if err != nil {
-				return err
-			}
-
-			// exports private key from keybase using password
-			privKey, err := kb.ExportPrivateKeyObject(accountName, decryptPassword)
-			if err != nil {
-				return err
-			}
-
-			// Exports private key from keybase using password
-			fileName, err := ethkeystore.CreateKeystoreByTmKey(privKey, dir, encryptPassword)
-			if err != nil {
-				return err
-			}
-
-			fmt.Printf("The keystore has exported to: %s \n", fileName)
-			return nil
+			return fmt.Errorf("This command has been deprecated,please use 'exchaincli keys export-eth-comp'")
 		},
 	}
 }
