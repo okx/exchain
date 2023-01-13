8db0d5cacf741e9a38ed888fcf0e07e63dd3841f
jianguo <zjg555543@163.com>
2023-01-13 18:01:18 +0800

    modify movem version

--- x/move/keeper/keeper.go
+++ x/move/keeper/keeper.go
@@ -7,6 +7,7 @@ import (
 	"github.com/okex/exchain/libs/tendermint/libs/log"
 	"github.com/okex/exchain/x/params"
 	"github.com/zjg555543/movevm/api"
+	movetypes "github.com/zjg555543/movevm/types"
 	"io/ioutil"
 
 	"github.com/okex/exchain/x/move/types"
@@ -53,39 +54,46 @@ func (k Keeper) PublishMove(ctx sdk.Context, delegatorAddr sdk.AccAddress, moveP
 	)
 
 	version, _ := movevm.Version()
+
 	fmt.Println("finished", version)
 
 	pathList := [...]string{
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/debug.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/fixed_point32.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/hash.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/vector.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/errors.mv", "/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/acl.mv", "/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/option.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/ascii.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/bit_vector.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/signer.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/error.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/capability.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/compare.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/guid.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/bcs.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/event.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/offer.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/role.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/string.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/type_name.mv",
-		"/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/vault.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/debug.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/fixed_point32.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/hash.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/vector.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/errors.mv", "/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/acl.mv", "/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/option.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/ascii.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/bit_vector.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/signer.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/error.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/capability.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/compare.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/guid.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/bcs.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/event.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/offer.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/role.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/string.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveStdlib/type_name.mv",
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/dependencies/MoveNursery/vault.mv",
+
+		"/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/Account.mv",
 	}
+
+	balance := movetypes.Coins{movetypes.NewCoin(250, "ATOM")}
+	querier := api.DefaultQuerier(api.MOCK_CONTRACT_ADDR, balance)
+
 	for _, s := range pathList {
-		movevm.Publish(readModule(s), []byte("0x1"), []byte("1234567890"), k.gasMeter, k.store, nil, nil, 10000, false)
+		movevm.Publish(readModule(s), []byte("0x1"), []byte("1234567890"), k.gasMeter, k.store, nil, &querier, 10000, false)
 	}
 
 	testByte := []byte("1234567890")
-	moduleBytes2 := readModule("/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/Test.mv")
-	movevm.Publish(moduleBytes2, []byte("0x2"), testByte, k.gasMeter, k.store, nil, nil, 10000, false)
+	moduleBytes2 := readModule("/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/Test.mv")
+	movevm.Publish(moduleBytes2, []byte("0x2"), testByte, k.gasMeter, k.store, nil, &querier, 10000, false)
 
-	moduleBytes3 := readModule("/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_modules/Caller.mv")
-	movevm.Publish(moduleBytes3, []byte("0x3"), testByte, k.gasMeter, k.store, nil, nil, 10000, false)
+	moduleBytes3 := readModule("/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_modules/Caller.mv")
+	movevm.Publish(moduleBytes3, []byte("0x3"), testByte, k.gasMeter, k.store, nil, &querier, 10000, false)
 	logger := k.Logger(ctx)
 
 	logger.Info("Publish move contract ok.......")
@@ -100,8 +108,11 @@ func (k Keeper) RunMove(ctx sdk.Context, delegatorAddr sdk.AccAddress, movePath 
 		),
 	)
 
-	scriptBytes := readModule("/Users/oker/workspace/move/movevm/contracts/readme/build/readme/bytecode_scripts/test_script.mv")
-	movevm.Run(scriptBytes, []byte("0xF"), []byte("1234567890"), k.gasMeter, k.store, nil, nil, 10000, false)
+	balance := movetypes.Coins{movetypes.NewCoin(250, "ATOM")}
+	querier := api.DefaultQuerier(api.MOCK_CONTRACT_ADDR, balance)
+
+	scriptBytes := readModule("/Users/oker/move/movevm/contracts/readme/build/readme/bytecode_scripts/test_script.mv")
+	movevm.Run(scriptBytes, []byte("0xF"), []byte("1234567890"), k.gasMeter, k.store, nil, &querier, 10000, false)
 
 	logger := k.Logger(ctx)
 
