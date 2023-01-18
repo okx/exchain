b15dc143761c9e3e6caf0278ce469dc506552f7d
Leo <335209779@qq.com>
2023-01-18 16:20:42 +0800

    improve code

---
+++ libs/tendermint/global/recommended_gasprice.go
@@ -0,0 +1,21 @@
+package global
+
+import (
+	"math/big"
+)
+
+var globalMinGasPrice *big.Int
+var globalMaxGasPrice *big.Int
+
+func SetGlobalMinGasPrice(minGP *big.Int) {
+	globalMinGasPrice = new(big.Int).Set(minGP)
+	globalMaxGasPrice = new(big.Int).Mul(globalMinGasPrice, big.NewInt(5000))
+}
+
+func GetGlobalMinGasPrice() *big.Int {
+	return globalMinGasPrice
+}
+
+func GetGlobalMaxGasPrice() *big.Int {
+	return globalMaxGasPrice
+}
