33c884fd7892e9a890a72a0adf0cdb549cf6d652
itsfunny <18757883747@163.com>
2023-02-22 17:56:18 +0800

    boolean

--- libs/ibc-go/modules/core/base/base.go
+++ libs/ibc-go/modules/core/base/base.go
@@ -40,7 +40,7 @@ var (
 
 		// ibc modules
 		if types.HigherThanVenus1(h) {
-			panic("asd")
+			return false
 		}
 
 		// < veneus1
@@ -54,7 +54,7 @@ var (
 
 		// ibc modulee && >=veneus1
 		if types.HigherThanVenus1(h) {
-			panic("vvv")
+			return false
 		}
 
 		// < veneus1
