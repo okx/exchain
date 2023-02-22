ae9ea277d6384856e604328256128043aec96224
itsfunny <18757883747@163.com>
2023-02-22 18:03:09 +0800

    optimize codes

--- libs/ibc-go/modules/core/base/base.go
+++ libs/ibc-go/modules/core/base/base.go
@@ -9,69 +9,7 @@ import (
 )
 
 var (
-	_      upgrade.UpgradeModule = (*BaseIBCUpgradeModule)(nil)
-	ibcMap                       = map[string]struct{}{
-		"ibc":            struct{}{},
-		"mem_capability": struct{}{},
-		"capability":     struct{}{},
-		"transfer":       struct{}{},
-		"erc20":          struct{}{},
-	}
-	defaultDenyFilter cosmost.StoreFilter = func(module string, h int64, store cosmost.CommitKVStore) bool {
-		_, exist := ibcMap[module]
-		if !exist {
-			return false
-		}
-		return true
-	}
-	defaultIBCCommitFilter cosmost.StoreFilter = func(module string, h int64, store cosmost.CommitKVStore) bool {
-		_, exist := ibcMap[module]
-		if !exist {
-			return false
-		}
-
-		// ==veneus1
-		if h == types.GetVenus1Height() {
-			if store != nil {
-				store.SetUpgradeVersion(h)
-			}
-			return false
-		}
-
-		// ibc modules
-		if types.HigherThanVenus1(h) {
-			return false
-		}
-
-		// < veneus1
-		return true
-	}
-	defaultIBCPruneFilter cosmost.StoreFilter = func(module string, h int64, store cosmost.CommitKVStore) bool {
-		_, exist := ibcMap[module]
-		if !exist {
-			return false
-		}
-
-		// ibc modulee && >=veneus1
-		if types.HigherThanVenus1(h) {
-			return false
-		}
-
-		// < veneus1
-		return true
-	}
-	defaultIBCVersionFilter cosmost.VersionFilter = func(h int64) func(cb func(name string, version int64)) {
-		if h < 0 {
-			return func(cb func(name string, version int64)) {}
-		}
-
-		return func(cb func(name string, version int64)) {
-			for name, _ := range ibcMap {
-				hh := types.GetVenus1Height()
-				cb(name, hh)
-			}
-		}
-	}
+	_ upgrade.UpgradeModule = (*BaseIBCUpgradeModule)(nil)
 )
 
 type BaseIBCUpgradeModule struct {
@@ -96,20 +34,14 @@ func (b *BaseIBCUpgradeModule) UpgradeHeight() int64 {
 }
 
 func (b *BaseIBCUpgradeModule) CommitFilter() *cosmost.StoreFilter {
-	if b.UpgradeHeight() == 0 {
-		return &defaultDenyFilter
-	}
-	return &defaultIBCCommitFilter
+	return nil
 }
 func (b *BaseIBCUpgradeModule) PruneFilter() *cosmost.StoreFilter {
-	if b.UpgradeHeight() == 0 {
-		return &defaultDenyFilter
-	}
-	return &defaultIBCPruneFilter
+	return nil
 }
 
 func (b *BaseIBCUpgradeModule) VersionFilter() *cosmost.VersionFilter {
-	return &defaultIBCVersionFilter
+	return nil
 }
 
 func (b *BaseIBCUpgradeModule) RegisterParam() params.ParamSet {
