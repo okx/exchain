872d33d86bf18bee3099b2336afbd7ba2d10fb1d
evan.han <iloversevan@gmail.com>
2023-02-27 15:26:25 +0800

    x/staking: move endblocker to abci

--- x/staking/abci.go
+++ x/staking/abci.go
@@ -1,14 +1,66 @@
 package staking
 
 import (
+	"fmt"
 	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
-	"github.com/okex/exchain/libs/tendermint/types"
+	abci "github.com/okex/exchain/libs/tendermint/abci/types"
+	tmtypes "github.com/okex/exchain/libs/tendermint/types"
+	"github.com/okex/exchain/x/staking/keeper"
+	"github.com/okex/exchain/x/staking/types"
 )
 
 // BeginBlocker will persist the current header and validator set as a historical entry
 // and prune the oldest entry based on the HistoricalEntries parameter
 func BeginBlocker(ctx sdk.Context, k Keeper) {
-	if types.HigherThanVenus1(ctx.BlockHeight()) {
+	if tmtypes.HigherThanVenus1(ctx.BlockHeight()) {
 		k.TrackHistoricalInfo(ctx)
 	}
+}
+
+// EndBlocker is called every block, update validator set
+func EndBlocker(ctx sdk.Context, k keeper.Keeper) []abci.ValidatorUpdate {
+	// calculate validator set changes
+	validatorUpdates := make([]abci.ValidatorUpdate, 0)
+	if k.IsEndOfEpoch(ctx) {
+		oldEpoch, newEpoch := k.GetEpoch(ctx), k.ParamsEpoch(ctx)
+		if oldEpoch != newEpoch {
+			k.SetEpoch(ctx, newEpoch)
+		}
+		k.SetTheEndOfLastEpoch(ctx)
+		//ctx.Logger().Debug("validatorUpdates epoch", "old", oldEpoch, "new", newEpoch)
+		//ctx.Logger().Debug(fmt.Sprintf("old epoch end blockHeight: %d", lastEpochEndHeight))
+
+		validatorUpdates = k.ApplyAndReturnValidatorSetUpdates(ctx)
+		// dont forget to delete in case that some validator need to kick out when an epoch ends
+		k.DeleteAbandonedValidatorAddrs(ctx)
+	} else if k.IsKickedOut(ctx) {
+		// if there are some validators to kick out in an epoch
+		validatorUpdates = k.KickOutAndReturnValidatorSetUpdates(ctx)
+		k.DeleteAbandonedValidatorAddrs(ctx)
+	}
+
+	// Unbond all mature validators from the unbonding queue.
+	k.UnbondAllMatureValidatorQueue(ctx)
+
+	k.IterateKeysBeforeCurrentTime(ctx, ctx.BlockHeader().Time,
+		func(index int64, key []byte) (stop bool) {
+			oldTime, delAddr := types.SplitCompleteTimeWithAddrKey(key)
+			k.DeleteAddrByTimeKey(ctx, oldTime, delAddr)
+
+			quantity, err := k.CompleteUndelegation(ctx, delAddr)
+			if err != nil {
+				ctx.Logger().Error(fmt.Sprintf("complete withdraw failed: %s", err))
+			} else {
+				ctx.EventManager().EmitEvent(
+					sdk.NewEvent(
+						types.EventTypeCompleteUnbonding,
+						sdk.NewAttribute(types.AttributeKeyDelegator, delAddr.String()),
+						sdk.NewAttribute(sdk.AttributeKeyAmount, quantity.String()),
+					),
+				)
+			}
+			return false
+		})
+
+	return validatorUpdates
 }
