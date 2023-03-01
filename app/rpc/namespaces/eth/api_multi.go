a051b014d9ab657561de65911fa03bc57f3d8c14
Leo <52782564+LeoGuo621@users.noreply.github.com>
2023-03-01 18:11:33 +0800

    Merge PR: Clear Logs of failed txs (#3002)

--- app/rpc/namespaces/eth/api_multi.go
+++ app/rpc/namespaces/eth/api_multi.go
@@ -251,9 +251,11 @@ func (api *PublicEthereumAPI) GetTransactionReceiptsByBlock(blockNrOrHash rpctyp
 			status = 0 // transaction failed
 		}
 
-		if len(data.Logs) == 0 {
+		if len(data.Logs) == 0 || status == 0 {
 			data.Logs = []*ethtypes.Log{}
+			data.Bloom = ethtypes.BytesToBloom(make([]byte, 256))
 		}
+
 		contractAddr := &data.ContractAddress
 		if data.ContractAddress == common.HexToAddress("0x00000000000000000000") {
 			contractAddr = nil
