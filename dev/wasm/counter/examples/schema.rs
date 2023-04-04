aa01b1c652edbc4ab29e79c0d2169f43e184df66
zhongqiuwood <zhongqiuwood@gmail.com>
2023-04-04 17:54:30 +0800

    upd

--- dev/wasm/counter/examples/schema.rs
+++ dev/wasm/counter/examples/schema.rs
@@ -3,8 +3,8 @@ use std::fs::create_dir_all;
 
 use cosmwasm_schema::{export_schema, remove_schemas, schema_for};
 
-use wasm_counter::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
-use wasm_counter::state::State;
+use counter::msg::{CountResponse, ExecuteMsg, InstantiateMsg, QueryMsg};
+use counter::state::State;
 
 fn main() {
     let mut out_dir = current_dir().unwrap();
--- dev/wasm/counter/run.sh
+++ dev/wasm/counter/run.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-OPTIONS="--from captain --gas-prices 0.00000001okt --gas auto -b block --gas-adjustment 1.1 -y"
+OPTIONS="--from captain --gas-prices 0.0000000001okt --gas auto -b block --gas-adjustment 6.1 -y"
+OPTIONS="--from captain --fees 0.01okt --gas 6000000 -b block -y"
 
+#exchaincli tx wasm store ./counter.wasm --from captain --gas-prices 0.00000001okt --gas auto -b block --gas-adjustment 1.1 -y
 exchaincli tx wasm store ./counter.wasm ${OPTIONS}
-
-exchaincli tx wasm instantiate 1 ${OPTIONS}
-
-exchaincli tx wasm execute  ${OPTIONS}
-
-exchaincli tx wasm execute  ${OPTIONS}
-
-exchaincli query wasm contract-state smart  '{"balance":{"address":"ex190227rqaps5nplhg2tg8hww7slvvquzy0qa0l0"}}'
+#
+exchaincli tx wasm instantiate 1 --from captain --fees 0.01okt --gas 6000000 -b block -y
+#
+#exchaincli tx wasm execute  ${OPTIONS}
+#
+#exchaincli tx wasm execute  ${OPTIONS}
+#
+#exchaincli query wasm contract-state smart  '{"balance":{"address":"ex190227rqaps5nplhg2tg8hww7slvvquzy0qa0l0"}}'
