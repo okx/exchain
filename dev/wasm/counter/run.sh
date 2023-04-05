0a078a1f9fdb6174d2505ae11879b70a7abdbe66
Zhong Qiu <36867992+zhongqiuwood@users.noreply.github.com>
2023-04-06 07:04:23 +0800

    rm no-admin option (#3064)
    * rm no-admin option

--- dev/wasm/counter/run.sh
+++ dev/wasm/counter/run.sh
@@ -6,7 +6,7 @@ OPTIONS="--from captain --fees 0.01okt --gas 6000000 -b block -y"
 #exchaincli tx wasm store ./counter.wasm --from captain --gas-prices 0.00000001okt --gas auto -b block --gas-adjustment 1.1 -y
 exchaincli tx wasm store ./counter.wasm --from captain --fees 0.01okt --gas 6000000 -b block -y
 #
-exchaincli tx wasm instantiate 1 '{}' --label test --from captain --fees 0.01okt --gas 6000000 --no-admin -b block -y
+exchaincli tx wasm instantiate 1 '{}'  --from captain --fees 0.01okt --gas 6000000 -b block -y
 #
 exchaincli tx wasm execute ex14hj2tavq8fpesdwxxcu44rty3hh90vhujrvcmstl4zr3txmfvw9s6fqu27  \
 '{"add":{"delta":"16"}}' \
