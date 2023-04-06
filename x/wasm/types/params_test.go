be76fc2b71d80978a631f122d99d6ab9317e8859
KamiD <44460798+KamiD@users.noreply.github.com>
2023-04-06 11:17:45 +0800

    fix ut

--- x/wasm/types/params_test.go
+++ x/wasm/types/params_test.go
@@ -149,7 +149,7 @@ func TestParamsUnmarshalJson(t *testing.T) {
 		exp Params
 	}{
 		"defaults": {
-			src: `{"code_upload_access": {"permission": "Nobody"},
+			src: `{"code_upload_access": {"permission": "Everybody"},
 				"instantiate_default_permission": "Everybody",
 				"use_contract_blocked_list":true,
 				"vmbridge_enable":false}`,
