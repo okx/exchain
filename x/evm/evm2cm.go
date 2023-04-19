616da0ba106dc577fc63387159e87e1173bb278a
lyh169 <liuyuhang203@sina.com>
2023-04-19 17:23:33 +0800

    update the syscontract abi

--- x/evm/evm2cm.go
+++ x/evm/evm2cm.go
@@ -17,7 +17,7 @@ var (
 )
 
 const (
-	sysContractABI            = `[{"inputs":[{"internalType":"string","name":"data","type":"string"}],"name":"invoke","outputs":[],"stateMutability":"nonpayable","type":"function"}]`
+	sysContractABI            = `[{"inputs":[{"internalType":"string","name":"_data","type":"string"}],"name":"invoke","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"string","name":"input","type":"string"}],"name":"query","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"}]`
 	sysContractInvokeFunction = "invoke"
 )
 
