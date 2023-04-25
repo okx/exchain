9b05bd74bde2c2d8f3d4c25aa752298699920e19
lyh169 <liuyuhang203@sina.com>
2023-04-25 13:14:50 +0800

    add wasm count tx decorator in E2C

---
+++ app/ante/wrapWasmCountTXDecorator.go
@@ -0,0 +1,33 @@
+package ante
+
+import (
+	sdk "github.com/okex/exchain/libs/cosmos-sdk/types"
+	tmtypes "github.com/okex/exchain/libs/tendermint/types"
+	"github.com/okex/exchain/x/evm/types"
+	wasmkeeper "github.com/okex/exchain/x/wasm/keeper"
+)
+
+type WrapWasmCountTXDecorator struct {
+	ctd       *wasmkeeper.CountTXDecorator
+	evmKeeper EVMKeeper
+}
+
+// NewWrapWasmCountTXDecorator constructor
+func NewWrapWasmCountTXDecorator(ctd *wasmkeeper.CountTXDecorator, evmKeeper EVMKeeper) *WrapWasmCountTXDecorator {
+	return &WrapWasmCountTXDecorator{ctd: ctd, evmKeeper: evmKeeper}
+}
+
+func (a WrapWasmCountTXDecorator) AnteHandle(ctx sdk.Context, tx sdk.Tx, simulate bool, next sdk.AnteHandler) (sdk.Context, error) {
+	if isE2CTx(a.evmKeeper, &ctx, tx) && tmtypes.HigherThanVenus6(ctx.BlockHeight()) {
+		return a.ctd.AnteHandle(ctx, tx, simulate, next)
+	}
+	return next(ctx, tx, simulate)
+}
+
+func isE2CTx(ek EVMKeeper, ctx *sdk.Context, tx sdk.Tx) bool {
+	evmTx, ok := tx.(*types.MsgEthereumTx)
+	if !ok {
+		return false
+	}
+	return IsE2CTx(ek, ctx, evmTx)
+}
