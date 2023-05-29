e1be51e4eed281f01602529b8b78eb3015793665
jianguo <zjg555543@163.com>
2023-05-29 12:01:42 +0800

    update

--- dev/rust.sh
+++ dev/rust.sh
@@ -8,12 +8,13 @@ cd -
 
 cd ../../wasmvm/libwasmvm
 cargo build --release
-cp target/release/libwasmvm.dylib ../api/libwasmvm.dylib
-
+#cp target/release/libwasmvm.dylib ../api/libwasmvm.dylib
+cp target/release/libwasmvm.dylib ../internal/api/libwasmvm.dylib
 
 cd -
 
 cd ../../exchain/
 
+go mod tidy
 go mod vendor
 
