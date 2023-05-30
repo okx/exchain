acbc76b0e24d943d36545842311755419ae164cb
Leo <335209779@qq.com>
2023-05-30 22:27:11 +0800

    fix smb caused by vm-bridge tx

--- x/wasm/types/types.go
+++ x/wasm/types/types.go
@@ -280,6 +280,10 @@ func NewEnv(ctx sdk.Context, contractAddr sdk.WasmAddress) wasmvmtypes.Env {
 	} else {
 		if txCounter, ok := TXCounter(ctx); ok {
 			env.Transaction = &wasmvmtypes.TransactionInfo{Index: txCounter}
+		} else {
+			// fix smb caused by vm-bridge tx
+			// more detail see https://github.com/okx/oec/issues/2190
+			env.Transaction = &wasmvmtypes.TransactionInfo{Index: 0}
 		}
 	}
 	return env
