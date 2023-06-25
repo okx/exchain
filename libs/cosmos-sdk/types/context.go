8468d8d8b8d75fd8cc9a70384d5ba8a37878602b
chengzhinei <chengxue5yuan@163.com>
2023-06-25 15:57:43 +0800

    fix dds wasm (#3181)
    * fix dds wasm
    
    * simulate not set WasmStoreCode
    
    * change isDeliver to IsDeliverWithSerial
    
    * change isDeliver to IsDeliverWithSerial
    
    * change isDeliver to IsDeliverWithSerial
    
    * change isDeliver to IsDeliverWithSerial
    
    ---------
    
    Co-authored-by: KamiD <44460798+KamiD@users.noreply.github.com>

--- libs/cosmos-sdk/types/context.go
+++ libs/cosmos-sdk/types/context.go
@@ -24,31 +24,31 @@ but please do not over-use it. We try to keep all data structured
 and standard additions here would be better just to add to the Context struct
 */
 type Context struct {
-	ctx                context.Context
-	ms                 MultiStore
-	header             *abci.Header
-	chainID            string
-	from               string
-	txBytes            []byte
-	logger             log.Logger
-	voteInfo           []abci.VoteInfo
-	gasMeter           GasMeter
-	blockGasMeter      GasMeter
-	isDeliver          bool
-	checkTx            bool
-	recheckTx          bool   // if recheckTx == true, then checkTx must also be true
-	wrappedCheckTx     bool   // if wrappedCheckTx == true, then checkTx must also be true
-	traceTx            bool   // traceTx is set true for trace tx and its predesessors , traceTx was set in app.beginBlockForTrace()
-	traceTxLog         bool   // traceTxLog is used to create trace logger for evm , traceTxLog is set to true when only tracing target tx (its predesessors will set false), traceTxLog is set before runtx
-	traceTxConfigBytes []byte // traceTxConfigBytes is used to save traceTxConfig, passed from api to x/evm
-	minGasPrice        DecCoins
-	consParams         *abci.ConsensusParams
-	eventManager       *EventManager
-	accountNonce       uint64
-	cache              *Cache
-	trc                *trace.Tracer
-	accountCache       *AccountCache
-	paraMsg            *ParaMsg
+	ctx                 context.Context
+	ms                  MultiStore
+	header              *abci.Header
+	chainID             string
+	from                string
+	txBytes             []byte
+	logger              log.Logger
+	voteInfo            []abci.VoteInfo
+	gasMeter            GasMeter
+	blockGasMeter       GasMeter
+	isDeliverWithSerial bool
+	checkTx             bool
+	recheckTx           bool   // if recheckTx == true, then checkTx must also be true
+	wrappedCheckTx      bool   // if wrappedCheckTx == true, then checkTx must also be true
+	traceTx             bool   // traceTx is set true for trace tx and its predesessors , traceTx was set in app.beginBlockForTrace()
+	traceTxLog          bool   // traceTxLog is used to create trace logger for evm , traceTxLog is set to true when only tracing target tx (its predesessors will set false), traceTxLog is set before runtx
+	traceTxConfigBytes  []byte // traceTxConfigBytes is used to save traceTxConfig, passed from api to x/evm
+	minGasPrice         DecCoins
+	consParams          *abci.ConsensusParams
+	eventManager        *EventManager
+	accountNonce        uint64
+	cache               *Cache
+	trc                 *trace.Tracer
+	accountCache        *AccountCache
+	paraMsg             *ParaMsg
 	//	txCount            uint32
 
 	wasmSimulateCache map[string][]byte
@@ -87,12 +87,12 @@ func (c *Context) VoteInfos() []abci.VoteInfo { return c.voteInfo }
 func (c *Context) GasMeter() GasMeter         { return c.gasMeter }
 func (c *Context) BlockGasMeter() GasMeter    { return c.blockGasMeter }
 
-func (c *Context) IsDeliver() bool {
-	return c.isDeliver
+func (c *Context) IsDeliverWithSerial() bool {
+	return c.isDeliverWithSerial
 }
 
 func (c *Context) UseParamCache() bool {
-	return c.isDeliver || (c.paraMsg != nil && !c.paraMsg.HaveCosmosTxInBlock) || c.checkTx
+	return c.isDeliverWithSerial || (c.paraMsg != nil && !c.paraMsg.HaveCosmosTxInBlock) || c.checkTx
 }
 
 func (c *Context) IsCheckTx() bool             { return c.checkTx }
@@ -207,8 +207,8 @@ func NewContext(ms MultiStore, header abci.Header, isCheckTx bool, logger log.Lo
 	}
 }
 
-func (c *Context) SetDeliver() *Context {
-	c.isDeliver = true
+func (c *Context) SetDeliverSerial() *Context {
+	c.isDeliverWithSerial = true
 	return c
 }
 
@@ -304,8 +304,8 @@ func (c *Context) SetIsCheckTx(isCheckTx bool) *Context {
 	return c
 }
 
-func (c *Context) SetIsDeliverTx(isDeliverTx bool) *Context {
-	c.isDeliver = isDeliverTx
+func (c *Context) SetIsDeliverTxSerial(isDeliverWithSerial bool) *Context {
+	c.isDeliverWithSerial = isDeliverWithSerial
 	return c
 }
 
