b377e3caae20f587b1a97ebb197d9784fb751686
lyh169 <liuyuhang203@sina.com>
2023-06-27 22:17:56 +0800

    adaptive the return result

--- x/wasm/keeper/cross_contarct_call.go
+++ x/wasm/keeper/cross_contarct_call.go
@@ -19,14 +19,14 @@ func SetWasmKeeper(k *Keeper) {
 	wasmKeeper = *k
 }
 
-func GenerateCallerInfo(q unsafe.Pointer, contractAddress string) ([]byte, wasmvm.KVStore, wasmvm.Querier) {
+func GenerateCallerInfo(q unsafe.Pointer, contractAddress string) ([]byte, wasmvm.KVStore, wasmvm.Querier, wasmvm.GasMeter) {
 	goQuerier := *(*wasmvm.Querier)(q)
 	qq := goQuerier.(QueryHandler)
-	code, _, store, querier := generateCallerInfo(qq.Ctx, contractAddress)
-	return code, store, querier
+	code, _, store, querier, gasMeter := generateCallerInfo(qq.Ctx, contractAddress)
+	return code, store, querier, gasMeter
 }
 
-func generateCallerInfo(ctx sdk.Context, addr string) ([]byte, wasmvmtypes.Env, wasmvm.KVStore, wasmvm.Querier) {
+func generateCallerInfo(ctx sdk.Context, addr string) ([]byte, wasmvmtypes.Env, wasmvm.KVStore, wasmvm.Querier, wasmvm.GasMeter) {
 	contractAddress, err := sdk.WasmAddressFromBech32(addr)
 	if err != nil {
 		panic(fmt.Sprintln("WasmAddressFromBech32 err", err))
@@ -34,8 +34,8 @@ func generateCallerInfo(ctx sdk.Context, addr string) ([]byte, wasmvmtypes.Env, 
 	env := types.NewEnv(ctx, contractAddress)
 	_, codeInfo, prefixStore, err := wasmKeeper.contractInstance(ctx, contractAddress)
 	if err != nil {
-		return nil, env, nil, nil
+		return nil, env, nil, nil, nil
 	}
 	queryHandler := wasmKeeper.newQueryHandler(ctx, contractAddress)
-	return codeInfo.CodeHash, env, prefixStore, &queryHandler
+	return codeInfo.CodeHash, env, prefixStore, &queryHandler, wasmKeeper.gasMeter(ctx)
 }
