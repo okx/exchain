8ea2ffb4bad25d43c5885ea3e7d2696bf7b88f12
LeoGuo621 <335209779@qq.com>
2023-07-11 20:59:27 +0800

    fix smb caused by evm AnteErr tx

--- libs/cosmos-sdk/baseapp/baseapp.go
+++ libs/cosmos-sdk/baseapp/baseapp.go
@@ -703,7 +703,7 @@ func (app *BaseApp) getContextForTx(mode runTxMode, txBytes []byte) sdk.Context 
 			// and there is a small probability that NeedUpdateTXCounter() will be wrong
 			// due to concurrent reading and writing of pm.txIndexMpUpdateTXCounter (slice),
 			// but such tx will be rerun, so this case can be ignored.
-			NeedUpdateTXCounter: app.parallelTxManage.NeedUpdateTXCounter(),
+			HaveCosmosTxInBlock: app.parallelTxManage.NeedUpdateTXCounter(),
 			CosmosIndexInBlock:  app.parallelTxManage.txByteMpCosmosIndex[string(txBytes)],
 		})
 		app.parallelTxManage.txByteMpCMIndexLock.RUnlock()
