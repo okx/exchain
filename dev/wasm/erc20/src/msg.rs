21d245c7dbee19b70fbaa82eab2442692879ec8a
BananaLF <864685021@qq.com>
2023-07-11 09:42:53 +0800

    fix erc20 to cw20 recipient can not user 0x prefix address (#3197)

--- dev/wasm/erc20/src/msg.rs
+++ dev/wasm/erc20/src/msg.rs
@@ -48,11 +48,6 @@ pub enum ExecuteMsg {
         recipient: String,
         amount: Uint128,
     },
-    CallToEvm {
-        evmContract: String,
-        calldata: String,
-        value: Uint128,
-    }
 }
 
 #[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
@@ -71,21 +66,6 @@ impl Into<CosmosMsg<SendToEvmMsg>> for SendToEvmMsg {
 }
 impl CustomMsg for SendToEvmMsg {}
 
-#[derive(Serialize, Deserialize, Clone, PartialEq, JsonSchema, Debug)]
-#[serde(rename_all = "snake_case")]
-pub struct CallToEvmMsg {
-    pub sender: String,
-    pub evmaddr: String,
-    pub calldata: String,
-    pub value: Uint128,
-
-}
-impl Into<CosmosMsg<CallToEvmMsg>> for CallToEvmMsg {
-    fn into(self) -> CosmosMsg<CallToEvmMsg> {
-        CosmosMsg::Custom(self)
-    }
-}
-impl CustomMsg for CallToEvmMsg {}
 
 #[derive(Serialize, Deserialize, Clone, Debug, PartialEq, JsonSchema)]
 #[serde(rename_all = "snake_case")]
