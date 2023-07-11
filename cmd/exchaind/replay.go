a020cd607444402414652d00d9896d0a3e4b3c0e
chengzhinei <chengxue5yuan@163.com>
2023-07-11 18:00:41 +0800

    sleep 2s

--- cmd/exchaind/replay.go
+++ cmd/exchaind/replay.go
@@ -358,7 +358,7 @@ func doReplay(ctx *server.Context, state sm.State, stateStoreDB dbm.DB, blockSto
 	needSaveBlock := viper.GetBool(saveBlock)
 	global.SetGlobalHeight(lastBlockHeight + 1)
 	for height := lastBlockHeight + 1; height <= haltheight; height++ {
-		time.Sleep(time.Second)
+		time.Sleep(time.Second * 2)
 		block := originBlockStore.LoadBlock(height)
 		meta := originBlockStore.LoadBlockMeta(height)
 		state, _, err = blockExec.ApplyBlockWithTrace(state, meta.BlockID, block)
