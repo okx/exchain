cdab1e27008c0fe763ee41825be76fc0cc19602e
lyh169 <liuyuhang203@sina.com>
2023-07-26 16:00:55 +0800

    rename the file and modify the ut

--- x/wasm/keeper/cross_contract_call_test.go
+++ x/wasm/keeper/cross_contract_call_test.go
@@ -33,21 +33,22 @@ func TestGetWasmCallInfo(t *testing.T) {
 	gotContractAddr, _, err := keepers.ContractKeeper.Instantiate(ctx, codeID, creator, nil, initMsgBz, "demo contract 1", nil)
 	require.NoError(t, err)
 
+	wasmkeeper := *keepers.WasmKeeper
 	// 1. contractAddress is equal to storeAddress
-	_, _, _, _, err = getCallerInfo(ctx, gotContractAddr.String(), gotContractAddr.String())
+	_, _, _, _, err = getCallerInfo(ctx, wasmkeeper, gotContractAddr.String(), gotContractAddr.String())
 	require.NoError(t, err)
 
 	// 2. contractAddress is not exist
-	_, _, _, _, err = getCallerInfo(ctx, "0xE70e7466a2f18FAd8C97c45Ba8fEc57d90F3435E", "0xE70e7466a2f18FAd8C97c45Ba8fEc57d90F3435E")
+	_, _, _, _, err = getCallerInfo(ctx, wasmkeeper, "0xE70e7466a2f18FAd8C97c45Ba8fEc57d90F3435E", "0xE70e7466a2f18FAd8C97c45Ba8fEc57d90F3435E")
 	require.NotNil(t, err)
 
 	// 3. storeAddress is not exist
-	_, _, _, _, err = getCallerInfo(ctx, gotContractAddr.String(), "0xE70e7466a2f18FAd8C97c45Ba8fEc57d90F3435E")
+	_, _, _, _, err = getCallerInfo(ctx, wasmkeeper, gotContractAddr.String(), "0xE70e7466a2f18FAd8C97c45Ba8fEc57d90F3435E")
 	require.NotNil(t, err)
 
 	// 4. contractAddress is not equal to storeAddress
 	gotContractAddr2, _, err := keepers.ContractKeeper.Instantiate(ctx, codeID, creator, nil, initMsgBz, "demo contract 1", nil)
-	_, kvs, q, _, err := getCallerInfo(ctx, gotContractAddr.String(), gotContractAddr2.String())
+	_, kvs, q, _, err := getCallerInfo(ctx, wasmkeeper, gotContractAddr.String(), gotContractAddr2.String())
 	require.NoError(t, err)
 	require.NotNil(t, kvs)
 	require.NotNil(t, q)
