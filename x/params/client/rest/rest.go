7588f1e9ca00b043643ac68802c046e22664823a
lyh169 <liuyuhang203@sina.com>
2023-08-30 18:42:23 +0800

    add rest query the block config

---
+++ x/params/client/rest/rest.go
@@ -0,0 +1,32 @@
+package rest
+
+import (
+	"fmt"
+	"github.com/okex/exchain/x/params/types"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/okex/exchain/libs/cosmos-sdk/client/context"
+	"github.com/okex/exchain/libs/cosmos-sdk/types/rest"
+	"github.com/okex/exchain/x/common"
+	"github.com/okex/exchain/x/params"
+)
+
+// RegisterRoutes, a central function to define routes
+// which is called by the rest module in main application
+func RegisterRoutes(cliCtx context.CLIContext, r *mux.Router) {
+	r.HandleFunc(fmt.Sprintf("/params/blockconfig"), QueryBlockConfigFn(cliCtx)).Methods("GET")
+}
+
+func QueryBlockConfigFn(cliCtx context.CLIContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		route := fmt.Sprintf("custom/%s/%s", params.RouterKey, types.QueryBlockConfig)
+		bz, _, err := cliCtx.QueryWithData(route, nil)
+		if err != nil {
+			sdkErr := common.ParseSDKError(err.Error())
+			common.HandleErrorMsg(w, cliCtx, sdkErr.Code, err.Error())
+			return
+		}
+		rest.PostProcessResponseBare(w, cliCtx, bz)
+	}
+}
