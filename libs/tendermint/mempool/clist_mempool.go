049e897b873abcd94a7cca0f3ddace0a4711e8a6
BananaLF <864685021@qq.com>
2023-12-10 23:36:03 +0800

    change log

--- libs/tendermint/mempool/clist_mempool.go
+++ libs/tendermint/mempool/clist_mempool.go
@@ -308,10 +308,10 @@ func (mem *CListMempool) CheckTx(tx types.Tx, cb func(*abci.Response), txInfo Tx
 		if !ok {
 			peerTxCount = 0
 		}
-		if peerTxCount > mem.config.MaxTxLimitPerPeer {
+		if peerTxCount >= mem.config.MaxTxLimitPerPeer {
 			mem.peersTxCountMtx.Unlock()
-			mem.logger.Error(fmt.Sprintf("%s has been over 100 transaction, please wait a few second", txInfo.SenderP2PID))
-			return fmt.Errorf("%s has been over 100 transaction, please wait a few second", txInfo.SenderP2PID)
+			mem.logger.Debug(fmt.Sprintf("%s has been over %d transaction, please wait a few second", txInfo.SenderP2PID, mem.config.MaxTxLimitPerPeer))
+			return fmt.Errorf("%s has been over %d transaction, please wait a few second", txInfo.SenderP2PID, mem.config.MaxTxLimitPerPeer)
 		}
 		peerTxCount++
 		mem.peersTxCount[string(txInfo.SenderP2PID)] = peerTxCount
